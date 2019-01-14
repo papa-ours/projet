21242d97de8de9b3848c3e63694c445bebd07d02
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-14 16:13:33 -0500

    VG VM : Affichage de la vue initiale dans le AppComponent D

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
+import { InitialViewComponent } from "./initial-view/initial-view.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialViewComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [BasicService],
   bootstrap: [AppComponent]
