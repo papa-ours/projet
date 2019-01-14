c280246372258db7b062e35150eaebf34834feae
mez <mezianechebili@gmail.com>
2019-01-14 12:32:31 -0500

    MC DP PDB: ajout du composant game list D

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
+import { GameListComponent } from './game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
