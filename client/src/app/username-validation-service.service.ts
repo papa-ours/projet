75a38d130e139f9929dcfc833a05cf9df9fcf48a
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-16 09:35:00 -0500

    VG VM : Indentation du code pour le rendre plus comprehensible R

--- client/src/app/username-validation-service.service.ts
+++ client/src/app/username-validation-service.service.ts
@@ -13,14 +13,12 @@ export class UsernameValidationService {
     public constructor(private http: HttpClient) { }
 
     public getUsernameValidation(username: string): Observable<Message> {
-        return this.http.get<Message>(this.BASE_URL + username).pipe(
-            catchError(this.handleError<Message>("getUsernameValidation"))
+        return this.http.get<Message>(this.BASE_URL + username)
+            .pipe(catchError(this.handleError<Message>("getUsernameValidation"))
         );
     }
 
     private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
-        return (error: Error): Observable<T> => {
-            return of(result as T);
-        };
+        return (error: Error): Observable<T> => of(result as T);
     }
 }
