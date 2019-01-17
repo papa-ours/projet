364bb035791ea84d9284711d29a2710da202c0e7
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-17 14:21:47 -0500

    VG VM : Création de l'interface UsernameValidation pour rendre le code plus lisible R

--- server/app/routes/username-validator.ts
+++ server/app/routes/username-validator.ts
@@ -3,12 +3,17 @@ import "reflect-metadata";
 import { injectable } from "inversify";
 import { Message } from "../../../common/communication/message";
 
+interface UsernameValidation {
+    isUsernameValid: boolean, 
+    errorMessage: string
+};
+
 @injectable() 
 export class UsernameValidator {
 
     private usernames: string[] = [];
 
-    public addUser(username: string, req?: Request) : {isUsernameValid: boolean, errorMessage: string} {
+    public addUser(username: string, req?: Request) : UsernameValidation {
         let usernameValidation = this.validateUsername(username);
 
         if (usernameValidation.isUsernameValid) {
@@ -22,34 +27,33 @@ export class UsernameValidator {
         return ch.match(/^[a-z0-9]+$/i) !== null;
     }
 
-    private validateUsername(username: string) : {isUsernameValid: boolean, errorMessage: string} {
-        let errorMessage: string = "";
-        let isUsernameValid: boolean = true;
-        
+    private validateUsername(username: string) : UsernameValidation {
+        let validation : UsernameValidation = {
+            isUsernameValid: true,
+            errorMessage: ""
+        };
+
         if (username == undefined || username.length > 16 || username.length < 3) {
-            isUsernameValid = false;
-            errorMessage = "Le nom d'utilisateur doit contenir entre 3 et 16 charactères";
+            validation.isUsernameValid = false;
+            validation.errorMessage = "Le nom d'utilisateur doit contenir entre 3 et 16 charactères";
         } else if (!this.isAlphaNumeric(username)) {
-            isUsernameValid = false;
-            errorMessage = "Le nom d'utilisateur doit contenir que des lettres et des chiffres";
+            validation.isUsernameValid = false;
+            validation.errorMessage = "Le nom d'utilisateur doit contenir que des lettres et des chiffres";
         } else if (this.usernames.indexOf(username) !== -1) {
-            isUsernameValid = false;
-            errorMessage = "Le nom d'utilisateur existe déjà";
+            validation.isUsernameValid = false;
+            validation.errorMessage = "Le nom d'utilisateur existe déjà";
         }
 
-        return {
-            isUsernameValid: isUsernameValid,
-            errorMessage: errorMessage
-        };
+        return validation;
     }
 
     public getUsernameValidation(req: Request, res: Response, next: NextFunction): void {
         let username = req.params.username;
-        let usernameValidationMessage = this.addUser(username, req).errorMessage;
+        let usernameValidation = this.addUser(username, req);
     
         const message: Message = {
             title: "Username Validation",
-            body: usernameValidationMessage
+            body: usernameValidation.errorMessage
         }
         res.send(JSON.stringify(message));
     }
