88ae3684364b903de5e3328998875d5122bd4563
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-17 16:44:47 -0500

    VG VM : Retrait de l'import de NextFunction dans username-validator.ts R

--- server/app/routes/username-validator.ts
+++ server/app/routes/username-validator.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import "reflect-metadata";
 import { injectable } from "inversify";
 import { Message } from "../../../common/communication/message";
