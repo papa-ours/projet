b7679278dd5eac522fb64bf15ae86ea291eb5df7
mez <mezianechebili@gmail.com>
2019-01-17 16:33:17 -0500

    DP MC : ajout du routeur D

---
+++ client/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { AdminViewComponent } from '../admin-view/admin-view.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const ROUTES: Routes = [
+  { path: "admin", component: AdminViewComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  exports: [ 
+    RouterModule 
+  ]
+})
+
+export class AppRoutingModule { }
+
+
+
+
+
+
+/*import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+const ROUTES:Routes{
+
+}
+export class AppRoutingModule {
+
+ }*/
