3c562ea0910403ed52ce61c9916345ff1dd880aa
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-17 16:46:20 -0500

    VG VM : appel du serveur dans la methode deleteUsername du service DeleteUsername D

--- client/src/app/delete-username.service.ts
+++ client/src/app/delete-username.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DeleteUsernameService {
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-  constructor() { }
+import { Message } from "../../../common/communication/message";
+
+@Injectable()
+export class UsernameValidationService {
+
+    private readonly BASE_URL: string = "http://localhost:3000/deleteUser/";
+    public constructor(private http: HttpClient) { }
+
+    public deleteUsername(username: string): Observable<Message> {
+        return this.http.get<Message>(this.BASE_URL + username)
+            .pipe(catchError(this.handleError<Message>("deleteUsername"))
+        );
+    }
+
+    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+        return (error: Error): Observable<T> => of(result as T);
+    }
 }
