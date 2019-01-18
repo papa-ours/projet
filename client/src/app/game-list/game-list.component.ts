2251f95ea361429bf6f4895f688bc71cad89674e
Pascal Dally-Belanger <padal@gigl.polymtl.ca>
2019-01-18 12:09:48 -0500

    PDB: ajout de la liste des descriptions dans la liste de sparties D

--- client/src/app/game-list/game-list.component.ts
+++ client/src/app/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameSheetDescription } from "../../../../common/communication/game-description";
 
 @Component({
   selector: 'app-game-list',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
+
+  private description: GameSheetDescription = GameListComponent.getDesc();
+
+  private static getDesc: () => GameSheetDescription[] = () => {
+    const placeholder: GameSheetDescription = {
+      name: "Placeholder",
+      preview: "../../assets/preview-placeholder.png",
+      topScores1v1: ["3:51 Username", "3:51 Username", "3:51 Username"],
+      topScoresSolo: ["3:51 Username", "3:51 Username", "3:51 Username"],
+    };
+
+    return [placeholder, placeholder, placeholder];
+  }
 
   constructor() { }
 
--- client/src/app/game-sheet/game-sheet.component.ts
+++ client/src/app/game-sheet/game-sheet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GameSheetDescription } from "../../../../common/communication/game-description";
 
 @Component({
@@ -8,15 +8,9 @@ import { GameSheetDescription } from "../../../../common/communication/game-desc
 })
 export class GameSheetComponent implements OnInit {
 
-  private description: GameSheetDescription;
+  @Input() private description: GameSheetDescription;
 
   public constructor() {
-    this.description = {
-      name: "Placeholder",
-      preview: "../../assets/preview-placeholder.png",
-      topScores1v1: ["3:51 Username", "3:51 Username", "3:51 Username"],
-      topScoresSolo: ["3:51 Username", "3:51 Username", "3:51 Username"],
-    };
   }
 
   ngOnInit() {
