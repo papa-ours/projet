9296f98b70e2f894635d7f0e6111912ab94a78ee
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-20 17:02:13 -0500

    VG: ajout d'une methode pour lire l'image entrée D

--- client/src/app/simple-game-creation/simple-game-creation.component.ts
+++ client/src/app/simple-game-creation/simple-game-creation.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+interface FileReaderEventTarget extends EventTarget {
+  result: string;
+  files: FileList;
+}
+
+interface FileReaderEvent extends Event {
+  target: FileReaderEventTarget;
+}
+
 @Component({
   selector: 'app-simple-game-creation',
   templateUrl: './simple-game-creation.component.html',
   styleUrls: ['./simple-game-creation.component.css']
 })
-export class SimpleGameCreationComponent implements OnInit {
 
+export class SimpleGameCreationComponent implements OnInit {
+  
+  private imageData: Uint8Array;
   constructor() { }
 
   ngOnInit() {
   }
 
+  private fileEntered(event: FileReaderEvent): void {
+    const reader: FileReader = new FileReader();
+    reader.onload = (): void => {
+      const data: ArrayBuffer = reader.result;
+      this.imageData = new Uint8Array(data);
+    };
+
+    reader.readAsArrayBuffer(event.target.files[0]);
+  }
 }
