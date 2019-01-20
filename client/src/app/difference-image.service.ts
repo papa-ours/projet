cf49e2dbb21605e340cd840d147a2d8cc89b5459
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-20 18:56:47 -0500

    VG: Méthode postDifferenceImage pour envoyer les données au serveur D

--- client/src/app/difference-image.service.ts
+++ client/src/app/difference-image.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { of, Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DifferenceImageService {
+import { Message } from "../../../common/communication/message";
 
-  constructor() { }
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class UsernameValidationService {
+
+    private readonly URL: string = "http://localhost:3000/diffImg/";
+    public constructor(private http: HttpClient) { }
+
+    public postDifferenceImage(formData: FormData): Observable<Message> {
+        return this.http.post<Message>(this.URL, formData)
+            .pipe(catchError(this.handleError<Message>("postDifferenceImage")),
+        );
+    }
+
+    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+        return (error: Error): Observable<T> => of(result as T);
+    }
 }
