f596760333bb9da2e507dcfa1848ecb0cff0be78
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-21 11:01:14 -0500

    VG VM : Creation de la methode pour obtenir l'image de difference D

--- server/app/routes/difference-image-generator.ts
+++ server/app/routes/difference-image-generator.ts
@@ -10,12 +10,21 @@ export class DifferenceImageGenerator {
             JSON.parse("[" + req.body.originalImage + "]");
         const modifiedImageData: Uint8Array =
             JSON.parse("[" + req.body.modifiedImage + "]");
+
+        if (this.isBMP(originalImageData) && this.isBMP(modifiedImageData)) {
+            this.getDifferenceImage(originalImageData, modifiedImageData);
+        } 
     }
 
-    private isImageBMP(imageData: Uint8Array): boolean {
+    private isBMP(imageData: Uint8Array): boolean {
         const B_CODE: number = 66;
         const M_CODE: number = 77;
 
         return (imageData[0] === B_CODE && imageData[1] === M_CODE);
+    }
+
+    private getDifferenceImage(originalImage: Uint8Array, modifiedImage: Uint8Array): Uint8Array {
+        let differenceImageData: Uint8Array;
+        return differenceImageData;
     }
 }
