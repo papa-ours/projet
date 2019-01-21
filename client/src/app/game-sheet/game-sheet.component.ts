61321337f7fd3fe6174615b72eabe4646533790d
David <david.piche@polymtl.ca>
2019-01-21 13:00:40 -0500

    DP PDB: Mise en forme des boutons en vue admin D

--- client/src/app/game-sheet/game-sheet.component.ts
+++ client/src/app/game-sheet/game-sheet.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GameSheetDescription } from "../../../../common/communication/game-description";
 
 @Component({
@@ -9,11 +9,16 @@ import { GameSheetDescription } from "../../../../common/communication/game-desc
 export class GameSheetComponent implements OnInit {
 
   @Input() private description: GameSheetDescription;
+  @Input() private privilege: "admin" | "user";
+  @ViewChild('btn1') btn1: ElementRef;
+  @ViewChild('btn2') btn2: ElementRef;
 
   public constructor() {
   }
 
   ngOnInit() {
+    this.btn1.nativeElement.textContent = this.privilege === "user" ? "Jouer" : "Supprimer";
+    this.btn2.nativeElement.textContent = this.privilege === "user" ? "Créer" : "Réinitialiser";
   }
 
 }
