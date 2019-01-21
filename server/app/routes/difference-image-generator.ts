103c5327510aa3a27e375ad9e8395efd6fe69952
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-21 10:56:20 -0500

    VG VM : Methode generate et verification que l'image est un BMP dans le serivce Difference D

--- server/app/routes/difference-image-generator.ts
+++ server/app/routes/difference-image-generator.ts
@@ -1,9 +1,21 @@
-import { injectable } from "inversify";
-import { Message } from "../../../common/communication/message";
-
 import { Request, Response } from "express";
+import { injectable } from "inversify";
 import "reflect-metadata";
+import { Message } from "../../../common/communication/message";
 
 @injectable()
 export class DifferenceImageGenerator {
+    public generate(req: Request, res: Response): void {
+        const originalImageData: Uint8Array =
+            JSON.parse("[" + req.body.originalImage + "]");
+        const modifiedImageData: Uint8Array =
+            JSON.parse("[" + req.body.modifiedImage + "]");
+    }
+
+    private isImageBMP(imageData: Uint8Array): boolean {
+        const B_CODE: number = 66;
+        const M_CODE: number = 77;
+
+        return (imageData[0] === B_CODE && imageData[1] === M_CODE);
+    }
 }
