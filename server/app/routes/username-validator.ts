24a090d927a5a7531d13e7b85705bd4268cd6de9
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-22 18:54:32 -0500

    VG: Refractoring de la classe UsernameValidator pour l'utilisation des WebSockets R

--- server/app/routes/username-validator.ts
+++ server/app/routes/username-validator.ts
@@ -1,81 +1,38 @@
 import { injectable } from "inversify";
+import "reflect-metadata";
 import { Message } from "../../../common/communication/message";
 
-import { Request, Response } from "express";
-import "reflect-metadata";
-
-interface UsernameValidation {
-    isUsernameValid: boolean;
-    errorMessage: string;
-}
 
 @injectable()
 export class UsernameValidator {
-
     private readonly MAX_LENGTH: number = 16;
     private readonly MIN_LENGTH: number = 3;
-
-    public addUser(username: string, req?: Request): UsernameValidation {
-        const usernameValidation: UsernameValidation =
-            this.validateUsername(username);
-
-        if (usernameValidation.isUsernameValid) {
-           this.usernames.push(username);
-        }
-
-        return usernameValidation;
-    }
 
     private isAlphaNumeric(ch: string): boolean {
         return ch.match(/^[a-z0-9]+$/i) !== null;
     }
 
-    private validateUsername(username: string): UsernameValidation {
-        const validation: UsernameValidation = {
-            isUsernameValid: true,
-            errorMessage: "",
-        };
+    private validateUsername(username: string, users: string[]): string {
+        let messageBody: string = "";
 
         const BASE_MESSAGE: string = "Le nom d'utilisateur ";
         if (username === undefined || username.length > this.MAX_LENGTH || username.length < this.MIN_LENGTH) {
-            validation.isUsernameValid = false;
-            validation.errorMessage += BASE_MESSAGE.concat(`doit contenir entre ${this.MIN_LENGTH}
-                                         et ${this.MAX_LENGTH} charactères`);
+            messageBody = BASE_MESSAGE.concat(`doit contenir entre ${this.MIN_LENGTH} et ${this.MAX_LENGTH} charactères`);
         } else if (!this.isAlphaNumeric(username)) {
-            validation.isUsernameValid = false;
-            validation.errorMessage += BASE_MESSAGE.concat("doit contenir que des lettres et des chiffres");
-        } else if (this.usernames.indexOf(username) !== -1) {
-            validation.isUsernameValid = false;
-            validation.errorMessage += BASE_MESSAGE.concat("existe déjà");
+            messageBody = BASE_MESSAGE.concat("doit contenir que des lettres et des chiffres");
+        } else if (users.indexOf(username) !== -1) {
+            messageBody = BASE_MESSAGE.concat("existe déjà");
         }
 
-        return validation;
+        return messageBody;
     }
 
-    public getUsernameValidation(req: Request, res: Response): void {
-        const username: string = req.params.username;
-        const usernameValidation: UsernameValidation = this.addUser(username, req);
+    public getUsernameValidation(username: string, users: string[]): Message {
+        const usernameValidation: string = this.validateUsername(username, users);
 
-        const message: Message = {
+        return {
             title: "Username Validation",
-            body: usernameValidation.errorMessage,
+            body: usernameValidation,
         };
-        res.send(message);
-    }
-
-    public deleteUsername(req: Request, res: Response): void {
-        const username: string = req.params.username;
-        const index: number = this.usernames.indexOf(username);
-        let usernameDeleted: boolean = false;
-        if (index >= 0) {
-            usernameDeleted = true;
-            this.usernames.splice(index, 1);
-        }
-
-        const message: Message = {
-            title: "Username Deletion : " + username,
-            body: usernameDeleted.toString(),
-        };
-        res.send(JSON.stringify(message));
     }
 }
