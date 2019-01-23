d62d02ed9762ec0b48fca00efbafd15bd986145e
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-22 19:21:55 -0500

    VG: Retrait du code dans le AppComponent D

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import * as io from "socket.io-client";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
-    public readonly title: string = "LOG2990";
-    public message: string;
-    private socket: SocketIOClient.Socket;
-
-    public ngOnInit(): void {
-      this.socket = io.connect("http://localhost:3000");
-      this.socket.on("maximum", (m: string) => {
-        console.log(m);
-      });
-    }
+export class AppComponent {
 }
