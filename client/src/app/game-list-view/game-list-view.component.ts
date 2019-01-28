e0cc583fa89db0e1c6331809faf163712fca6cc0
David <david.piche@polymtl.ca>
2019-01-28 10:54:23 -0500

    DP:Ajout de game-list-view D

---
+++ client/src/app/game-list-view/game-list-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-list-view',
+  templateUrl: './game-list-view.component.html',
+  styleUrls: ['./game-list-view.component.css']
+})
+export class GameListViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
