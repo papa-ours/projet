9581f89527263073fc6c86c1539fae045d5ff8c6
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-28 12:46:29 -0500

    VG: Ajout de l'enum color D

--- server/app/services/difference-image-generator.service.ts
+++ server/app/services/difference-image-generator.service.ts
@@ -2,11 +2,13 @@ import { Request, Response } from "express";
 import { injectable } from "inversify";
 import "reflect-metadata";
 import { Message } from "../../../common/communication/message";
+import { Color } from "./enums";
 
 @injectable()
 export class DifferenceImageGenerator {
 
     private readonly PIXEL_LENGTH: number = 3;
+    private readonly IMAGE_WIDTH: number = 640;
 
     public generate(req: Request, res: Response): void {
         const originalImageData: Uint8Array =
@@ -40,25 +42,42 @@ export class DifferenceImageGenerator {
 
     private calculateDifferenceFromImages(originalImage: Uint8Array, modifiedImage: Uint8Array): number[] {
         const OFF_SET_LOCATION: number = 10;
-        const WHITE: number = 255;
-        const BLACK: number = 0;
         const OFF_SET_ORIGINAL_IMAGE: number = originalImage[OFF_SET_LOCATION];
         const differenceImage: number[] = Array.from(originalImage);
-
+        const differencesPosition: number[] = [];
         for ( let i: number = OFF_SET_ORIGINAL_IMAGE;
              i < originalImage.length ; i += this.PIXEL_LENGTH ) {
 
             const originalImagePixel: Uint8Array = originalImage.slice(i, i + this.PIXEL_LENGTH);
             const modifiedImagePixel:  Uint8Array = modifiedImage.slice(i, i + this.PIXEL_LENGTH);
-            const color: number = this.isPixelEqual(originalImagePixel, modifiedImagePixel) ? WHITE : BLACK;
+            const color: number = this.isPixelEqual(originalImagePixel, modifiedImagePixel) ? Color.White : Color.Black;
+            if (color === Color.Black) {
+                differencesPosition.push(i);
+            }
             differenceImage.splice(i, this.PIXEL_LENGTH, color, color, color);
         }
 
-        return differenceImage;
+        return this.augmentPixelsDifferences(differenceImage, differencesPosition);
     }
 
     private isPixelEqual(firstPixel: Uint8Array, secondPixel: Uint8Array): Boolean {
         return firstPixel.toString() === secondPixel.toString();
+    }
+
+    private augmentPixelsDifferences(differenceImage: number[], differencesPosition: number[]): number[] {
+
+        return differenceImage;
+    }
+
+    private resolveIndex(i: number, j: number): number {
+        return j * this.IMAGE_WIDTH + i;
+    }
+
+    private resolvePosition(index: number): {i: number, j: number} {
+        return {
+            i: Math.floor(index / this.IMAGE_WIDTH),
+            j: index % this.IMAGE_WIDTH,
+        }
     }
 
 }
