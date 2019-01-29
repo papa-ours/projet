b35d4526c0753b64b0dee925c4125b00861c17b9
Vincent Gagnon <vg_4_@hotmail.fr>
2019-01-29 10:23:16 -0500

    VG: Ajout des méthodes getNumberOfDifferences et findDifferences D

--- server/app/services/differences-finder.service.ts
+++ server/app/services/differences-finder.service.ts
@@ -4,4 +4,14 @@ import "reflect-metadata";
 @injectable()
 export class DifferencesFinderService {
 
+    public getNumberOfDifferences(imageData: number[]): number {
+        let differencesCount: number = 0;
+        this.findDifferences(imageData, differencesCount);
+
+        return differencesCount;
+    }
+
+    private findDifferences(data: number[], count: number): void {
+        
+    }
 }
