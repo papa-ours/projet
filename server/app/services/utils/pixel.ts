38ea66f49c80bc362fc04aceaf6d25e2cc19c6d6
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-01-30 14:21:43 -0500

    PDB: ajout de la classe Pixel D

---
+++ server/app/services/utils/pixel.ts
@@ -0,0 +1,22 @@
+import { readLittleEndianBytes, numberToLittleEndinanByteArray } from "./binary";
+
+export class Pixel {
+
+    public static readonly WHITE_PIXEL: Pixel = new Pixel(0xFFFFFF);
+    public static readonly BLACK_PIXEL: Pixel = new Pixel(0x000000);
+    public static readonly BYTES_PER_PIXEL: number = 3;
+
+    public constructor(private readonly rgb: number) {}
+
+    public static readPixelColor(array: Uint8Array, index: number): Pixel {
+        return new Pixel(readLittleEndianBytes(array, Pixel.BYTES_PER_PIXEL, index));
+    }
+
+    public toArray(): Uint8Array {
+        return numberToLittleEndinanByteArray(this.rgb, Pixel.BYTES_PER_PIXEL);
+	}
+	
+	public equals(other: Pixel): boolean {
+		return this.rgb === other.rgb;
+	}
+}
