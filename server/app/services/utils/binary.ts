fb79465b96d81d0e1492c00d77d864a26b90927b
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-01-30 14:13:26 -0500

    PDB: correction du fonctionnement des fonctions binaires B

--- server/app/services/utils/binary.ts
+++ server/app/services/utils/binary.ts
@@ -1,9 +1,9 @@
 export const NUMBER_OF_BITS_IN_A_BYTE = 8;
 
-export function readLittleEndianBytes(array: Uint8Array, index: number, byteCount: number): number {
+export function readLittleEndianBytes(array: Uint8Array, byteCount: number, index: number): number {
 	let val: number = 0;
 	for (let i: number = 0; i < byteCount; i++) {
-		val |= array[index + i] << NUMBER_OF_BITS_IN_A_BYTE * (byteCount - i);
+		val |= array[index + i] << NUMBER_OF_BITS_IN_A_BYTE * i;
 	}
 	return val;
 }
@@ -11,7 +11,8 @@ export function readLittleEndianBytes(array: Uint8Array, index: number, byteCoun
 export function numberToLittleEndinanByteArray(val: number, byteCount: number): Uint8Array {
 	const array: Uint8Array = new Uint8Array(byteCount);
 	for (let i: number = 0; i < byteCount; i++) {
-		array[i] = (val >> NUMBER_OF_BITS_IN_A_BYTE * (byteCount - i)) & 0xFF;
+		array[i] = val & 0xFF;
+		val >>= NUMBER_OF_BITS_IN_A_BYTE;
 	}
 	return array;
 }
