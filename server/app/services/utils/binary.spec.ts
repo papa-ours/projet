46a0575190784e8f0fba446a544750ea3f172298
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-01-30 13:10:44 -0500

    PDB: ajout des autres tests T

--- server/app/services/utils/binary.spec.ts
+++ server/app/services/utils/binary.spec.ts
@@ -5,19 +5,75 @@ describe("binary", () => {
 	describe("numberToLittleEndinanByteArray", () => {
 		it("should return an array of 0 if the number is 0", () => {
 			const val: number = 0;
-			const expected: Uint8Array = new Uint8Array(1);
-			expected[0] = 0;
+			const expected: Uint8Array = new Uint8Array([0]);
 			const result = numberToLittleEndinanByteArray(val, 1);
 			expect(result).to.deep.equals(expected);
 		});
+
+		it("should return an array containing 3, 2, 1 if the input is 0x010203", () => {
+			const byteCount: number = 3;
+			const val: number = 0x010203;
+			const expected: Uint8Array = new Uint8Array([3, 2, 1]);
+			const result = numberToLittleEndinanByteArray(val, byteCount);
+			expect(result).to.deep.equals(expected);
+		});
+
+		it("should return an array containing 4, 3, 2, 1 if the input is 0x01020304", () => {
+			const byteCount: number = 4;
+			const val: number = 0x01020304;
+			const expected: Uint8Array = new Uint8Array([4, 3, 2, 1]);
+			const result = numberToLittleEndinanByteArray(val, byteCount);
+			expect(result).to.deep.equals(expected);
+		});
 	});
-	
+
 	describe("readLittleEndianBytes", () => {
 		it("should return 0 if the array contains 0", () => {
 			const expected: number = 0;
-			const val: Uint8Array = new Uint8Array(1);
-			val[0] = 0;
+			const val: Uint8Array = new Uint8Array([0]);
 			const result = readLittleEndianBytes(val, 1, 0);
+			expect(result).to.equals(expected);
+		});
+
+		it("should return 0x030201 if the array contains 1, 2, 3", () => {
+			const byteCount: number = 3;
+			const expected: number = 0x030201;
+			const val: Uint8Array = new Uint8Array([1, 2, 3]);
+			const result = readLittleEndianBytes(val, byteCount, 0);
+			expect(result).to.equals(expected);
+		});
+
+		it("should return 0x04030201 if the array contains 1, 2, 3, 4", () => {
+			const byteCount: number = 4;
+			const expected: number = 0x04030201;
+			const val: Uint8Array = new Uint8Array([1, 2, 3, 4]);
+			const result = readLittleEndianBytes(val, byteCount, 0);
+			expect(result).to.equals(expected);
+		});
+
+		it("should not read bytes after byteCount", () => {
+			const byteCount: number = 4;
+			const expected: number = 0x04030201;
+			const val: Uint8Array = new Uint8Array([1, 2, 3, 4, 255, 255]);
+			const result = readLittleEndianBytes(val, byteCount, 0);
+			expect(result).to.equals(expected);
+		});
+
+		it("should not read bytes before offset", () => {
+			const byteCount: number = 4;
+			const offset: number = 2;
+			const expected: number = 0x04030201;
+			const val: Uint8Array = new Uint8Array([255, 255, 1, 2, 3, 4]);
+			const result = readLittleEndianBytes(val, byteCount, offset);
+			expect(result).to.equals(expected);
+		});
+
+		it("should not read bytes before offset or after byteCount + offset", () => {
+			const byteCount: number = 4;
+			const offset: number = 2;
+			const expected: number = 0x04030201;
+			const val: Uint8Array = new Uint8Array([255, 255, 1, 2, 3, 4, 255, 255]);
+			const result = readLittleEndianBytes(val, byteCount, offset);
 			expect(result).to.equals(expected);
 		});
 	});
