e199eabec311e33d480739bc15e7c78d6e8633b7
mez <mezianechebili@gmail.com>
2019-02-01 13:01:00 -0500

    MC : correction tslint dans les component ts R

--- client/src/app/game-list-view/game-list-view.component.ts
+++ client/src/app/game-list-view/game-list-view.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-game-list-view',
-  templateUrl: './game-list-view.component.html',
-  styleUrls: ['./game-list-view.component.css']
+  selector: "app-game-list-view",
+  templateUrl: "./game-list-view.component.html",
+  styleUrls: ["./game-list-view.component.css"],
 })
 export class GameListViewComponent implements OnInit {
 
   // @ts-ignore
   private username: string = "";
-  constructor(private route: ActivatedRoute) { }
+  public constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.username = params["username"];
-    })
+    });
   }
 
 }
--- client/src/app/game-list/game-list.component.ts
+++ client/src/app/game-list/game-list.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { GameSheetDescription } from "../../../../common/communication/game-description";
-import { GameListService } from "../game-list-getter.service"
-import { Privilege } from "../../../../common/communication/game-description";
+import { Component, Input, OnInit } from "@angular/core";
+import { GameSheetDescription, Privilege } from "../../../../common/communication/game-description";
+import { GameListService } from "../game-list-getter.service";
 
 @Component({
-  selector: 'app-game-list',
-  templateUrl: './game-list.component.html',
-  styleUrls: ['./game-list.component.css']
+  selector: "app-game-list",
+  templateUrl: "./game-list.component.html",
+  styleUrls: ["./game-list.component.css"],
 })
 export class GameListComponent implements OnInit {
-  
-  //@ts-ignore
+
+  // @ts-ignore
   private descriptions: GameSheetDescription[];
   @Input() private is2D: boolean;
-  //@ts-ignore
-  @Input() private privilege : Privilege;
-  constructor(private gameListService: GameListService) { }
+  // @ts-ignore
+  @Input() private privilege: Privilege;
+  public constructor(private gameListService: GameListService) { }
 
-  ngOnInit() {
-    this.gameListService.getGameList().subscribe(lists => this.descriptions = this.is2D ? lists.list2d : lists.list3d);
+  public ngOnInit(): void {
+    this.gameListService.getGameList().subscribe((lists) => this.descriptions = this.is2D ? lists.list2d : lists.list3d);
   }
 
 }
