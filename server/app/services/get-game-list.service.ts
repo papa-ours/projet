c0217f2d100063912b71bbf6d22d5e420180a82e
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-02 13:50:37 -0500

    VG: Utilisation de la base de données pour le get les gamesheets D

--- server/app/services/get-game-list.service.ts
+++ server/app/services/get-game-list.service.ts
@@ -1,8 +1,12 @@
-import { injectable, inject } from "inversify";
+import { Response } from "express";
+import { inject, injectable } from "inversify";
+import { MongooseDocument } from "mongoose";
 import "reflect-metadata";
-import { GameLists, GameSheetDescription } from "../../../common/communication/game-description";
+// import { GameLists, GameSheetDescription } from "../../../common/communication/game-description";
+import { GameLists, TopScore } from "../../../common/communication/game-description";
 import Types from "../types";
 import { DBConnectionService } from "./dbconnection.service";
+import { Message } from "../../../common/communication/message";
 
 @injectable()
 export class GetGameListService {
@@ -10,20 +14,27 @@ export class GetGameListService {
     public constructor(@inject(Types.DBConnectionService) private dbConnection: DBConnectionService) {
     }
 
-    public getGameList(): GameLists {
-        const placeholder: GameSheetDescription = {
-            name: "Placeholder",
-            preview: "../../assets/preview-placeholder.png",
-            topScores: [
-                { solo: "3:51 Solo", pvp: "3:51 1v1" },
-                { solo: "3:51 Solo", pvp: "3:51 1v1" },
-                { solo: "3:51 Solo", pvp: "3:51 1v1" },
-            ],
-        };
+    public getGameList(res: Response): void {
+        const gameList: GameLists = { list2d: [], list3d: [] };
 
-        return {
-            list2d: [placeholder, placeholder, placeholder],
-            list3d: [placeholder, placeholder],
-        };
+        this.dbConnection.connect()
+        .then(() => {
+            this.dbConnection.getGameSheets2D()
+                .then((gameSheets: MongooseDocument[]): void => {
+                    gameSheets.forEach((doc: MongooseDocument) => {
+                        gameList.list2d.push({
+                            name: doc.get("name", String),
+                            preview: doc.get("preview", String),
+                            topScores: doc.get("topScores", Array<TopScore>()),
+                        });
+                    });
+
+                    const message: Message = {
+                        title: "Game List",
+                        body: JSON.stringify(gameList),
+                    }
+                    res.send(message);
+                });
+        });
     }
 }
