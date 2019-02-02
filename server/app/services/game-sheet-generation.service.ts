3a6e4a4fbb95299c9e77704ddcb258e9fe3f6121
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-02 13:52:00 -0500

    VG: Utilisation de la BD pour save les gamesheets créée D

--- server/app/services/game-sheet-generation.service.ts
+++ server/app/services/game-sheet-generation.service.ts
@@ -1,20 +1,41 @@
 import { inject, injectable } from "inversify";
+import { DBConnectionService } from "./dbconnection.service";
 import { DifferenceImageGenerator } from "./difference-image-generator.service";
 import { DifferencesFinderService } from "./differences-finder.service";
 
 import "reflect-metadata";
+import { GameSheetDescription } from "../../../common/communication/game-description";
 import Types from "../types";
 import { BMPImage } from "./utils/bmp-image";
 
 @injectable()
 export class GameSheetGenerationService {
     public constructor(@inject(Types.DifferenceImageGenerator) private differenceImageGenerator: DifferenceImageGenerator,
-                       @inject(Types.DifferencesFinderService) private differencesFinder: DifferencesFinderService) {}
+                       @inject(Types.DifferencesFinderService) private differencesFinder: DifferencesFinderService,
+                       @inject(Types.DBConnectionService) private dbConnection: DBConnectionService) {}
 
-    public generateGameSheet(name: string, originalImageData: Uint8Array, modifiedImageData: Uint8Array): string {
+    public generateGameSheet(name: string, originalImageData: Uint8Array, modifiedImageData: Uint8Array): void {
         const differenceImage: BMPImage = this.differenceImageGenerator.generate(originalImageData, modifiedImageData) as BMPImage;
         this.differencesFinder.getNumberOfDifferences(differenceImage);
 
-        return differenceImage.toArray().toString();
+        this.saveGameSheet({
+            name: name,
+            preview: originalImageData.toString(),
+            topScores: [
+                { solo: "4:20 Vincent", pvp: "9:99 Vincent" },
+                { solo: "4:20 Vincent", pvp: "9:99 Vincent" },
+                { solo: "4:20 Vincent", pvp: "9:99 Vincent" },
+            ],
+        });
+    }
+
+    public saveGameSheet(gameSheetDescription: GameSheetDescription): void {
+        this.dbConnection.connect()
+            .then(() => {
+                this.dbConnection.saveGameSheet2D(gameSheetDescription);
+            })
+            .catch((err: Error) => {
+                console.error(err);
+            });
     }
 }
