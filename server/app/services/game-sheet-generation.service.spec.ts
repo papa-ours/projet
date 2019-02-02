c50405e4dc96fca58b9e3ee308e528287bfab1b3
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-02 16:45:11 -0500

    VG: LEs tests pour les messages d'erreur de la génération de ficher fonctionnent D

---
+++ server/app/services/game-sheet-generation.service.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import * as fs from "fs";
+import { DBConnectionService } from "./dbconnection.service";
+import { DifferenceImageGenerator } from "./difference-image-generator.service";
+import { DifferencesFinderService } from "./differences-finder.service";
+import { GameSheetGenerationService } from "./game-sheet-generation.service";
+
+describe.only("game sheet generation", () => {
+    let gameSheetGenerator: GameSheetGenerationService;
+
+    beforeEach(() => {
+        gameSheetGenerator = new GameSheetGenerationService(new DifferenceImageGenerator(),
+                                                            new DifferencesFinderService(),
+                                                            new DBConnectionService());
+    });
+
+    it("should return the correct error message if the images don't have 7 differences", () => {
+        let imageData: Uint8Array;
+        fs.readFile("../client/src/assets/img/dog.bmp", (err: NodeJS.ErrnoException, fileData: Buffer) => {
+            imageData = Uint8Array.from(fileData);
+            const result: string = gameSheetGenerator.generateGameSheet("", imageData, imageData).body;
+            expect(result).to.equals("Les images n'ont pas exactement 7 différences, la création a été annulée");
+        });
+    });
+
+    it("should return an empty body message if the images have 7 differences", () => {
+        const originalImageBuffer: Buffer = fs.readFileSync("../client/src/assets/img/dog.bmp");
+        const modifiedImageBuffer: Buffer = fs.readFileSync("../client/src/assets/img/dog_7_diff.bmp");
+
+        const originalImageData: Uint8Array = Uint8Array.from(originalImageBuffer);
+        const modifiedImageData: Uint8Array = Uint8Array.from(modifiedImageBuffer);
+
+        const result: string = gameSheetGenerator.generateGameSheet("", originalImageData, modifiedImageData).body;
+        expect(result).to.equals("");
+    });
+});
--- server/app/services/game-sheet-generation.service.ts
+++ server/app/services/game-sheet-generation.service.ts
@@ -4,9 +4,10 @@ import { DifferenceImageGenerator } from "./difference-image-generator.service";
 import { DifferencesFinderService } from "./differences-finder.service";
 
 import "reflect-metadata";
-import { GameSheetDescription } from "../../../common/communication/game-description";
+import { GameSheetDescription, TopScore } from "../../../common/communication/game-description";
 import Types from "../types";
 import { BMPImage } from "./utils/bmp-image";
+import { Message } from "../../../common/communication/message";
 
 @injectable()
 export class GameSheetGenerationService {
@@ -14,25 +15,47 @@ export class GameSheetGenerationService {
                        @inject(Types.DifferencesFinderService) private differencesFinder: DifferencesFinderService,
                        @inject(Types.DBConnectionService) private dbConnection: DBConnectionService) {}
 
-    public generateGameSheet(name: string, originalImageData: Uint8Array, modifiedImageData: Uint8Array): void {
+    public generateGameSheet(name: string, originalImageData: Uint8Array, modifiedImageData: Uint8Array): Message {
         const differenceImage: BMPImage = this.differenceImageGenerator.generate(originalImageData, modifiedImageData) as BMPImage;
-        this.differencesFinder.getNumberOfDifferences(differenceImage);
 
-        this.saveGameSheet({
+        const numberOfDifferences: number = this.differencesFinder.getNumberOfDifferences(differenceImage);
+        const REQUIRED_DIFFERENCES: number = 7;
+
+        const message: Message = {
+            title: "GameSheet Generation",
+            body: "",
+        };
+
+        if (numberOfDifferences !== REQUIRED_DIFFERENCES) {
+            message.body = "Les images n'ont pas exactement 7 différences, la création a été annulée";
+        } else {
+            this.createGameSheet(name, originalImageData);
+        }
+
+        return message;
+    }
+
+    private createGameSheet(name: string, originalImageData: Uint8Array): void {
+        const TOP_SCORES_LENGTH: number = 3;
+        const topScores: TopScore[] = new Array(TOP_SCORES_LENGTH).map(() => this.generateTopScore());
+
+        const gameSheet: GameSheetDescription = {
             name: name,
             preview: originalImageData.toString(),
-            topScores: [
-                { solo: "4:20 Vincent", pvp: "9:99 Vincent" },
-                { solo: "4:20 Vincent", pvp: "9:99 Vincent" },
-                { solo: "4:20 Vincent", pvp: "9:99 Vincent" },
-            ],
-        });
+            topScores: topScores,
+        };
+
+        this.saveGameSheet(gameSheet);
+    }
+
+    public generateTopScore(): TopScore {
+        return { solo: "", pvp: "" };
     }
 
     public saveGameSheet(gameSheetDescription: GameSheetDescription): void {
         this.dbConnection.connect()
             .then(() => {
-                this.dbConnection.saveGameSheet2D(gameSheetDescription);
+                // this.dbConnection.saveGameSheet2D(gameSheetDescription);
             })
             .catch((err: Error) => {
                 console.error(err);
