295753617f31eb8722aa519cbbcc4af3bbe11e00
mez <mezianechebili@gmail.com>
2019-02-01 19:20:35 -0500

    MC : ajout de test pour les requetes post de difference-image service T

--- client/src/app/difference-image.service.spec.ts
+++ client/src/app/difference-image.service.spec.ts
@@ -1,14 +1,60 @@
+import {HttpClientTestingModule , HttpTestingController, TestRequest} from "@angular/common/http/testing"
+import { TestBed } from "@angular/core/testing";
+import { Message } from "../../../common/communication/message";
 import { DifferenceImageService } from "./difference-image.service";
 
 describe("DifferenceImageService", () => {
-  let httpClientSpy: { get: jasmine.Spy };
-  let  deleteUsernameService: DifferenceImageService;
+  let  differenceImageService: DifferenceImageService;
+  let httpMock: HttpTestingController;
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [DifferenceImageService],
+    });
     // tslint:disable-next-line:no-any
-    deleteUsernameService = new DifferenceImageService( httpClientSpy as any) ;
+    differenceImageService = TestBed.get(DifferenceImageService);
+    httpMock = TestBed.get(HttpTestingController);
   });
+
   it("should be created", () => {
-    expect(deleteUsernameService).toBeTruthy();
+    expect(differenceImageService).toBeTruthy();
+  });
+
+  it("sould be a POST REQUEST", () => {
+    const response: Message = {
+      title: "this is the title",
+      body: "this is the body",
+    };
+    differenceImageService.postDifferenceImageData(new FormData()).subscribe(
+      (data: Message) => { expect(data).toBeDefined(); },
+    );
+    const request: TestRequest = httpMock.expectOne(`${differenceImageService.URL}`);
+    expect(request.request.method).toBe("POST");
+    request.flush(response);
+  });
+
+  it("sould return a proper message body on POST request", () => {
+    const response: Message = {
+      title: "this is the title",
+      body: "this is the body",
+    };
+    differenceImageService.postDifferenceImageData(new FormData()).subscribe(
+      (data: Message) => { expect(data.body).toBe(response.body); },
+   );
+    const request: TestRequest = httpMock.expectOne(`${differenceImageService.URL}`);
+    request.flush(response);
+    expect(request.request.method).toBe("POST");
+  });
+  it("sould return a proper message title on POST request", () => {
+    const response: Message = {
+      title: "this is the title",
+      body: "this is the body",
+    };
+    differenceImageService.postDifferenceImageData(new FormData()).subscribe(
+      (data: Message) => { expect(data.title).toBe(response.title); },
+   );
+    const request: TestRequest = httpMock.expectOne(`${differenceImageService.URL}`);
+    request.flush(response);
+    expect(request.request.method).toBe("POST");
   });
 });
--- client/src/app/difference-image.service.ts
+++ client/src/app/difference-image.service.ts
@@ -9,7 +9,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class DifferenceImageService {
 
-    private readonly URL: string = "http://localhost:3000/api/imagedifference";
+    public readonly URL: string = "http://localhost:3000/api/imagedifference";
     public constructor(private http: HttpClient) { }
 
     public postDifferenceImageData(formData: FormData): Observable<Message> {
