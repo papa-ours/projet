32225b62c27508933b9ca69d408e3a052dfb876c
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-02 13:48:30 -0500

    VG: Implémentation des méthodes d'accès et de save pour les gamesheet2D D

--- server/app/services/dbconnection.service.ts
+++ server/app/services/dbconnection.service.ts
@@ -1,18 +1,41 @@
 import { injectable } from "inversify";
 import * as mongoose from "mongoose";
 import "reflect-metadata";
+import { GameSheetDescription } from "../../../common/communication/game-description";
 
 @injectable()
 export class DBConnectionService {
     private readonly uri: string = "mongodb+srv://ving34:pass123@cluster0-m1gwf.mongodb.net/test?retryWrites=true";
+    private readonly gameSheetSchema: mongoose.Schema = new mongoose.Schema({
+        name: String,
+        preview: String,
+        topScores: Array,
+    });
+    public connected: boolean = false;
 
     public constructor() {
-        this.connect();
+        if (!mongoose.models.GameSheet2D) {
+            mongoose.model("GameSheet2D", this.gameSheetSchema);
+        }
     }
 
-    private connect(): void {
-        mongoose.connect(this.uri, {
+    public connect(): Promise<typeof mongoose> {
+        return mongoose.connect(this.uri, {
             useNewUrlParser: true,
         });
+    }
+
+    public async saveGameSheet2D(gameSheetDescription: GameSheetDescription): Promise<mongoose.Document> {
+        const gameSheet: mongoose.Document = new mongoose.models.GameSheet2D(gameSheetDescription);
+
+        return gameSheet.save();
+    }
+
+    public async getGameSheets2D(): Promise<mongoose.Document[]> {
+        return mongoose.models.GameSheet2D.find({}).exec();
+    }
+
+    public deleteAll(): void {
+        mongoose.models.GameSheet2D.deleteMany({}).then(() => console.log("deleted all"));
     }
 }
