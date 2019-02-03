5e44d776e08ed681f7a2655c43d47cb22c62abe7
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-03 13:50:45 -0500

    VG: Correction TSLint R

--- client/src/app/username-validation-service.service.spec.ts
+++ client/src/app/username-validation-service.service.spec.ts
@@ -1,11 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { UsernameValidationService } from './username-validation-service.service';
+import { UsernameValidationService } from "./username-validation-service.service";
 
-describe('UsernameValidationServiceService', () => {
+describe("UsernameValidationServiceService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
+  it("should be created", () => {
     const service: UsernameValidationService = TestBed.get(UsernameValidationService);
     expect(service).toBeTruthy();
   });
