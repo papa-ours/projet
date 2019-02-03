2ddf5d61c70aff7561202a6c27a046cca00b198d
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-03 13:42:58 -0500

    VG: Correction TSLint R

--- client/src/app/game-sheet/game-sheet.component.ts
+++ client/src/app/game-sheet/game-sheet.component.ts
@@ -1,27 +1,26 @@
-import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
 import { GameSheetDescription } from "../../../../common/communication/game-description";
 
 @Component({
-  selector: 'app-game-sheet',
-  templateUrl: './game-sheet.component.html',
-  styleUrls: ['./game-sheet.component.css']
+  selector: "app-game-sheet",
+  templateUrl: "./game-sheet.component.html",
+  styleUrls: ["./game-sheet.component.css"]
 })
 export class GameSheetComponent implements OnInit {
 
+  // @ts-ignore
   private medalColors: string[] = [
     "#FFD700",
     "#C0C0C0",
     "#CD7F32",
   ];
+  // @ts-ignore
   @Input() private description: GameSheetDescription;
   @Input() private privilege: "admin" | "user";
-  @ViewChild('btn1') btn1: ElementRef;
-  @ViewChild('btn2') btn2: ElementRef;
+  @ViewChild("btn1") private btn1: ElementRef;
+  @ViewChild("btn2") private btn2: ElementRef;
 
-  public constructor() {
-  }
-
-  ngOnInit() {
+  public ngOnInit(): void {
     this.btn1.nativeElement.textContent = this.privilege === "user" ? "Jouer" : "Supprimer";
     this.btn2.nativeElement.textContent = this.privilege === "user" ? "Créer" : "Réinitialiser";
   }
