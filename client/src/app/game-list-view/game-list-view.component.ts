72908243a808dfab855b8e1addc1db7241ca6e35
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-03 01:00:39 -0500

    VG: Ajout du GetGameListService dans GameListView D

--- client/src/app/game-list-view/game-list-view.component.ts
+++ client/src/app/game-list-view/game-list-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GameListService } from "../game-list-getter.service"
 
 @Component({
   selector: 'app-game-list-view',
@@ -10,12 +11,17 @@ export class GameListViewComponent implements OnInit {
 
   // @ts-ignore
   private username: string = "";
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private gameListService: GameListService) { }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.username = params["username"];
-    })
+    });
+
+    this.gameListService.getGameList().subscribe((lists) => {
+      console.log(lists);
+    });
   }
 
 }
