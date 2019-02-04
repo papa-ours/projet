30a6b8301ecd072a59ec26008b6940e5a11bb073
mez <mezianechebili@gmail.com>
2019-02-04 09:59:55 -0500

    DP PDB MC : fichier de depart pour la scene 3d D

--- client/src/app/scene3d/scene3d.component.spec.ts
+++ client/src/app/scene3d/scene3d.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Scene3dComponent } from './scene3d.component';
 
 describe('Scene3dComponent', () => {
-  let component: Scene3dComponent;
-  let fixture: ComponentFixture<Scene3dComponent>;
+    let component: Scene3dComponent;
+    let fixture: ComponentFixture<Scene3dComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ Scene3dComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [Scene3dComponent]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(Scene3dComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(Scene3dComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/scene3d/scene3d.component.ts
+++ client/src/app/scene3d/scene3d.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import * as THREE from "three";
 @Component({
-  selector: 'app-scene3d',
-  templateUrl: './scene3d.component.html',
-  styleUrls: ['./scene3d.component.css']
+    selector: "app-scene3d",
+    templateUrl: "./scene3d.component.html",
+    styleUrls: ["./scene3d.component.css"],
 })
 export class Scene3dComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    public constructor() { }
+    public ngOnInit(): void { }
 
 }
