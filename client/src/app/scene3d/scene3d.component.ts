aa4516f5472c9a68a5e287198b67b87463204bc9
mez <mezianechebili@gmail.com>
2019-02-04 11:22:50 -0500

    DP MC PDP : ajout de l'implementation de la scene 3d D

--- client/src/app/scene3d/scene3d.component.ts
+++ client/src/app/scene3d/scene3d.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from "@angular/core";
-import * as THREE from "three";
+import { AfterViewInit, Component, ElementRef, HostListener, Input, ViewChild } from "@angular/core";
+import { RenderService } from "./render.service";
+
 @Component({
     selector: "app-scene3d",
     templateUrl: "./scene3d.component.html",
     styleUrls: ["./scene3d.component.css"],
 })
-export class Scene3dComponent implements OnInit {
+export class Scene3dComponent implements AfterViewInit {
 
-    public constructor() { }
-    public ngOnInit(): void { }
+    public constructor(private renderService: RenderService) {
+    }
+
+    private get container(): HTMLDivElement {
+        return this.containerRef.nativeElement;
+    }
+
+    @ViewChild("container")
+    private containerRef: ElementRef;
+
+    @Input()
+    public rotationSpeedX: number = 0.005;
+
+    @Input()
+    public rotationSpeedY: number = 0.01;
+
+    @HostListener("window:resize", ["$event"])
+    public onResize(): void {
+        this.renderService.onResize();
+    }
+
+    public ngAfterViewInit(): void {
+        this.renderService.initialize(this.container, this.rotationSpeedX, this.rotationSpeedY);
+    }
 
 }
