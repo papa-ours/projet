866f959f6e9de20d3896592b406f2ef16450007c
David <david.piche@polymtl.ca>
2019-02-04 11:08:21 -0500

    DP: Création du service pour créer des objets D

---
+++ server/app/services/geometric-object-generator.service.ts
@@ -0,0 +1,22 @@
+import { injectable } from "inversify";
+import * as THREE from "three";
+
+@injectable()
+export class GeometricObjectGenerator {
+
+    private readonly COLOR_MAX_NUMBER = 16777215;
+
+    public generateRandomObject(){
+        
+    }
+
+    private generateRandomColor(): string{
+
+        return "";
+    }
+
+    private generateSize(){
+
+    }
+
+}
