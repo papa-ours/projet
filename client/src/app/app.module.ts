5d78dcc4f70a833b400ebd84adf6945b204bc41e
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-04 11:33:52 -0500

    VG: Import de FontAwesome D

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { AdminViewComponent } from "./admin-view/admin-view.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { AppComponent } from "./app.component";
+import { ChatMessagesComponent } from "./chat-messages/chat-messages.component";
 import { DifferenceImageService } from "./difference-image.service";
+import { GameImagesComponent } from "./game-images/game-images.component";
 import { GameListViewComponent } from "./game-list-view/game-list-view.component";
 import { GameListComponent } from "./game-list/game-list.component";
 import { GameSheetComponent } from "./game-sheet/game-sheet.component";
+import { GameplayViewComponent } from "./gameplay-view/gameplay-view.component";
 import { InitialViewComponent } from "./initial-view/initial-view.component";
 import { FileReaderUtil } from "./simple-game-creation/file-reader.util";
 import { SimpleGameCreationComponent } from "./simple-game-creation/simple-game-creation.component";
 import { UsernameValidationService } from "./username-validation-service.service";
-
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-import { GameplayViewComponent } from "./gameplay-view/gameplay-view.component";
-import { MessagesComponent } from './messages/messages.component';
-import { ChatMessagesComponent } from './chat-messages/chat-messages.component';
-import { GameImagesComponent } from './game-images/game-images.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
     declarations: [
@@ -29,7 +28,6 @@ import { GameImagesComponent } from './game-images/game-images.component';
         AdminViewComponent,
         SimpleGameCreationComponent,
         GameplayViewComponent,
-        MessagesComponent,
         ChatMessagesComponent,
         GameImagesComponent,
     ],
@@ -38,6 +36,7 @@ import { GameImagesComponent } from './game-images/game-images.component';
         HttpClientModule,
         FormsModule,
         AppRoutingModule,
+        FontAwesomeModule,
       ],
     providers: [
         UsernameValidationService,
