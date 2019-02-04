21905469fc0c689f7d3f986d4966ee477d4a214d
David <david.piche@polymtl.ca>
2019-02-04 11:11:25 -0500

    DP: Ajout de test pour geometric-object-generator D

---
+++ server/app/services/geometric-object-generator.service.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import * as fs from "fs";
+import { GeometricObjectGenerator } from "./geometric-object-generator.service";
+
+
+describe("game sheet generation", () => {
+    let geometricObjectGenerator: GeometricObjectGenerator;
+
+    beforeEach(() => {
+        geometricObjectGenerator = new GeometricObjectGenerator();
+    });
+
+    
+});
