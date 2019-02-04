b534e39362376b85bc0181ce705f6722a945a270
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-04 10:51:59 -0500

    VG VM: Création du component GameImages D

---
+++ client/src/app/game-images/game-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-images',
+  templateUrl: './game-images.component.html',
+  styleUrls: ['./game-images.component.css']
+})
+export class GameImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
