49823e23c23ecc30dc860498564c07088ca237f0
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-04 09:34:15 -0500

    VM: creation gameplay-view component D

---
+++ client/src/app/gameplay-view/gameplay-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gameplay-view',
+  templateUrl: './gameplay-view.component.html',
+  styleUrls: ['./gameplay-view.component.css']
+})
+export class GameplayViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
