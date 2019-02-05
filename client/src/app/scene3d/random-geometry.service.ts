0265c2881e7f529496ace15ebc68add15755cc40
unknown <meziane.chebili@polymtl.ca>
2019-02-05 17:10:10 -0500

    MC : correction tlslint dans random geometry service R

--- client/src/app/scene3d/random-geometry.service.ts
+++ client/src/app/scene3d/random-geometry.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
-import { Pyramid } from "./pyramid";
 @Injectable({
     providedIn: "root",
 })
 export class RandomGeometryService {
 
-    private functionList: Function [] = [];
+    private functionList: Function[] = [];
     public constructor() {
         this.functionList.push(this.createSphere);
         this.functionList.push(this.createCube);
@@ -15,20 +14,20 @@ export class RandomGeometryService {
         this.functionList.push(this.createPyramid);
     }
     private createSphere(size: number, material: THREE.Material): THREE.Mesh {
-        return new THREE.Mesh( new THREE.SphereGeometry(size, size, size), material);
+        return new THREE.Mesh(new THREE.SphereGeometry(size), material);
     }
     private createCube(size: number, material: THREE.Material): THREE.Mesh {
-        return  new THREE.Mesh( new THREE.BoxGeometry(size, size, size), material);
+        return new THREE.Mesh(new THREE.BoxGeometry(size, size, size), material);
     }
     private createCone(size: number, material: THREE.Material): THREE.Mesh {
-        return  new THREE.Mesh( new THREE.ConeGeometry(size, size, size), material);
+        return new THREE.Mesh(new THREE.ConeGeometry(size, size, size), material);
     }
     private createCylinder(size: number, material: THREE.Material): THREE.Mesh {
-        return  new THREE.Mesh( new THREE.CylinderGeometry(size, size, size), material);
+        return new THREE.Mesh(new THREE.CylinderGeometry(size, size, size), material);
     }
 
     public createPyramid(size: number, material: THREE.Material): THREE.Mesh {
-        return   new THREE.Mesh( new Pyramid(size).generate(), material);
+        return new THREE.Mesh(new THREE.TetrahedronGeometry(size, 0), material);
     }
     public create(size: number, material: THREE.Material): THREE.Mesh {
         const functionSelector: number = Math.floor(Math.random() * this.functionList.length);
