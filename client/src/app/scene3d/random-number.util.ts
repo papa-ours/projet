f77aca86d32e9c0561943b8b4890b54a8d50449c
unknown <meziane.chebili@polymtl.ca>
2019-02-05 13:25:05 -0500

    MC : ajout d'une fonction scale dans randomNumber D

--- client/src/app/scene3d/random-number.util.ts
+++ client/src/app/scene3d/random-number.util.ts
@@ -9,4 +9,10 @@ export class RandomNumber {
 
         return Math.floor(Math.random() * BASE_COLOR);
     }
+    public randomScale(numberToScale: number): number {
+        const MIN_FACTOR: number = 0.5;
+        const MAX_FACTOR: number = 1.5;
+
+        return this.randomInteger(numberToScale * MIN_FACTOR, numberToScale * MAX_FACTOR);
+    }
 }
