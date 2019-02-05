015b2a08a6ca7025be2182a9766648af7f5b4826
unknown <meziane.chebili@polymtl.ca>
2019-02-04 22:26:37 -0500

    MC : ajout de la pyramide au random geometry generator D

--- client/src/app/scene3d/random-geometry.service.ts
+++ client/src/app/scene3d/random-geometry.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { Pyramid } from "./pyramid";
 @Injectable({
     providedIn: "root",
 })
@@ -11,6 +12,7 @@ export class RandomGeometryService {
         this.functionList.push(this.createCube);
         this.functionList.push(this.createCone);
         this.functionList.push(this.createCylinder);
+        this.functionList.push(this.createPyramid);
     }
     private createSphere(size: number): THREE.SphereGeometry {
         return new THREE.SphereGeometry(size, size, size);
@@ -24,16 +26,13 @@ export class RandomGeometryService {
     private createCylinder(size: number): THREE.CylinderGeometry {
         return new THREE.CylinderGeometry(size, size, size);
     }
+
+    public createPyramid(size: number): THREE.Geometry {
+        return  new Pyramid(size).pyramid;
+    }
     public create(size: number): THREE.Geometry {
         const functionSelector: number = Math.floor(Math.random() * this.functionList.length);
 
         return this.functionList[functionSelector](size);
     }
-    /*
-    private createPyramid(): THREE.CylinderGeometry {
-        const radomsize: number = this.getRandomSize();
-        const a = THREE.Pyramid
-
-        return  new THREE.PiramyderGeometry(radomsize, radomsize, radomsize);
-    }*/
 }
