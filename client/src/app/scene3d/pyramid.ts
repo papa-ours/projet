296d30b5be2d46c3f4023ed1898fb57659f3fd01
unknown <meziane.chebili@polymtl.ca>
2019-02-04 22:25:29 -0500

    MC : creation de la pyramide a base triangulaire D

---
+++ client/src/app/scene3d/pyramid.ts
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+export class Pyramid {
+    private geometry: THREE.Geometry;
+    private scaleFactor: number;
+    public constructor(scale: number) {
+        this.scaleFactor = scale;
+        this.generate();
+    }
+    private createVertices(): void {
+        this.geometry.vertices = [
+            new THREE.Vector3( 0, 0, 0 ),
+            new THREE.Vector3( -1, 1, 0 ),
+            new THREE.Vector3( 1, 1, 0 ),
+            new THREE.Vector3( 0 , 0.5, 1 ),
+        ];
+    }
+    private createFaces(): void {
+        this.geometry.faces = [
+            new THREE.Face3( 0, 1, 2 ),
+            new THREE.Face3( 0, 1, 3 ),
+            new THREE.Face3( 0, 1, 3 ),
+            new THREE.Face3( 1, 2, 3 ),
+        ];
+    }
+    private scale(): void {
+        const transformation: THREE.Matrix4 = new THREE.Matrix4().makeScale( this.scaleFactor, this.scaleFactor, this.scaleFactor );
+        this.geometry.applyMatrix( transformation );
+    }
+    private generate(): void {
+        this.geometry = new THREE.Geometry();
+        this.createVertices();
+        this.createFaces();
+        this.scale();
+    }
+    public get pyramid(): THREE.Geometry {
+        return this.geometry;
+    }
+}
