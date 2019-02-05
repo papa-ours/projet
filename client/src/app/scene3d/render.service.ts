4dd6b8c5477d9dce1fb095bddd539d43161a3a39
mez <mezianechebili@gmail.com>
2019-02-04 19:36:25 -0500

    MC : integration du randomgeometry generator service au renderer D

--- client/src/app/scene3d/render.service.ts
+++ client/src/app/scene3d/render.service.ts
@@ -1,112 +1,118 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { RandomGeometryService } from "./random-geometry.service";
+
 // import Stats = require('stats.js');
 
 @Injectable()
 export class RenderService {
 
-  private container: HTMLDivElement;
+    private container: HTMLDivElement;
 
-  private camera: THREE.PerspectiveCamera;
+    private camera: THREE.PerspectiveCamera;
 
-  // private stats: Stats;
+    private shapes: THREE.Mesh[] = [];
 
-  private cube: THREE.Mesh;
+    private renderer: THREE.WebGLRenderer;
 
-  private renderer: THREE.WebGLRenderer;
+    private scene: THREE.Scene;
 
-  private scene: THREE.Scene;
+    private cameraZ: number = 400;
 
-  private cameraZ: number = 400;
+    private fieldOfView: number = 70;
 
-  private fieldOfView: number = 70;
+    private nearClippingPane: number = 1;
 
-  private nearClippingPane: number = 1;
+    private farClippingPane: number = 1000;
 
-  private farClippingPane: number = 1000;
+    public rotationSpeedX: number = 0.005;
 
-  public rotationSpeedX: number = 0.005;
+    public rotationSpeedY: number = 0.01;
 
-  public rotationSpeedY: number = 0.01;
+    private readonly GEOMETRY_SIZE: number = 65;
 
-  private animateCube(): void {
-    this.cube.rotation.x += this.rotationSpeedX;
-    this.cube.rotation.y += this.rotationSpeedY;
-  }
+    public constructor(private randomGeometryService: RandomGeometryService) {}
+    private animateShapes(): void {
+        for (const shape of this.shapes) {
+            shape.rotation.x += this.rotationSpeedX;
+            shape.rotation.y += this.rotationSpeedY;
+        }
+    }
+    public createRandomShape(): void {
+        const MAX_SHAPE: number = 1;
+        for (let i: number = 0; i < MAX_SHAPE; i++) {
+            const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: this.getRandomColor(), wireframe: true });
+            const randomShape: THREE.Mesh = new THREE.Mesh(this.randomGeometryService.create(this.getRandomSize()), material);
+            this.shapes.push(randomShape);
+            this.scene.add(randomShape);
+        }
+    }
+    // TODO : devrais etre dans un autre fichier
+    private getRandomInteger(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min)  + min ) ;
+    }
+    // TODO : devrais etre dans un autre fichier
+    private getRandomSize(): number {
+        const MIN_FACTOR: number = 0.5;
+        const MAX_FACTOR: number = 1.5;
 
-  private createCube(): void {
-    const GEOMETRY_SIZE: number = 200;
-    const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(GEOMETRY_SIZE, GEOMETRY_SIZE, GEOMETRY_SIZE);
+        return  this.getRandomInteger(MIN_FACTOR * this.GEOMETRY_SIZE, MAX_FACTOR * this.GEOMETRY_SIZE);
+    }
+    // TODO : devrais etre dans une autre classe
+    private getRandomColor(): number {
+        const BASE_COLOR: number = 0xFFFFFF;
 
-    for (let i = 0; i < geometry.faces.length; i += 2) {
-      const hex = Math.random() * 0xffffff;
-      geometry.faces[i].color.setHex(hex);
-      geometry.faces[i + 1].color.setHex(hex);
+        return Math.floor(Math.random() * BASE_COLOR);
+    }
+    private createScene(): void {
+        /* Scene */
+        this.scene = new THREE.Scene();
+
+        /* Camera */
+        const aspectRatio: number = this.getAspectRatio();
+        this.camera = new THREE.PerspectiveCamera(
+            this.fieldOfView,
+            aspectRatio,
+            this.nearClippingPane,
+            this.farClippingPane,
+        );
+        this.camera.position.z = this.cameraZ;
     }
 
-    const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors, overdraw: 0.5 });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
-  }
+    private getAspectRatio(): number {
+        return this.container.clientWidth / this.container.clientHeight;
+    }
 
-  private createScene(): void {
-    /* Scene */
-    this.scene = new THREE.Scene();
+    private startRenderingLoop(): void {
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setPixelRatio(devicePixelRatio);
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
 
-    /* Camera */
-    const aspectRatio = this.getAspectRatio();
-    this.camera = new THREE.PerspectiveCamera(
-      this.fieldOfView,
-      aspectRatio,
-      this.nearClippingPane,
-      this.farClippingPane
-    );
-    this.camera.position.z = this.cameraZ;
-  }
+        this.container.appendChild(this.renderer.domElement);
+        this.render();
+    }
 
-  private getAspectRatio(): number {
-    return this.container.clientWidth / this.container.clientHeight;
-  }
+    private render(): void {
+        requestAnimationFrame(() => this.render());
+        this.animateShapes();
 
-  private startRenderingLoop(): void {
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setPixelRatio(devicePixelRatio);
-    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+        this.renderer.render(this.scene, this.camera);
+    }
 
-    this.container.appendChild(this.renderer.domElement);
-    this.render();
-  }
+    public onResize(): void {
+        this.camera.aspect = this.getAspectRatio();
+        this.camera.updateProjectionMatrix();
 
-  private render(): void {
-    requestAnimationFrame(() => this.render());
-    this.animateCube();
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+    }
 
-    this.renderer.render(this.scene, this.camera);
-    // this.stats.update();
-  }
+    public initialize(container: HTMLDivElement, rotationX: number, rotationY: number): void {
+        this.container = container;
+        this.rotationSpeedX = rotationX;
+        this.rotationSpeedY = rotationY;
 
-  private initStats(): void {
-    // this.stats = new Stats();
-    // this.stats.dom.style.position = 'absolute';
-
-    // this.container.appendChild(this.stats.dom);
-  }
-
-  public onResize(): void {
-    this.camera.aspect = this.getAspectRatio();
-    this.camera.updateProjectionMatrix();
-
-    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-  }
-
-  public initialize(container: HTMLDivElement, rotationX: number, rotationY: number): void {
-    this.container = container;
-    this.rotationSpeedX = rotationX;
-    this.rotationSpeedY = rotationY;
-
-    this.createScene();
-    this.createCube();
-    this.initStats();
-    this.startRenderingLoop();
-  }
+        this.createScene();
+        this.createRandomShape();
+        this.startRenderingLoop();
+    }
 }
