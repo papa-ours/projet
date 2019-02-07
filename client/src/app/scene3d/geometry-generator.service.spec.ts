9b0a2f157501cd3ba7f3a04e65e7cfe0831d4ca0
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-07 18:42:41 -0500

    MC : test ajout de tests pour create sphere T

--- client/src/app/scene3d/geometry-generator.service.spec.ts
+++ client/src/app/scene3d/geometry-generator.service.spec.ts
@@ -25,9 +25,19 @@ describe("GeometryGeneratorService", () => {
         const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({color: 0xFF4256});
         expect(geometryGeneratorService.createCube(size, material).material).toBe(material);
     });
-    it("should be the same size as expected in dependency on create cube", () => {
+    /*it("should be the same size as expected in dependency on create cube", () => {
         const size: number = 100;
         const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial();
-        expect(geometryGeneratorService.createCube(size, material).geometry).toBe(material);
+        expect(geometryGeneratorService.createCube(size, material).geometry).toBe();
+    });*/
+    it("should return a cube geometry on create sphere", () => {
+        const size: number = 100;
+        const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial();
+        expect(geometryGeneratorService.createSphere(size, material).geometry.type).toBe(new THREE.SphereGeometry().type);
+    });
+    it("should be the same material as expected in dependency on create shpere", () => {
+        const size: number = 100;
+        const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({color: 0xFF4256});
+        expect(geometryGeneratorService.createSphere(size, material).material).toBe(material);
     });
 });
