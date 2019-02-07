e37085ebb7b0c537f38f62ef8251e4f3163650c7
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-07 17:19:16 -0500

    VG: Implémentation du controller de GetGameController D

--- server/app/controllers/get-game.controller.ts
+++ server/app/controllers/get-game.controller.ts
@@ -1,17 +1,25 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import { Message, MessageType } from "../../../common/communication/message";
+import { GetGameService } from "../services/get-game.service";
+import Types from "../types";
 
 @injectable()
 export class GetGameController {
 
-    public constructor() { }
+    public constructor(@inject(Types.GetGameService) private getGameService: GetGameService) { }
 
     public get router(): Router {
         const router: Router = Router();
 
         router.get("/:id",
                    (req: Request, res: Response, next: NextFunction) => {
+                        const message: Message = {
+                            type: MessageType.GAME_SHEET_GENERATION,
+                            body: JSON.stringify(this.getGameService.getGame(req.params.id)),
+                        };
 
+                        res.send(message);
                    });
 
         return router;
