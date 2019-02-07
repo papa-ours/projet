4df338cf58a596b14f6ea0ee3d3a38678d3a14e1
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-07 17:43:44 -0500

    VM: modification methode GetgameplayImages D

--- client/src/app/gameplay-view/gameplay-view.component.ts
+++ client/src/app/gameplay-view/gameplay-view.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { faHourglassHalf, IconDefinition } from "@fortawesome/free-solid-svg-icons";
-import { GameplayService } from "../gameplay.service";
 import { Message } from "../../../../common/communication/message";
+import { GameplayService } from "../gameplay.service";
 
 @Component({
   selector: "app-gameplay-view",
@@ -13,18 +13,34 @@ export class GameplayViewComponent implements OnInit {
 
     public hourglassIcon: IconDefinition = faHourglassHalf;
     private id: string;
+    public images: string[];
 
     public constructor(private route: ActivatedRoute, private gameplayService: GameplayService) { }
 
     public ngOnInit(): void {
         this.route.params.subscribe((params: Params) => {
             this.id = params["id"];
+            this.getGameplayImages();
         });
     }
 
-    private getGameplayImages(id: string): void {
-        this.gameplayService.getGameplayImages(id).subscribe((message: Message) => {
-            console.log(message);
+    private getGameplayImages(): void {
+        this.gameplayService.getGameplayImages(this.id).subscribe((message: Message) => {
+            const images: string[] = JSON.parse(message.body);
+            if (images.length) {
+                this.encodeImage(message.body);
+                this.images = images.map((imageData: string) => {
+                    return this.encodeImage(imageData);
+                });
+            }
+
         });
+    }
+
+    private encodeImage(imageData: string): string {
+        const numberData: number[] = imageData.split(",").map(Number);
+        const encodedString: string[] = numberData.map((val: number) => String.fromCharCode(val));
+
+        return "data:image/bmp;base64," + btoa(encodedString.join(""));
     }
 }
