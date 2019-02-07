bdc180966161b72b8079f79c2cd3557a43578b1a
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-07 14:55:28 -0500

    VM: correction tslint game-images.component.ts D

--- client/src/app/game-images/game-images.component.ts
+++ client/src/app/game-images/game-images.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-game-images',
-  templateUrl: './game-images.component.html',
-  styleUrls: ['./game-images.component.css']
+  selector: "app-game-images",
+  templateUrl: "./game-images.component.html",
+  styleUrls: ["./game-images.component.css"],
 })
 export class GameImagesComponent implements OnInit {
 
