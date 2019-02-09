49545826cc5b5de07d6fd3ac12385191d0e5973c
Vincent <vg_4_@hotmail.fr>
2019-02-09 15:36:06 -0500

    VG: Obtention des coordonnées x, y du click D

--- client/src/app/game-image/game-image.component.ts
+++ client/src/app/game-image/game-image.component.ts
@@ -6,11 +6,19 @@ import { Component, ElementRef, HostListener, Input, ViewChild } from "@angular/
   styleUrls: ["./game-image.component.css"],
 })
 export class GameImageComponent {
+    private readonly IMAGE_WIDTH: number = 640;
+    private readonly HTML_IMAGE_WIDTH: number = 500;
     @Input() public source: string;
-    //@ts-ignore
     @ViewChild("image") private imageElement: ElementRef;
 
     // tslint:disable-next-line:no-any
     @HostListener("click", ["$event"]) public checkDifference(event: any): void {
+        const imageRectangle: DOMRect = this.imageElement.nativeElement.getBoundingClientRect();
+        const x: number = this.mapValue(event.x - imageRectangle.left);
+        const y: number = this.mapValue(event.y - imageRectangle.top);
+    }
+
+    private mapValue(val: number): number {
+        return Math.floor(this.IMAGE_WIDTH  / this.HTML_IMAGE_WIDTH * val);
     }
 }
