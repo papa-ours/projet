907b39ef291c241548a2aee09d78d395d960a3a5
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-09 14:30:59 -0500

    MC : ajout d'une fonction pour generer un random float D

--- client/src/app/scene3d/random-number.util.ts
+++ client/src/app/scene3d/random-number.util.ts
@@ -4,6 +4,9 @@ export class RandomNumber {
     public randomInteger(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min) + min);
     }
+    public randomFloat(min: number, max: number): number {
+        return Math.random() * (max - min) + min;
+    }
     public randomColor(): number {
         const BASE_COLOR: number = 0xFFFFFF;
 
