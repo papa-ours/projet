1f914f6f43530edcd067c19d5394f9d766b301c3
Vincent <vg_4_@hotmail.fr>
2019-02-09 14:42:42 -0500

    VG: Création du component GameImageComponent D

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { AppComponent } from "./app.component";
 import { ChatMessagesComponent } from "./chat-messages/chat-messages.component";
 import { DifferenceImageService } from "./difference-image.service";
-import { GameImagesComponent } from "./game-images/game-images.component";
+import { GameImageComponent } from "./game-image/game-image.component";
 import { GameListViewComponent } from "./game-list-view/game-list-view.component";
 import { GameListComponent } from "./game-list/game-list.component";
 import { GameSheetComponent } from "./game-sheet/game-sheet.component";
@@ -12,6 +12,7 @@ import { InitialViewComponent } from "./initial-view/initial-view.component";
 import { FileReaderUtil } from "./simple-game-creation/file-reader.util";
 import { SimpleGameCreationComponent } from "./simple-game-creation/simple-game-creation.component";
 import { UsernameValidationService } from "./username-validation-service.service";
+
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
@@ -29,7 +30,7 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
         SimpleGameCreationComponent,
         GameplayViewComponent,
         ChatMessagesComponent,
-        GameImagesComponent,
+        GameImageComponent,
     ],
     imports: [
         BrowserModule,
---
+++ client/src/app/game-image/game-image.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-game-image",
+  templateUrl: "./game-image.component.html",
+  styleUrls: ["./game-image.component.css"],
+})
+export class GameImageComponent implements OnInit {
+    public constructor() { }
+    public ngOnInit() {
+    }
+
+}
