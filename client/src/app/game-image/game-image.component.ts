804d8c92fcd0a5098b33306172e67af1692c7030
Vincent <vg_4_@hotmail.fr>
2019-02-09 14:44:57 -0500

    VG: Ajout d'un attribut source D

--- client/src/app/game-image/game-image.component.ts
+++ client/src/app/game-image/game-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-game-image",
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./game-image.component.css"],
 })
 export class GameImageComponent implements OnInit {
+    @Input() public source: string;
     public constructor() { }
     public ngOnInit() {
     }
