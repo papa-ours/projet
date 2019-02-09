1542f9612dc5bbb53b40e82ed56bade0a693f001
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-09 16:23:22 -0500

    MC : modification des zones de la skybox D

--- client/src/app/scene3d/skybox.ts
+++ client/src/app/scene3d/skybox.ts
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
 // tslint:disable-next-line:no-magic-numbers
-export const SKYBOX_MIN: THREE.Vector3 = new THREE.Vector3(-500, -500, -100);
+export const SKYBOX_MIN: THREE.Vector3 = new THREE.Vector3(-1000, -1000, -500);
 // tslint:disable-next-line:no-magic-numbers
-export const SKYBOX_MAX: THREE.Vector3 = new THREE.Vector3(500, 500, -1100);
+export const SKYBOX_MAX: THREE.Vector3 = new THREE.Vector3(1000, 1000, -5000);
