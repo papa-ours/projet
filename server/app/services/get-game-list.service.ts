a7a2feb9be236c63550c46eb65f2823521f8c0ee
Vincent Gagnon <vg_4_@hotmail.fr>
2019-02-09 11:05:37 -0500

    VG: Utilisation de la nouvelle méthode pour les GameSheetDescriptions D

--- server/app/services/get-game-list.service.ts
+++ server/app/services/get-game-list.service.ts
@@ -1,15 +1,21 @@
 import { Response } from "express";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import "reflect-metadata";
 import { GameLists} from "../../../common/communication/game-description";
 import { Message, MessageType } from "../../../common/communication/message";
+import Types from "../types";
+import { GetGameService } from "./get-game.service";
 
 @injectable()
 export class GetGameListService {
 
+    public constructor(@inject(Types.GetGameService) private getGameService: GetGameService) {
+    }
+
     public getGameList(res: Response): void {
         const gameList: GameLists = { list2d: [], list3d: [] };
 
+        gameList.list2d = this.getGameService.getGameDescriptions();
         const message: Message = {
             type: MessageType.USERNAME_VALIDATION,
             body: JSON.stringify(gameList),
