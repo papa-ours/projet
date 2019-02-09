b1ad8f6bfe5cd52d2f2d69cfd136d4a03c07349d
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-09 14:25:52 -0500

    MC : cretion d'une skybox pour limiter la zone d'apparation des formes geometrique D

---
+++ client/src/app/scene3d/skybox.ts
@@ -0,0 +1,6 @@
+import * as THREE from "three";
+
+// tslint:disable-next-line:no-magic-numbers
+export const SKYBOX_MIN: THREE.Vector3 = new THREE.Vector3(-500, -500, -100);
+// tslint:disable-next-line:no-magic-numbers
+export const SKYBOX_MAX: THREE.Vector3 = new THREE.Vector3(500, 500, -1100);
