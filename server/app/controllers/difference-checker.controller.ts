94ec48db5f25adb3e43af15335ce9b21aaffdf1d
Vincent <vg_4_@hotmail.fr>
2019-02-09 23:20:36 -0500

    VG: Appelle de la méthode isPositionDifference du DifferenceCheckerService D

--- server/app/controllers/difference-checker.controller.ts
+++ server/app/controllers/difference-checker.controller.ts
@@ -1,18 +1,26 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { Message, MessageType } from "../../../common/communication/message";
+import { DifferenceCheckerService } from "../services/difference-checker.service";
+import Types from "../types";
 
 @injectable()
 export class DifferenceCheckerController {
+
+    public constructor( @inject(Types.DifferenceCheckerService) private differenceChecker: DifferenceCheckerService,
+                        ) {}
 
     public get router(): Router {
         const router: Router = Router();
 
         router.get("/:id/:x/:y", (req: Request, res: Response, next: NextFunction) => {
+            const x: number = parseInt(req.params.x, 10);
+            const y: number = parseInt(req.params.y, 10);
+            const id: string = req.params.id;
 
             const message: Message = {
                 type: MessageType.GAME_SHEET_GENERATION,
-                body: false.toString(),
+                body: this.differenceChecker.isPositionDifference(x, y, id).toString(),
             };
 
             res.send(message);
