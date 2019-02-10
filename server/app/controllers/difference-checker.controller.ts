14469602c2b5f1ae45b6975ba38cfd1d2e240d67
Vincent <vg_4_@hotmail.fr>
2019-02-09 22:42:26 -0500

    VG: Envoie d'un message dans la route D

--- server/app/controllers/difference-checker.controller.ts
+++ server/app/controllers/difference-checker.controller.ts
@@ -1,10 +1,23 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { injectable } from "inversify";
+import { Message, MessageType } from "../../../common/communication/message";
 
 @injectable()
 export class DifferenceCheckerController {
 
     public get router(): Router {
-        return Router();
+        const router: Router = Router();
+
+        router.get("/:id/:x/:y", (req: Request, res: Response, next: NextFunction) => {
+
+            const message: Message = {
+                type: MessageType.GAME_SHEET_GENERATION,
+                body: false.toString(),
+            };
+
+            res.send(message);
+        });
+
+        return router;
     }
 }
