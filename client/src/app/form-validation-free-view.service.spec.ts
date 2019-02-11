3951e48ef7bc8e4c9a06bd268a50fb951b6107be
David <david.piche@polymtl.ca>
2019-02-11 08:10:37 -0500

    DP: Ajout methode de validation de taille de nom D

--- client/src/app/form-validation-free-view.service.spec.ts
+++ client/src/app/form-validation-free-view.service.spec.ts
@@ -3,10 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { FormValidationFreeViewService } from './form-validation-free-view.service';
 
 describe('FormValidationFreeViewService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    const formValidationFreeViewService: FormValidationFreeViewService = new FormValidationFreeViewService();
 
-  it('should be created', () => {
-    const service: FormValidationFreeViewService = TestBed.get(FormValidationFreeViewService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: FormValidationFreeViewService = TestBed.get(FormValidationFreeViewService);
+        expect(service).toBeTruthy();
+    });
+    it("should return false with an empty name ", () => {
+        const name: string = "";
+        expect(formValidationFreeViewService.isFormValid(name)).toBeFalsy();
+    });
 });
--- client/src/app/form-validation-free-view.service.ts
+++ client/src/app/form-validation-free-view.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FormValidationFreeViewService {
+    private readonly NAME_MIN_SIZE: number = 5;
+    private readonly NAME_MAX_SIZE: number = 15;
 
-  constructor() { }
+    private validateName(name: string): boolean {
+        return  name !== undefined &&
+                name.length >= this.NAME_MIN_SIZE &&
+                name.length <= this.NAME_MAX_SIZE;
+    }
+
+    public isFormValid(name: string): boolean {
+        return this.validateName(name);
+    }
+    constructor() { }
 }
