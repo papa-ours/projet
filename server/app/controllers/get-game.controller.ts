9b2d9a6913ae94306e74ded232118a803a5345e5
Vincent <vg_4_@hotmail.fr>
2019-02-10 19:06:42 -0500

    VG: Le controller utilise la nouvelle version des Games D

--- server/app/controllers/get-game.controller.ts
+++ server/app/controllers/get-game.controller.ts
@@ -32,7 +32,7 @@ export class GetGameController {
                     (req: Request, res: Response, next: NextFunction) => {
                             const game: Game | undefined = this.getGameService.getGame(req.params.id);
                             const imageData: string = game ?
-                                                        game.preview :
+                                                        game.originalImage.toArray().toString() :
                                                         "";
                             const message: Message = {
                                 type: MessageType.GAME_SHEET_GENERATION,
@@ -46,7 +46,7 @@ export class GetGameController {
                     (req: Request, res: Response, next: NextFunction) => {
                             const game: Game | undefined = this.getGameService.getGame(req.params.id);
                             const imageData: string = game ?
-                                                        game.modifiedImage :
+                                                        game.modifiedImage.toArray().toString() :
                                                         "";
                             const message: Message = {
                                 type: MessageType.GAME_SHEET_GENERATION,
