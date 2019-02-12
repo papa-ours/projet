a5921b5a846091faae1d351cad73f0161ea1a43d
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-12 10:02:38 -0500

    MC : la scene prend les donnes du serveur pour ce generer  R

--- client/src/app/scene3d/scene-generator.service.ts
+++ client/src/app/scene3d/scene-generator.service.ts
@@ -1,39 +1,36 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { GeometryMessage } from "../../../../common/communication/geometryMessage";
 import { RandomGeometryService } from "./random-geometry.service";
-import { RandomNumber } from "./random-number.util";
 
 @Injectable({
     providedIn: "root",
 })
 export class SceneGeneratorService {
     private backgroundColor: number = 0x515151;
-    private randomNumber: RandomNumber = new RandomNumber();
-    private GEOMETRY_BASE_SIZE: number = 65;
-
+    private scene: THREE.Scene = new THREE.Scene();
     public constructor(private randomGeometryService: RandomGeometryService) { }
 
-    private changeBackgroundScene(scene: THREE.Scene): void {
-        scene.background = new THREE.Color(this.backgroundColor);
+    private changeBackgroundScene(): void {
+        this.scene.background = new THREE.Color(this.backgroundColor);
     }
-    private addGeometry(numberOfShapes: number, scene: THREE.Scene ): void {
-        for (let i: number = 0; i < numberOfShapes; i++) {
-            const color: number = this.randomNumber.randomColor();
-            const size: number = this.randomNumber.randomScale(this.GEOMETRY_BASE_SIZE);
+    private addGeometry(sceneData: GeometryMessage[]): void {
+        for (const data of sceneData) {
             const material: THREE.MeshStandardMaterial = new THREE.MeshStandardMaterial({
-                color: color,
+                color: data.color,
                 metalness: 0.7,
                 roughness: 0.2,
             });
-            const randomShape: THREE.Mesh = this.randomGeometryService.getRandomShape(size, material);
-            scene.add(randomShape);
+            const randomShape: THREE.Mesh = this.randomGeometryService.getRandomShape(data.size, material);
+            randomShape.rotation.set(data.rotation.x, data.rotation.y, data.rotation.z);
+            randomShape.position.set(data.position.x, data.position.y, data.position.z);
+            this.scene.add(randomShape);
         }
     }
-    public createScene(numberOfShapes: number): THREE.Scene {
-        const scene: THREE.Scene = new THREE.Scene();
-        this.changeBackgroundScene(scene);
-        this.addGeometry(numberOfShapes, scene);
+    public createScene(sceneData: GeometryMessage[]): THREE.Scene {
+        this.changeBackgroundScene();
+        this.addGeometry(sceneData);
 
-        return scene;
+        return this.scene;
     }
 }
