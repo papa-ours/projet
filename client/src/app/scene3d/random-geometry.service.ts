7611814306d3bc516ff7a91f267f7e9cd291a5f1
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-12 10:00:03 -0500

    MC : suprression des fonctions pour la generation de position aleatoire R

--- client/src/app/scene3d/random-geometry.service.ts
+++ client/src/app/scene3d/random-geometry.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
 import { GeometryGeneratorService } from "./geometry-generator.service";
-import { RandomNumber } from "./random-number.util";
-import { SKYBOX_MAX, SKYBOX_MIN } from "./skybox";
 @Injectable({
     providedIn: "root",
 })
 export class RandomGeometryService {
     private geometryGeneratorService: GeometryGeneratorService = new GeometryGeneratorService() ;
     private functionList: Function[] = [];
-    private randomNumber: RandomNumber = new RandomNumber();
+    // private randomNumber: RandomNumber = new RandomNumber();
     public constructor() {
         this.functionList.push(this.geometryGeneratorService.createSphere);
         this.functionList.push(this.geometryGeneratorService.createCube);
@@ -17,24 +15,9 @@ export class RandomGeometryService {
         this.functionList.push(this.geometryGeneratorService.createCylinder);
         this.functionList.push(this.geometryGeneratorService.createPyramid);
     }
-    private applyRandomPosition(shape: THREE.Mesh): void {
-        shape.position.setX(this.randomNumber.randomInteger(SKYBOX_MIN.x, SKYBOX_MAX.x));
-        shape.position.setY(this.randomNumber.randomInteger(SKYBOX_MIN.y, SKYBOX_MAX.y));
-        shape.position.setZ(this.randomNumber.randomInteger(SKYBOX_MIN.z, SKYBOX_MAX.z));
-    }
-    private applRandomRotation(shape: THREE.Mesh): void {
-        const minAngle: number = 0;
-        const maxAngle: number = Math.PI;
-        shape.rotateX(this.randomNumber.randomFloat(minAngle , maxAngle));
-        shape.rotateY(this.randomNumber.randomFloat(minAngle , maxAngle));
-        shape.rotateZ(this.randomNumber.randomFloat(minAngle , maxAngle));
-    }
     public getRandomShape(size: number, material: THREE.Material): THREE.Mesh {
         const functionSelector: number = Math.floor(Math.random() * this.functionList.length);
-        const randomShape: THREE.Mesh = this.functionList[functionSelector](size, material);
-        this.applyRandomPosition(randomShape);
-        this.applRandomRotation(randomShape);
 
-        return randomShape;
+        return this.functionList[functionSelector](size, material);
     }
 }
