708ee8024a695dce301eb13621cdb7cf94b4e388
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-11 22:14:11 -0500

    VM: test difference-image T

---
+++ server/app/services/utils/difference-image.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from "chai";
+import * as fs from "fs";
+import { BMPImage } from "./bmp-image";
+import { DifferenceImage } from "./difference-image";
+import { Pixel } from "./pixel";
+
+describe("difference-image", () => {
+
+    const width: number = 640;
+    const height: number = 480;
+
+    let differenceImage: DifferenceImage;
+
+    before(() => {
+        differenceImage =  new DifferenceImage(pixels, header, width, height);
+    });
+    const imageArray: Uint8Array = fs.readFileSync("../client/src/assets/img/dog.bmp");
+    const imageBMP: BMPImage = BMPImage.fromArray(fs.readFileSync("../client/src/assets/img/dog.bmp"));
+    const pixels: Pixel[] = imageBMP.pixels;
+    const header: Uint8Array = imageBMP.header;
+
+    it("should test something", () => {
+        expect(true).to.be.equal(true);
+    });
+    it.skip("should convert from BMPImage to DifferenceImage properly", () => {
+        expect(DifferenceImage.fromBMPImage(imageBMP)).to.be.equal(differenceImage);
+    });
+    it.skip("should convert from Array to DifferenceImage properly", () => {
+        expect(DifferenceImage.fromArray(imageArray)).to.be.equal(differenceImage);
+    });
+});
