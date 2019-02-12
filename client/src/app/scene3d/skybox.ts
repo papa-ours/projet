6b7bfa23808d46ac4280060240ae0dd52fe8c17f
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-12 09:50:19 -0500

    MC : deplacement de la skybox dans common R

--- client/src/app/scene3d/skybox.ts
+++
@@ -1,6 +0,0 @@
-import * as THREE from "three";
-
-// tslint:disable-next-line:no-magic-numbers
-export const SKYBOX_MIN: THREE.Vector3 = new THREE.Vector3(-1000, -1000, -500);
-// tslint:disable-next-line:no-magic-numbers
-export const SKYBOX_MAX: THREE.Vector3 = new THREE.Vector3(1000, 1000, -5000);
