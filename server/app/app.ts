d41a98667de9abf3b90f9d6c0efc4a1bbacd96c2
Vincent <vg_4_@hotmail.fr>
2019-02-13 16:24:24 -0500

    VG: Ajout du GameSheetGenerationController dans App D

--- server/app/app.ts
+++ server/app/app.ts
@@ -6,6 +6,7 @@ import { inject, injectable } from "inversify";
 import * as logger from "morgan";
 import { DifferenceCheckerController } from "./controllers/difference-checker.controller";
 import { DifferenceImageController } from "./controllers/difference-image.controller";
+import { GameSheetGenerationController } from "./controllers/game-sheet-generation.controller";
 import { GetGameListController } from "./controllers/get-game-list.controller";
 import { GetGameController } from "./controllers/get-game.controller";
 import Types from "./types";
@@ -19,6 +20,7 @@ export class Application {
     public constructor(
             @inject(Types.GetGameListController) private getGameListController: GetGameListController,
             @inject(Types.GetGameController) private getGameController: GetGameController,
+            @inject(Types.GameSheetGenerationController) private gameSheetGenerationController: GameSheetGenerationController,
             @inject(Types.DifferenceImageController) private differenceImageController: DifferenceImageController,
             @inject(Types.DifferenceCheckerController) private differenceCheckerController: DifferenceCheckerController) {
         this.app = express();
@@ -39,6 +41,7 @@ export class Application {
 
     public bindRoutes(): void {
         // Notre application utilise le routeur de notre API `Index`
+        this.app.use("/api/gamesheet", this.gameSheetGenerationController.router);
         this.app.use("/api/difference", this.differenceCheckerController.router);
         this.app.use("/api/gamelist", this.getGameListController.router);
         this.app.use("/api/difference_image", this.differenceImageController.router);
