671e8b09bb073b5e2447720a61cdd11771664eb1
Vincent <vg_4_@hotmail.fr>
2019-02-13 16:18:10 -0500

    VG: Ajout d'un message d'erreur D

--- server/app/services/utils/file-writer.util.ts
+++ server/app/services/utils/file-writer.util.ts
@@ -1,13 +1,11 @@
 import * as fs from "fs";
 
 export class FileWriterUtil {
-    public static writeFile(path: string, data: Uint8Array): Promise<Buffer> {
-        return new Promise((reject: Function) => {
-            fs.writeFile(path, data, (err: Error) => {
-                if (err) {
-                    reject(err);
-                }
-            });
+    public static writeFile(path: string, data: Uint8Array): void {
+        fs.writeFile(path, data, (err: Error) => {
+            if (err) {
+                console.error("An error occured while writing the file at " + path);
+            }
         });
     }
 }
