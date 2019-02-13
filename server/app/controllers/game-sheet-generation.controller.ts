fe1b33682f4960751d95d4d96b4e14eae9d065db
Vincent <vg_4_@hotmail.fr>
2019-02-13 16:22:46 -0500

    VG: Retrait de tout ce qui a rapport à multer dans GameSheetGenerationController D

--- server/app/controllers/game-sheet-generation.controller.ts
+++ server/app/controllers/game-sheet-generation.controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { injectable } from "inversify";
-import * as multer from "multer";
 import { Message, MessageType } from "../../../common/communication/message";
 
 @injectable()
@@ -8,12 +7,8 @@ export class GameSheetGenerationController {
 
     public get router(): Router {
         const router: Router = Router();
-        const upload: multer.Instance = this.createMulterObject();
 
         router.post("/",
-                    upload.fields([ { name: "name", maxCount: 1 },
-                                    { name: "originalImage", maxCount: 1 },
-                                    { name: "modifiedImage", maxCount: 1 } ]),
                     (req: Request, res: Response, next: NextFunction) => {
                         const message: Message = { type: MessageType.GAME_SHEET_GENERATION, body: ""};
 
@@ -21,18 +16,5 @@ export class GameSheetGenerationController {
                     });
 
         return router;
-    }
-
-    private createMulterObject(): multer.Instance {
-        const storage: multer.StorageEngine = multer.diskStorage({
-            destination: (req: Request, file: Express.Multer.File, callback: Function) => {
-                callback(null, "uploads/");
-            },
-            filename: (req: Request, file: Express.Multer.File, callback: Function) => {
-                callback(null, req.body.name + "-" + file.fieldname + ".bmp");
-            },
-        });
-
-        return multer({ storage: storage });
     }
 }
