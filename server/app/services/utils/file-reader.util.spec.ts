cab2b0a8b13f3e3f55a5b1dbb1c05e0091526b2e
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-12 22:47:29 -0500

    VM: test file-reader.util T

---
+++ server/app/services/utils/file-reader.util.spec.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import * as fs from "fs";
+import { FileReaderUtil } from "./file-reader.util";
+
+describe.only("file-reader.util", () => {
+    let data: Buffer;
+    fs.readFile("../client/src/assets/img/car_original.bmp", (err: NodeJS.ErrnoException, fileData: Buffer) => {
+        data = fileData;
+    });
+
+    it("should resolve", async () => {
+        const result: Buffer = await FileReaderUtil.readFile("../client/src/assets/img/car_original.bmp");
+        expect(result).to.deep.equal(data);
+    });
+});
