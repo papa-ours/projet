6b1feff9899108b506a2d10b5162167b796e7c01
Vincent <vg_4_@hotmail.fr>
2019-02-13 16:16:49 -0500

    VG: Utilisation d'un Uint8Array au lieu d'un Buffer D

--- server/app/services/utils/file-writer.util.ts
+++ server/app/services/utils/file-writer.util.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 
 export class FileWriterUtil {
-    public static writeFile(path: string, data: Buffer): Promise<Buffer> {
+    public static writeFile(path: string, data: Uint8Array): Promise<Buffer> {
         return new Promise((reject: Function) => {
             fs.writeFile(path, data, (err: Error) => {
                 if (err) {
