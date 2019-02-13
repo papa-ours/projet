e59f11f819d2f14c9fd868254ae2c324fc01418d
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-12 22:01:58 -0500

    MC : ajout de test pour la fonction addGeometry T

--- server/app/services/scene/scene-difference-generator.spec.ts
+++ server/app/services/scene/scene-difference-generator.spec.ts
@@ -1,15 +1,26 @@
 import { expect } from "chai";
+import { GeometryData } from "../../../../common/communication/geometryMessage";
 import { SceneDataGeneratorService } from "./scene-data-generator";
 import { SceneDataDifference } from "./scene-difference-generator";
 
-describe("Differences finder", () => {
+describe("SceneDataDifference", () => {
     // @ts-ignore a enlever
     let sceneDataDifference: SceneDataDifference;
-
+    let sceneDataGeneratorService: SceneDataGeneratorService;
     beforeEach(() => {
         sceneDataDifference = new SceneDataDifference( new SceneDataGeneratorService());
+        sceneDataGeneratorService = new SceneDataGeneratorService()
     });
-    it("test bidon", () => {
-        expect(true).to.equal(true);
+    it("should have greater length on addGeometry", () => {
+        const geometryData: GeometryData[] = [sceneDataGeneratorService.getGeometryData()];
+        const lenghtBeforeAdd: number = geometryData.length;
+        sceneDataDifference.addGeometryData(geometryData);
+        const lenghtAfterAdd: number = geometryData.length;
+        expect(lenghtAfterAdd).to.be.greaterThan(lenghtBeforeAdd);
+    });
+    it("should set proprety is modified to true on addGeometry", () => {
+        const geometryData: GeometryData[] = [sceneDataGeneratorService.getGeometryData()];
+        sceneDataDifference.addGeometryData(geometryData);
+        expect(geometryData[geometryData.length - 1].isModified).to.equal(true);
     });
 });
