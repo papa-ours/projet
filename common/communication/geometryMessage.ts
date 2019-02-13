490137cc7ffce8ef649d3eb24511cd97ad8507fa
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-12 19:59:24 -0500

    MC : creation d'une interface pour les donnees de la scene D

--- common/communication/geometryMessage.ts
+++ common/communication/geometryMessage.ts
@@ -6,3 +6,9 @@ export interface GeometryData {
    color:    number;
    size :    number;
 }
+
+export interface SceneData {
+    name: string;
+    originalScene:GeometryData [];
+    modifiedScene:GeometryData [];
+}
