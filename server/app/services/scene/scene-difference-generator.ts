01d28dc47d0f4d37a2209e3fb837477b638e2119
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-12 20:59:36 -0500

    MC : fonctionalite ajout d'un objet D

--- server/app/services/scene/scene-difference-generator.ts
+++ server/app/services/scene/scene-difference-generator.ts
@@ -1,8 +1,21 @@
 import { injectable } from "inversify";
 import "reflect-metadata";
+import { GeometryData } from "../../../../common/communication/geometryMessage";
+import { Vector } from "../../../../common/communication/position";
+import { SceneDataGeneratorService } from "./scene-data-generator";
 
 @injectable()
 export class SceneDataDifference {
-    public constructor() { }
+    public constructor(private sceneDataGeneratorService: SceneDataGeneratorService) { }
 
+    public addObject(geometryData: GeometryData[]): void {
+       const color: number = this.sceneDataGeneratorService.getRandomColor();
+       const position: Vector = this.sceneDataGeneratorService.getRandomPosition();
+       const rotation: Vector = this.sceneDataGeneratorService.getRandomRotation();
+       const size: number = this.sceneDataGeneratorService.getRandomSize();
+       geometryData.push({position: position , rotation: rotation, size: size, color: color, isModified: true});
+    }
+    public getDifference(geometryData: GeometryData[]): GeometryData[] {
+        return geometryData;
+    }
 }
