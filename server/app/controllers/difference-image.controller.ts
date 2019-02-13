5ac41de4fb14a53e8403aaf0f353361f25db5a3b
Vincent <vg_4_@hotmail.fr>
2019-02-13 16:15:45 -0500

    VG: Utilisation du FileWriterUtil pour sauvegarder l'image de difference D

--- server/app/controllers/difference-image.controller.ts
+++ server/app/controllers/difference-image.controller.ts
@@ -5,6 +5,7 @@ import { Message, MessageType } from "../../../common/communication/message";
 import { DifferenceImage } from "../../../common/images/difference-image";
 import { DifferenceImageGenerator } from "../services/difference-image-generator.service";
 import { DifferencesFinderService } from "../services/differences-finder.service";
+import { FileWriterUtil } from "../services/utils/file-writer.util";
 import Types from "../types";
 
 @injectable()
@@ -37,6 +38,10 @@ export class DifferenceImageController {
                             const numberOfDifferences: number = this.differencesFinder.getNumberOfDifferences(differenceImage);
                             message.body = numberOfDifferences === REQUIRED_DIFFERENCES ?
                                 "" : "Les images n'ont pas exactement " + REQUIRED_DIFFERENCES + " différences, la création a été annulée";
+
+                            if (numberOfDifferences === REQUIRED_DIFFERENCES) {
+                                FileWriterUtil.writeFile(`uploads/${name}-differenceImage.bmp`, differenceImage.toArray());
+                            }
                         }
 
                         res.send(message);
