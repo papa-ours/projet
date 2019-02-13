f9c6ad9dce327cbd48950d4c119f2554c8d2d5eb
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-13 17:35:30 -0500

    MC : send envoie en message contenant sceneData D

--- server/app/controllers/scene-data.controler.ts
+++ server/app/controllers/scene-data.controler.ts
@@ -1,14 +1,16 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
-import { GeometryData } from "../../../common/communication/geometryMessage";
+import { GeometryData, SceneData } from "../../../common/communication/geometryMessage";
 import { Message, MessageType } from "../../../common/communication/message";
 import { SceneDataGeneratorService } from "../services/scene/scene-data-generator";
+import { SceneDataDifferenceService } from "../services/scene/scene-difference-generator";
 import Types from "../types";
 
 @injectable()
 export class SceneDataControler {
 
-    public constructor(@inject(Types.SceneDataGeneratorService) private sceneDataGeneratorService: SceneDataGeneratorService) { }
+    public constructor(@inject(Types.SceneDataDifferenceService) private sceneDataDifferenceService: SceneDataDifferenceService,
+                       @inject(Types.SceneDataGeneratorService) private sceneDataGeneratorService: SceneDataGeneratorService) { }
 
     public get router(): Router {
         const router: Router = Router();
@@ -16,12 +18,14 @@ export class SceneDataControler {
         router.post("/",
                     (req: Request, res: Response, next: NextFunction) => {
                        // Send the request to the service and send the response
-                        const scene: GeometryData [] = this.sceneDataGeneratorService.getSceneData(Number(req.body.body));
-                        const message: Message = {
+                      const originalGeometry: GeometryData [] = this.sceneDataGeneratorService.getSceneData(Number(req.body.body));
+                      const modifiedGeometry: GeometryData [] = this.sceneDataDifferenceService.getDifference(originalGeometry);
+                      const scene: SceneData = {name: "placeholder", originalScene: originalGeometry, modifiedScene: modifiedGeometry};
+                      const message: Message = {
                             type: MessageType.SCENE_DATA,
                             body: JSON.stringify(scene),
                        };
-                        res.send(message);
+                      res.send(message);
                    });
 
         return router;
