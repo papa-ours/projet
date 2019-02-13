ff193d60c0efa2f086b0f2cd8da8bb862b6f21bd
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-12 21:34:56 -0500

    MC : ajout de la fonctionnalite de supression d'une geometrie D

--- server/app/services/scene/scene-difference-generator.ts
+++ server/app/services/scene/scene-difference-generator.ts
@@ -1,24 +1,29 @@
 import { injectable } from "inversify";
 import "reflect-metadata";
 import { GeometryData } from "../../../../common/communication/geometryMessage";
-import { Vector } from "../../../../common/communication/position";
 import { SceneDataGeneratorService } from "./scene-data-generator";
 
 @injectable()
 export class SceneDataDifference {
     public constructor(private sceneDataGeneratorService: SceneDataGeneratorService) { }
 
-    public addGeometryData(geometryDataDifference: GeometryData[]): void {
-       const color: number = this.sceneDataGeneratorService.getRandomColor();
-       const position: Vector = this.sceneDataGeneratorService.getRandomPosition();
-       const rotation: Vector = this.sceneDataGeneratorService.getRandomRotation();
-       const size: number = this.sceneDataGeneratorService.getRandomSize();
-       geometryDataDifference.push({position: position , rotation: rotation, size: size, color: color, isModified: true});
+    public addGeometryData(geometryDataDifference: GeometryData[]): GeometryData[] {
+        geometryDataDifference.push(this.sceneDataGeneratorService.getGeometryData());
+
+        return geometryDataDifference;
+    }
+    public deleteGeometryData(geometryDataDifference: GeometryData[], index: number): GeometryData[] {
+        const numberOfDeletion: number = 1;
+        geometryDataDifference.splice(index, numberOfDeletion);
+
+        return geometryDataDifference;
     }
     public getDifference(geometryData: GeometryData[]): GeometryData[] {
         // create a copy of object
-        const geometryDataDifference: GeometryData[] = geometryData.map((data: GeometryData) => data);
-        this.addGeometryData(geometryDataDifference);
+        let geometryDataDifference: GeometryData[] = geometryData.map((data: GeometryData) => data);
+        geometryDataDifference = this.addGeometryData(geometryDataDifference);
+        const randomIndex: number = Math.floor(Math.random() * geometryDataDifference.length);
+        geometryDataDifference = this.deleteGeometryData(geometryDataDifference, randomIndex);
 
         return geometryDataDifference;
     }
