5f357bb57c3f86d91e5eee1aa946c237e8e89cdc
Vincent <vg_4_@hotmail.fr>
2019-02-13 15:28:58 -0500

    VG: Retrait de tous les choses inutiles D

--- server/app/services/game-sheet-generation.service.ts
+++ server/app/services/game-sheet-generation.service.ts
@@ -1,17 +1,13 @@
 import { inject, injectable } from "inversify";
 import { DBConnectionService } from "./dbconnection.service";
-import { DifferenceImageGenerator } from "./difference-image-generator.service";
-import { DifferencesFinderService } from "./differences-finder.service";
 
 import "reflect-metadata";
 import { GameSheetDescription } from "../../../common/communication/game-description";
-import { Message, MessageType } from "../../../common/communication/message";
 import { DifferenceImage } from "../../../common/images/difference-image";
 import Types from "../types";
 import { Game } from "./game";
 import { GetGameService } from "./get-game.service";
 import { TopScores } from "./score/top-scores";
-import { FileReaderUtil } from "./utils/file-reader.util";
 
 // @ts-ignore
 enum ImageType {
@@ -25,51 +21,11 @@ export class GameSheetGenerationService {
     private imagesData: Uint8Array[];
 
     public constructor(
-        @inject(Types.DifferenceImageGenerator) private differenceImageGenerator: DifferenceImageGenerator,
-        @inject(Types.DifferencesFinderService) private differencesFinder: DifferencesFinderService,
         @inject(Types.DBConnectionService) private dbConnection: DBConnectionService,
         @inject(Types.GetGameService) private getGameService: GetGameService,
-    ) {
-        this.generateGameSheet("Voiture", ["assets/voiture-originalImage.bmp", "assets/voiture-modifiedImage.bmp"]);
-        this.generateGameSheet("Pringles", ["assets/pringles-originalImage.bmp", "assets/pringles-modifiedImage.bmp"]);
-    }
+    ) {}
 
-    public generateGameSheet(name: string, paths: string[]): void {
-        const readFiles: Promise<Buffer>[] = paths.map((path: string) => {
-            return FileReaderUtil.readFile(path);
-        });
-
-        Promise.all(readFiles).then((buffers: Buffer[]) => {
-            this.imagesData = buffers.map((buffer: Buffer) => {
-                return new Uint8Array(buffer);
-            });
-            this.filesAreRead(name);
-        });
-    }
-
-    // @ts-ignore
-    private filesAreRead(name: string): Message {
-        const differenceImage: DifferenceImage = this.differenceImageGenerator
-            .generate(this.imagesData[ImageType.Original], this.imagesData[ImageType.Modified]) as DifferenceImage;
-
-        const numberOfDifferences: number = this.differencesFinder.getNumberOfDifferences(differenceImage);
-        const REQUIRED_DIFFERENCES: number = 7;
-
-        const message: Message = {
-            type: MessageType.GAME_SHEET_GENERATION,
-            body: "",
-        };
-
-        if (numberOfDifferences !== REQUIRED_DIFFERENCES) {
-            message.body = "Les images n'ont pas exactement 7 différences, la création a été annulée";
-        } else {
-            this.createGame(name, differenceImage);
-        }
-
-        return message;
-    }
-
-    private createGame(name: string, differenceImage: DifferenceImage): void {
+    public createGame(name: string, differenceImage: DifferenceImage): void {
         const id: string = this.generateId();
         const game: Game = new Game(id, this.imagesData[ImageType.Original], this.imagesData[ImageType.Modified], differenceImage);
         const gameSheet: GameSheetDescription = {
