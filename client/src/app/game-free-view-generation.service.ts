f02c060b2168540b9afcc976d07e9886a77010b7
David <david.piche@polymtl.ca>
2019-02-13 15:11:43 -0500

    DP: AJout d'appel post pour la generation de scene libre D

--- client/src/app/game-free-view-generation.service.ts
+++ client/src/app/game-free-view-generation.service.ts
@@ -10,7 +10,24 @@ import { HttpClient } from "@angular/common/http";
 })
 export class GameFreeViewGenerationService {
     
-    public constructor() { }
+    public readonly URL: string = "http://localhost:3000/api/gamefreeview";
+    public constructor(private http: HttpClient) { }
 
+    public postGenerate(formData: FormData): Observable<Message> {
+        return this.http.post<Message>(this.URL, {
+            name: formData.get("name"),
+            originalImage: formData.get("originalImage"),
+            modifiedImage: formData.get("modifiedImage"),
+            nbObjects: formData.get("nbObjects"),
+            ajout: formData.get("ajout"),
+            suppression: formData.get("suppression"),
+            changementCouleur: formData.get("changementCouleur"),
+            objectType: formData.get("objectType")
+        }).pipe(catchError(this.handleError<Message>("postGenerate")),
+        );
+    }
+    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+        return (error: Error): Observable<T> => of(result as T);
+    }
 
 }
