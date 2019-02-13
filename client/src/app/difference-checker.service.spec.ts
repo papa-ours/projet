f70482948e84c5991ad9416338c02f3a8e72ab3f
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-12 22:47:00 -0500

    VM: changement du type de la variable data T

--- client/src/app/difference-checker.service.spec.ts
+++ client/src/app/difference-checker.service.spec.ts
@@ -21,7 +21,7 @@ describe("DifferenceCheckerService", () => {
 
     it("should be a GET REQUEST", () => {
         differenceCheckerService.isPositionDifference("0", 0, 0).subscribe(
-            (data: string) => { expect(data).toBeDefined();
+            (data: boolean) => { expect(data).toBeDefined();
         });
         const request: TestRequest = httpMock.expectOne(`${differenceCheckerService.BASE_URL + "0/0/0"}`);
         expect(request.request.method).toBe("GET");
