dc36d398d5ce10ca6e2e9e6663b945ef861e8ed0
Vincent <vg_4_@hotmail.fr>
2019-02-13 21:14:11 -0500

    VG: La valeur attendue est un string D

--- client/src/app/gameplay.service.ts
+++ client/src/app/gameplay.service.ts
@@ -13,9 +13,9 @@ export class GameplayService {
     public readonly URL: string = "http://localhost:3000/api/game/";
     public constructor(private http: HttpClient) { }
 
-    public getGameId(name: string): Observable<string[]> {
+    public getGameId(name: string): Observable<string> {
         return this.http.get<Message>(this.URL + name)
-            .pipe(map((message: Message) => JSON.parse(message.body)),
+            .pipe(map((message: Message) => message.body),
         );
     }
 }
