c1defd0984f7d84d8be3582fe6ca0ccaa26e92fd
Vincent <vg_4_@hotmail.fr>
2019-02-14 11:42:10 -0500

    VG: Test pour GetGameService sont à 100% de coverage T

--- server/app/services/get-game.service.spec.ts
+++ server/app/services/get-game.service.spec.ts
@@ -1,13 +1,14 @@
 import { expect } from "chai";
 import { GameSheet, TopScoresInterface } from "../../../common/communication/game-description";
+import { Game } from "./game";
 import { GetGameService } from "./get-game.service";
 
+// tslint:disable-next-line:max-func-body-length
 describe("GetGameService", () => {
 
     let getGameService: GetGameService;
-    const carID: string = "0";
     const topScoreInterface: TopScoresInterface[] = [];
-    const gameSheet: GameSheet = {id: carID, name: "car", topScores: topScoreInterface};
+    const gameSheet: GameSheet = {id: "", name: "car", topScores: topScoreInterface};
 
     beforeEach(() => {
         getGameService = new GetGameService();
@@ -26,12 +27,24 @@ describe("GetGameService", () => {
 
     it("should add the game in the GameService properly", () => {
         getGameService.addGameSheet(gameSheet);
-        expect(getGameService.getGameSheet(carID)).to.equal(gameSheet);
+        expect(getGameService.getGameSheet(gameSheet.id)).to.equal(gameSheet);
     });
 
     it("should return the GameSheetDescription properly", () => {
         const expected: GameSheet[] = [gameSheet];
         const result: GameSheet[] = getGameService.getGameDescriptions();
         expect(result).to.deep.equal(expected);
+    });
+
+    it("should create an id with the correct length", () => {
+        const ID_LENGTH: number = 25;
+        expect(gameSheet.id.length).to.equals(ID_LENGTH);
+    });
+
+    it("should create a game properly", () => {
+        const id: string = getGameService.createGame("voiture");
+        const game: Game | undefined = getGameService.getGame(id);
+
+        game ? expect(game.id).to.equals(id) : expect(false).to.equals(true);
     });
 });
