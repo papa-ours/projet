d42d4113a8045811a45e36bf4921f454f8fb822e
Vincent <vg_4_@hotmail.fr>
2019-02-14 11:03:51 -0500

    VG: Ajout d'un test pour FileReaderUtil qui test le reject T

--- server/app/services/utils/file-reader.util.spec.ts
+++ server/app/services/utils/file-reader.util.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import * as fs from "fs";
 import { FileReaderUtil } from "./file-reader.util";
 
-describe("file-reader.util", () => {
+describe.only("file-reader.util", () => {
     let data: Buffer;
     fs.readFile("../client/src/assets/img/car_original.bmp", (err: NodeJS.ErrnoException, fileData: Buffer) => {
         data = fileData;
@@ -11,5 +11,13 @@ describe("file-reader.util", () => {
     it("should resolve", async () => {
         const result: Buffer = await FileReaderUtil.readFile("../client/src/assets/img/car_original.bmp");
         expect(result).to.deep.equal(data);
+    });
+
+    it("should reject if the image doesn't exist", async () => {
+        try {
+            await FileReaderUtil.readFile("");
+        } catch {
+            expect(true).to.equals(true);
+        }
     });
 });
