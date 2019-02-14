e24216fffe6b34c20b2664cfe757fd5b92eb4b40
Vincent <vg_4_@hotmail.fr>
2019-02-14 10:58:13 -0500

    VG: Les tests pour l'identifcation de ddifference fonctionnent T

--- server/app/services/difference-checker.service.spec.ts
+++ server/app/services/difference-checker.service.spec.ts
@@ -1,7 +1,32 @@
 import { expect } from "chai";
+import { DifferenceCheckerService } from "./difference-checker.service";
+import { Game } from "./game";
 
 describe("Difference Checker", () => {
-    it("should pass this test", () => {
-        expect(true).to.equals(true);
+    const differenceChecker: DifferenceCheckerService = new DifferenceCheckerService();
+    let game: Game;
+
+    beforeEach((done: MochaDone) => {
+        const ONE_SECOND: number = 1000;
+        game = new Game("0", "voiture");
+        setTimeout(
+            () => {
+                done();
+            },
+            ONE_SECOND);
+    });
+
+    it("should return false if there is no difference at the position", () => {
+        const result: boolean = differenceChecker.isPositionDifference(0, 0, game);
+
+        expect(result).to.equals(false);
+    });
+
+    it("should return true is the position is a difference", () => {
+        const x: number = 35;
+        const y: number = 193;
+        const result: boolean = differenceChecker.isPositionDifference(x, y, game);
+
+        expect(result).to.equals(true);
     });
 });
