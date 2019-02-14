48f48c98ad8cc2f24b3b15f6b7f6afff56e2ab60
Vincent <vg_4_@hotmail.fr>
2019-02-14 11:21:34 -0500

    VG: Ajout d'un test pour tester le resolve, le coverage de FileWriter est de 100% T

--- server/app/services/utils/file-writer.util.spec.ts
+++ server/app/services/utils/file-writer.util.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { FileReaderUtil } from "./file-reader.util";
 import { FileWriterUtil } from "./file-writer.util";
 
 describe.only("FileWriterUtil", () => {
@@ -9,5 +10,14 @@ describe.only("FileWriterUtil", () => {
         } catch {
             expect(true).to.equals(true);
         }
+    });
+
+    it("should resolve if the directory exists", async () => {
+        const data: Uint8Array = new Uint8Array([0, 1, 0, 1]);
+        await FileWriterUtil.writeFile("test/test.bmp", data);
+        const readData: Buffer = await FileReaderUtil.readFile("test/test.bmp");
+        const readArray: Uint8Array = new Uint8Array(readData);
+
+        expect(readArray).to.deep.equals(data);
     });
 });
