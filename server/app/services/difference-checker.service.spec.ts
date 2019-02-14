ae6da00c8d9b39e141b106ce0f5bf3622e5e610f
Vincent <vg_4_@hotmail.fr>
2019-02-14 10:59:36 -0500

    VG: Coverage de DifferenceCheckerService est a 100% T

--- server/app/services/difference-checker.service.spec.ts
+++ server/app/services/difference-checker.service.spec.ts
@@ -22,11 +22,19 @@ describe("Difference Checker", () => {
         expect(result).to.equals(false);
     });
 
-    it("should return true is the position is a difference", () => {
+    it("should return true if the position is a difference", () => {
         const x: number = 35;
         const y: number = 193;
         const result: boolean = differenceChecker.isPositionDifference(x, y, game);
 
         expect(result).to.equals(true);
+    });
+
+    it("should return false if the position is out of the image", () => {
+        const x: number = -1;
+        const y: number = -1;
+        const result: boolean = differenceChecker.isPositionDifference(x, y, game);
+
+        expect(result).to.equals(false);
     });
 });
