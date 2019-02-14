8e65a662d8d9ec639589bc2adfc5fb2ae4085076
Vincent <vg_4_@hotmail.fr>
2019-02-14 11:21:02 -0500

    VG: Ajout d'un resolve pour que la Promise se termine D

--- server/app/services/utils/file-writer.util.ts
+++ server/app/services/utils/file-writer.util.ts
@@ -2,11 +2,13 @@ import * as fs from "fs";
 
 export class FileWriterUtil {
     public static async writeFile(path: string, data: Uint8Array): Promise<{}> {
-        return new Promise((reject: Function) => {
+        return new Promise((resolve: Function, reject: Function) => {
             fs.writeFile(path, data, (err: Error) => {
                 if (err) {
                     reject(err);
                 }
+
+                resolve();
             });
         });
     }
