cc30729c1c1c75f14320faf5b8f377039da452d7
David <david.piche@polymtl.ca>
2019-02-15 19:05:05 -0500

    DP: Correction des appels de methodes dans la methode test R

--- client/src/app/form-validation-free-view.service.spec.ts
+++ client/src/app/form-validation-free-view.service.spec.ts
@@ -5,33 +5,36 @@ import { FormValidationFreeViewService } from './form-validation-free-view.servi
 describe('FormValidationFreeViewService', () => {
     let name: string = "abcdefghijklmno";
     let nbObjects = 20;
+    let ajout = true;
+    let modification = true;
+    let suppression = true;
     it('should be created', () => {
         const service: FormValidationFreeViewService = TestBed.get(FormValidationFreeViewService);
         expect(service).toBeTruthy();
     });
     it("should return false with an empty name ", () => {
         name = "";
-        expect(FormValidationFreeViewService.isFormValid(name,nbObjects)).toBeFalsy();
+        expect(FormValidationFreeViewService.isFormValid(name,nbObjects,ajout,modification,suppression)).toBeFalsy();
     });
     it("should return false with a name shorter or equal to 5 characters  ", () => {
         name = "abc";
-        expect(FormValidationFreeViewService.isFormValid(name,nbObjects)).toBeFalsy();
+        expect(FormValidationFreeViewService.isFormValid(name,nbObjects,ajout,modification,suppression)).toBeFalsy();
     });
     it("should return false with a name longer than 15 characters ", () => {
         name = "abcdefghijklmnoq";
-        expect(FormValidationFreeViewService.isFormValid(name,nbObjects)).toBeFalsy();;
+        expect(FormValidationFreeViewService.isFormValid(name,nbObjects,ajout,modification,suppression)).toBeFalsy();;
     });
     it("should return true with a name between 5 and 15 characters ", () => {
         name = "abcdefghijklmno";
-        expect(FormValidationFreeViewService.isFormValid(name,nbObjects)).toBeTruthy();;
+        expect(FormValidationFreeViewService.isFormValid(name,nbObjects,ajout,modification,suppression)).toBeTruthy();;
     });
     it("should return false with a number of objects below 10", () => {
         nbObjects = 8;
-        expect(FormValidationFreeViewService.isFormValid(name,nbObjects)).toBeFalsy();;
+        expect(FormValidationFreeViewService.isFormValid(name,nbObjects,ajout,modification,suppression)).toBeFalsy();;
     });
     it("should return false with a number of objects over 200", () => {
         nbObjects = 210;
-        expect(FormValidationFreeViewService.isFormValid(name,nbObjects)).toBeFalsy();;
+        expect(FormValidationFreeViewService.isFormValid(name,nbObjects,ajout,modification,suppression)).toBeFalsy();;
     });
     
 });
