e09ff06d40cc74214f3c52562c5718aa179e66e4
David <david.piche@polymtl.ca>
2019-02-17 13:12:40 -0500

    DP: Suppression du test auot-genere par Angular T

--- client/src/app/game-free-view-generation.service.spec.ts
+++ client/src/app/game-free-view-generation.service.spec.ts
@@ -5,8 +5,4 @@ import { GameFreeViewGenerationService } from './game-free-view-generation.servi
 describe('GameFreeViewGenerationService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: GameFreeViewGenerationService = TestBed.get(GameFreeViewGenerationService);
-    expect(service).toBeTruthy();
-  });
 });
