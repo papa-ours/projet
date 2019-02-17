1364d75ad8e98bf0a390360e2165f469704b8829
David <david.piche@polymtl.ca>
2019-02-17 13:16:37 -0500

    DP: Mise en forme du beforeEach() T

--- client/src/app/game-free-view-generation.service.spec.ts
+++ client/src/app/game-free-view-generation.service.spec.ts
@@ -1,8 +1,18 @@
-import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
 import { GameFreeViewGenerationService } from './game-free-view-generation.service';
 
 describe('GameFreeViewGenerationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    let gameFreeViewGenerationService: GameFreeViewGenerationService;
+    let httpMock: HttpTestingController;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [GameFreeViewGenerationService],
+        });
+        // tslint:disable-next-line:no-any
+        gameFreeViewGenerationService = TestBed.get(GameFreeViewGenerationService);
+        httpMock = TestBed.get(HttpTestingController);
+    });
 
 });
