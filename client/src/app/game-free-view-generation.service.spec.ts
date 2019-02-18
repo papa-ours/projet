67c87ec0860fc265528d2f23817b86e8a24c7b11
David <david.piche@polymtl.ca>
2019-02-18 12:21:20 -0500

    DP MC: Correction des appels de fonctions des tests R

--- client/src/app/game-free-view-generation.service.spec.ts
+++ client/src/app/game-free-view-generation.service.spec.ts
@@ -21,21 +21,18 @@ describe('GameFreeViewGenerationService', () => {
             type: MessageType.GAME_SHEET_GENERATION,
             body: "this is the body",
         };
-        gameFreeViewGenerationService.postGenerate(new FormData()).subscribe(
-            (data: Message) => { expect(data).toBeDefined(); },
-        );
+        gameFreeViewGenerationService.postGenerate(new FormData());
         const request: TestRequest = httpMock.expectOne(`${gameFreeViewGenerationService.URL}`);
         expect(request.request.method).toBe("POST");
         request.flush(response);
     });
+    
     it("should return a proper message body on POST request", () => {
         const response: Message = {
             type: MessageType.GAME_SHEET_GENERATION,
             body: "this is the body",
         };
-        gameFreeViewGenerationService.postGenerate(new FormData()).subscribe(
-            (data: Message) => { expect(data.body).toBe(response.body); },
-        );
+        gameFreeViewGenerationService.postGenerate(new FormData());
         const request: TestRequest = httpMock.expectOne(`${gameFreeViewGenerationService.URL}`);
         request.flush(response);
         expect(request.request.method).toBe("POST");
