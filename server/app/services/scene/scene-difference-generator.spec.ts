9477531cceeea6f4ed739852c860114535116458
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-18 11:35:22 -0500

    MC : appelle de methode privee dans les tests pour tester la logique R

--- server/app/services/scene/scene-difference-generator.spec.ts
+++ server/app/services/scene/scene-difference-generator.spec.ts
@@ -14,32 +14,36 @@ describe("SceneDataDifference", () => {
     it("should have a greater length on addGeometryData", () => {
         const geometryData: GeometryData[] = [sceneDataGeneratorService.getRandomGeometryData()];
         const lenghtBeforeAdd: number = geometryData.length;
-        sceneDataDifference.addGeometryData(geometryData, 0);
+        // call private method because non deterministic behavior
+        sceneDataDifference["addGeometryData"](geometryData, 0);
         const lenghtAfterAdd: number = geometryData.length;
         expect(lenghtAfterAdd).to.be.greaterThan(lenghtBeforeAdd);
     });
     it("should set proprety is modified to true on addGeometryData", () => {
         const geometryData: GeometryData[] = [sceneDataGeneratorService.getRandomGeometryData()];
-        sceneDataDifference.addGeometryData(geometryData, 0);
+        sceneDataDifference["addGeometryData"](geometryData, 0);
         expect(geometryData[geometryData.length - 1].isModified).to.equal(true);
     });
     it("should have a lower length on deleteGeometryData", () => {
         const geometryData: GeometryData[] = [sceneDataGeneratorService.getRandomGeometryData()];
         const lenghtBeforeAdd: number = geometryData.length;
-        sceneDataDifference.deleteGeometryData(geometryData, 0);
+        // call private method because non deterministic behavior
+        sceneDataDifference["deleteGeometryData"](geometryData, 0);
         const lenghtAfterAdd: number = geometryData.length;
         expect(lenghtAfterAdd).to.be.lessThan(lenghtBeforeAdd);
     });
     it("should not have the same color after changeColorGeometryData", () => {
         const geometryData: GeometryData[] = [sceneDataGeneratorService.getRandomGeometryData()];
         const colorBefore: number = geometryData[0].color;
-        sceneDataDifference.changeColorGeometryData(geometryData, 0);
+        // call private method because non deterministic behavior
+        sceneDataDifference["changeColorGeometryData"](geometryData, 0);
         const colorAfter: number = geometryData[0].color;
         expect(colorBefore).to.be.not.equal(colorAfter);
     });
     it("should set proprety is modified to true on changeColorGeometryData", () => {
         const geometryData: GeometryData[] = [sceneDataGeneratorService.getRandomGeometryData()];
-        sceneDataDifference.changeColorGeometryData(geometryData, 0);
+        // call private method because non deterministic behavior
+        sceneDataDifference["changeColorGeometryData"](geometryData, 0);
         expect(geometryData[geometryData.length - 1].isModified).to.equal(true);
     });
     it("should not change the geometry data injected it should make a copie instead", () => {
