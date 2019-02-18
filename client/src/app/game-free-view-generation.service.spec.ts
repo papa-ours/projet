e64cdcbf633f074eee811b6e50772af0902653d0
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-18 12:26:05 -0500

    MC :  tslint R

--- client/src/app/game-free-view-generation.service.spec.ts
+++ client/src/app/game-free-view-generation.service.spec.ts
@@ -1,11 +1,12 @@
 import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 import { Message, MessageType } from "../../../common/communication/message";
-import { GameFreeViewGenerationService } from './game-free-view-generation.service';
+import { GameFreeViewGenerationService } from "./game-free-view-generation.service";
 
-describe('GameFreeViewGenerationService', () => {
+describe("GameFreeViewGenerationService", () => {
     let gameFreeViewGenerationService: GameFreeViewGenerationService;
     let httpMock: HttpTestingController;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
@@ -28,6 +29,7 @@ describe('GameFreeViewGenerationService', () => {
         expect(request.request.method).toBe("POST");
         request.flush(response);
     });
+
     it("should return a proper message body on POST request", () => {
         const response: Message = {
             type: MessageType.GAME_SHEET_GENERATION,
@@ -40,4 +42,5 @@ describe('GameFreeViewGenerationService', () => {
         request.flush(response);
         expect(request.request.method).toBe("POST");
     });
+
 });
