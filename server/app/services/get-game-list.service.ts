f84f60747f6675778908594ad19e6986e3e5e928
Vincent <vg_4_@hotmail.fr>
2019-02-18 19:28:49 -0500

    VG: Utilisation des types pour obtenir les différents type de GameSheets D

--- server/app/services/get-game-list.service.ts
+++ server/app/services/get-game-list.service.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { inject, injectable } from "inversify";
 import "reflect-metadata";
-import { GameLists} from "../../../common/communication/game-description";
+import { GameLists, GameType} from "../../../common/communication/game-description";
 import { Message, MessageType } from "../../../common/communication/message";
 import Types from "../types";
 import { GetGameService } from "./get-game.service";
@@ -13,9 +13,11 @@ export class GetGameListService {
     }
 
     public getGameList(res: Response): void {
-        const gameList: GameLists = { list2d: [], list3d: [] };
+        const gameList: GameLists = {
+            list2d: this.getGameService.getGameDescriptions(GameType.Simple),
+            list3d: this.getGameService.getGameDescriptions(GameType.Free),
+        };
 
-        gameList.list2d = this.getGameService.getGameDescriptions();
         const message: Message = {
             type: MessageType.USERNAME_VALIDATION,
             body: JSON.stringify(gameList),
