610d6273c0de335e4a423fa80160a3b1f891a2df
Vincent <vg_4_@hotmail.fr>
2019-02-18 19:29:23 -0500

    VG: Utilisation du type de jeu pour créer la gameSheet D

--- server/app/controllers/game-sheet-generation.controller.ts
+++ server/app/controllers/game-sheet-generation.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
+import { GameType } from "../../../common/communication/game-description";
 import { GameSheetGenerationService } from "../services/game-sheet-generation.service";
 import Types from "../types";
 
@@ -14,7 +15,7 @@ export class GameSheetGenerationController {
         router.post(
             "/",
             (req: Request, res: Response, next: NextFunction) => {
-                this.gameSheetGenerationService.createGameSheet(req.body.name);
+                this.gameSheetGenerationService.createGameSheet(req.body.name, GameType.Simple);
             });
 
         return router;
