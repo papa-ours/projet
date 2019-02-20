594cc83aa33bb5912900aa13e3fd52b8c21afcbb
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-20 16:55:57 -0500

    [R] VM: changement || par &&

--- server/app/services/score/top-scores.spec.ts
+++ server/app/services/score/top-scores.spec.ts
@@ -13,7 +13,7 @@ describe("Top Scores", () => {
         const scoreOneNumber: number = topScores.scores[0].toNumber();
         const scoreTwoNumber: number = topScores.scores[1].toNumber();
         const scoreThreeNumber: number = topScores.scores[2].toNumber();
-        const result: boolean = (scoreOneNumber <= scoreTwoNumber || scoreTwoNumber <= scoreThreeNumber);
+        const result: boolean = (scoreOneNumber <= scoreTwoNumber && scoreTwoNumber <= scoreThreeNumber);
 
         expect(result).to.be.equals(true);
     });
