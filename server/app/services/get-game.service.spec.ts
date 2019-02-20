71453c082b774e2e08bbe0a9c0d2b36bc6ee5b1f
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-20 16:52:50 -0500

    [R] VM: ajout espaces avant les expects

--- server/app/services/get-game.service.spec.ts
+++ server/app/services/get-game.service.spec.ts
@@ -5,7 +5,6 @@ import { GetGameService } from "./get-game.service";
 
 // tslint:disable-next-line:max-func-body-length
 describe("GetGameService", () => {
-
     let getGameService: GetGameService;
     const topScoreInterface: TopScoresInterface[] = [];
     const gameSheet: GameSheet = {id: "", name: "car", topScores: topScoreInterface};
@@ -21,6 +20,7 @@ describe("GetGameService", () => {
 
     it("should return throw an error if there's no corresponding id", () => {
         const id: string = "1";
+
         expect(() => {
             return getGameService.getGame(id);
         }).to.throws("Aucune Game n'a le id 1");
@@ -28,6 +28,7 @@ describe("GetGameService", () => {
 
     it("should return undefined if there's no corresponding gamesheet with id", () => {
         const id: string = "1";
+
         expect(() => {
             getGameService.getGameSheet(id);
         }).to.throws("Aucune GameSheet n'a le id 1");
@@ -36,22 +37,26 @@ describe("GetGameService", () => {
     it("should return the GameSheetDescription properly if it's empty", () => {
         const expected: GameSheet[] = [];
         const result: GameSheet[] = getGameService.getGameDescriptions();
+
         expect(result).to.deep.equal(expected);
     });
 
     it("should add the game in the GameService properly", () => {
         getGameService.addGameSheet(gameSheet);
+
         expect(getGameService.getGameSheet(gameSheet.id)).to.equal(gameSheet);
     });
 
     it("should return the GameSheetDescription properly", () => {
         const expected: GameSheet[] = [gameSheet];
         const result: GameSheet[] = getGameService.getGameDescriptions();
+
         expect(result).to.deep.equal(expected);
     });
 
     it("should create an id with the correct length", () => {
         const ID_LENGTH: number = 25;
+
         expect(gameSheet.id.length).to.equals(ID_LENGTH);
     });
 
