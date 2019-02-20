629454bfb32206bf368573732dd23577d7148112
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-20 16:51:06 -0500

    [R] VM: ajout espaces entre les tests et avant les expects

--- server/app/services/game.spec.ts
+++ server/app/services/game.spec.ts
@@ -5,6 +5,7 @@ import { Position } from "./utils/circle-area";
 describe("Game", () => {
     const game: Game = new Game("0", "voiture");
     const ONE_SECOND: number = 1000;
+
     it("should restore the difference like the original image", () => {
         setTimeout(
             () => {
@@ -13,16 +14,19 @@ describe("Game", () => {
                 game.restoreModifiedImage(x, y);
                 const position: Position = {i: x, j: y };
                 const index: number = game.images[0].getIndex(position);
+
                 expect(game.images[0].pixelAt(index)).to.equals(game.images[1].pixelAt(index));
             },
             ONE_SECOND);
     });
+
     it("should not restore something in the modified image if not a difference", () => {
         setTimeout(
             () => {
                 game.restoreModifiedImage(0, 0);
                 const position: Position = {i: 0, j: 0};
                 const index: number = game.images[0].getIndex(position);
+
                 expect(game.images[0].pixelAt(index)).to.deep.equal(game.images[1].pixelAt(index));
             },
             ONE_SECOND);
