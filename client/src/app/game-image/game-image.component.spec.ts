a850aa8361cc33d954063f66cea57dbbf6d4c5f8
padal <pascal.dally-belanger@polymtl.ca>
2019-02-21 13:45:36 -0500

    PDB: tslint et identation R

--- client/src/app/game-image/game-image.component.spec.ts
+++ client/src/app/game-image/game-image.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { GameImageComponent } from "./game-image.component";
 
 describe("GameImageComponent", () => {
-  let component: GameImageComponent;
-  let fixture: ComponentFixture<GameImageComponent>;
+    let component: GameImageComponent;
+    let fixture: ComponentFixture<GameImageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GameImageComponent ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [GameImageComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GameImageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GameImageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
