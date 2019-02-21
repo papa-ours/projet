b895e74ff8faaa4ecda70977b988126ca3c6af4c
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-02-20 21:18:18 -0500

    PDB: ajout de tolérance à l'algorithme D

--- server/app/services/scene/geometry-intersection.ts
+++ server/app/services/scene/geometry-intersection.ts
@@ -16,11 +16,12 @@ export class GeometryIntersection {
     public static intersects(g1: GeometryData, g2: GeometryData): boolean {
         // tslint:disable-next-line:no-magic-numbers
         const LONGEST_LINE_IN_A_BOX: number = Math.sqrt(3) / 2;
+        const TOLERANCE: number = 5;
 
         const distance: VectorImpl = (g1.position as VectorImpl).sub(g2.position);
         const radius: number = LONGEST_LINE_IN_A_BOX * (g1.size + g2.size);
 
-        return distance.size2() <= radius * radius;
+        return distance.size2() + TOLERANCE <= radius * radius;
     }
 
 }
