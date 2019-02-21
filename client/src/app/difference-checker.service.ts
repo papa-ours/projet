eb87f5bc3e4fccc25c6900e4269eed6f5cc891bd
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-21 14:59:18 -0500

    VM: deplacement de la method mapValue() vers le service R

--- client/src/app/difference-checker.service.ts
+++ client/src/app/difference-checker.service.ts
@@ -9,12 +9,25 @@ import { Message } from "../../../common/communication/message";
 })
 export class DifferenceCheckerService {
 
+    private readonly IMAGE_WIDTH: number = 640;
+    private readonly IMAGE_HEIGHT: number = 480;
+    private readonly HTML_IMAGE_WIDTH: number = 500;
+    private readonly HTML_IMAGE_HEIGHT: number = 375;
+
     public readonly BASE_URL: string = "http://localhost:3000/api/difference/";
     public constructor(private http: HttpClient) { }
 
     public isPositionDifference(id: string, x: number, y: number): Observable<boolean> {
+        x = Math.floor(this.mapValue(x, 0, this.HTML_IMAGE_WIDTH, 0, this.IMAGE_WIDTH));
+        // We have to flip the y since the pixels are stored in bottom-up format
+        y = Math.floor(this.mapValue(y, 0, this.HTML_IMAGE_HEIGHT, this.IMAGE_HEIGHT, 0));
+
         return this.http.get<Message>(this.BASE_URL + id + "/" + x + "/" + y)
             .pipe(map((message: Message) => JSON.parse(message.body)),
         );
+    }
+
+    private mapValue(val: number, fromMin: number, fromMax: number, toMin: number, toMax: number): number {
+        return (val - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
     }
 }
