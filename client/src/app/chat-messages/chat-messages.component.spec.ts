2fcd1d74e0b6980abd4524ff96639e39d35d70e9
padal <pascal.dally-belanger@polymtl.ca>
2019-02-21 13:44:15 -0500

    PDB: indentation R

--- client/src/app/chat-messages/chat-messages.component.spec.ts
+++ client/src/app/chat-messages/chat-messages.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChatMessagesComponent } from './chat-messages.component';
 
 describe('ChatMessagesComponent', () => {
-  let component: ChatMessagesComponent;
-  let fixture: ComponentFixture<ChatMessagesComponent>;
+    let component: ChatMessagesComponent;
+    let fixture: ComponentFixture<ChatMessagesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ChatMessagesComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [ChatMessagesComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChatMessagesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ChatMessagesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        expect(component).toBeTruthy();
+    });
 });
