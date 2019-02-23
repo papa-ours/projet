4fcd516805be943fb85aadd03dcce28f2eef82bb
Vincent <vg_4_@hotmail.fr>
2019-02-23 13:22:45 -0500

    VG VM: Retour de GameLists au lieu d'un message

--- server/app/services/get-game-list.service.ts
+++ server/app/services/get-game-list.service.ts
@@ -1,8 +1,6 @@
-import { Response } from "express";
 import { inject, injectable } from "inversify";
 import "reflect-metadata";
 import { GameLists, GameType} from "../../../common/communication/game-description";
-import { Message, MessageType } from "../../../common/communication/message";
 import Types from "../types";
 import { GetGameService } from "./get-game.service";
 
@@ -12,17 +10,10 @@ export class GetGameListService {
     public constructor(@inject(Types.GetGameService) private getGameService: GetGameService) {
     }
 
-    public getGameList(res: Response): void {
-        const gameList: GameLists = {
+    public getGameList(): GameLists {
+        return {
             list2d: this.getGameService.getGameDescriptions(GameType.Simple),
             list3d: this.getGameService.getGameDescriptions(GameType.Free),
         };
-
-        const message: Message = {
-            type: MessageType.USERNAME_VALIDATION,
-            body: JSON.stringify(gameList),
-        };
-
-        res.send(message);
     }
 }
