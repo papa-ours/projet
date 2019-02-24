5543f28d10561270de1d71491624204416140f83
Vincent <vg_4_@hotmail.fr>
2019-02-24 15:25:50 -0500

    VG: Correction des tests T

--- client/src/app/username-validation-service.service.spec.ts
+++ client/src/app/username-validation-service.service.spec.ts
@@ -1,14 +1,38 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
 import { UsernameValidationService } from "./username-validation-service.service";
 
-describe("UsernameValidationServiceService", () => {
+describe("UsernameValidationService", () => {
     let usernameValidationService: UsernameValidationService;
+    let httpMock: HttpTestingController;
+
     beforeEach(() => {
-        usernameValidationService = new UsernameValidationService();
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [UsernameValidationService],
+        });
+        usernameValidationService = TestBed.get(UsernameValidationService);
+        httpMock = TestBed.get(HttpTestingController);
     });
+
     it("should be created", () => {
         expect(usernameValidationService).toBeTruthy();
     });
-    it("it should return an observable on request", () => {
-        expect(usernameValidationService.getUsernameValidation()).toBeDefined();
+
+    it("should return a validation string", () => {
+        usernameValidationService.getUsernameValidation("name")
+            .subscribe((data: string) => expect(data).toBeDefined());
+    });
+
+    it("should be a POST REQUEST", () => {
+        const request: TestRequest = httpMock.expectOne(`${usernameValidationService.BASE_URL}name`);
+        expect(request.request.method).toBe("POST");
+        httpMock.verify();
+    });
+
+    it("should be a DELETE REQUEST", () => {
+        const request: TestRequest = httpMock.expectOne(`${usernameValidationService.BASE_URL}delete/name`);
+        expect(request.request.method).toBe("DELETE");
+        httpMock.verify();
     });
 });
