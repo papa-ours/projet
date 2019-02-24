1f9b4fd8dcb4927fe706e8a788e8c34024dd8418
Vincent <vg_4_@hotmail.fr>
2019-02-24 18:35:13 -0500

    VG: Test pour l'ajout d'un user T

---
+++ server/app/services/users-container.service.spec.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import { UsersContainerService } from "./users-container.service";
+
+describe.only("UsersContainerService", () => {
+    const usersContainerService: UsersContainerService = new UsersContainerService();
+    const name: string = "test-user";
+    const id: string = "test-id";
+
+    it("should add the user properly", () => {
+        usersContainerService.addUser({name: name, socketId: id});
+        const result: string = UsersContainerService.usernames[0];
+
+        expect(result).to.equals(name);
+    });
+});
