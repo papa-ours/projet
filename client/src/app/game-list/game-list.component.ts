68674a58b207266f7c870d649d46818ac2ec50bb
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-02-24 14:28:57 -0500

    PDB: indentation R

--- client/src/app/game-list/game-list.component.ts
+++ client/src/app/game-list/game-list.component.ts
@@ -4,9 +4,9 @@ import { GameSheet } from "../../../../common/communication/game-description";
 import { GameListService } from "../game-list-getter.service";
 
 @Component({
-  selector: "app-game-list",
-  templateUrl: "./game-list.component.html",
-  styleUrls: ["./game-list.component.css"],
+    selector: "app-game-list",
+    templateUrl: "./game-list.component.html",
+    styleUrls: ["./game-list.component.css"],
 })
 export class GameListComponent {
 
