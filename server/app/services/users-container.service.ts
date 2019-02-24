7eca000b4c0f477cd0bd815eaa82ace6a5da2034
Vincent <vg_4_@hotmail.fr>
2019-02-24 16:59:29 -0500

    VG: AJout d'une méthode addUser D

--- server/app/services/users-container.service.ts
+++ server/app/services/users-container.service.ts
@@ -1,1 +1,9 @@
-export class UsersContainerService {}
+import { User } from "./user";
+
+export class UsersContainerService {
+    private static users: Map<string, string> = new Map();
+
+    public addUser(user: User): void {
+        UsersContainerService.users.set(user.socketId, user.name);
+    }
+}
