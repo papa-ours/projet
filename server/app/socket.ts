6acdaf045aea429109f1650b34651e1c6ae85a64
Vincent <vg_4_@hotmail.fr>
2019-02-24 14:33:04 -0500

    VG: Retrait des choses inutilies dans le socket D

--- server/app/socket.ts
+++ server/app/socket.ts
@@ -1,18 +1,12 @@
 import * as http from "http";
-import { inject, injectable } from "inversify";
+import { injectable } from "inversify";
 import * as socketio from "socket.io";
-import { Message } from "../../common/communication/message";
 import { DBConnectionService } from "./services/dbconnection.service";
 import { User } from "./services/user";
-import { UsernameValidatorService } from "./services/username-validator.service";
-import Types from "./types";
 
 @injectable()
 export class Socket {
     private io: SocketIO.Server;
-
-    public constructor(
-        @inject(Types.UsernameValidatorService) private usernameValidatorService: UsernameValidatorService) {}
 
     public init(server: http.Server): void {
         this.io = socketio(server);
@@ -20,21 +14,8 @@ export class Socket {
         this.io.on("connection", (socket: SocketIO.Socket) => {
             const currentUser: User = {name: ""};
 
-            this.setupValidation(socket, currentUser);
             this.setupDisconnect(socket, currentUser);
             this.setupDeleteUser(socket);
-        });
-    }
-
-    private setupValidation(socket: SocketIO.Socket, user: User): void {
-        socket.on("requestUsernameValidation", async (username: string) => {
-            const message: Message = await this.usernameValidatorService.getUsernameValidation(username);
-
-            if (message.body === "") {
-                user.name = username;
-                DBConnectionService.getInstance().addUser(user);
-            }
-            socket.emit("validation", message);
         });
     }
 
