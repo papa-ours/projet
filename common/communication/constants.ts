3bd0742dbfdad62e1d40843ff398b818addc9579
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-24 15:29:33 -0500

    VM: ajout de constantes pour les nombres de differences R

--- common/communication/constants.ts
+++ common/communication/constants.ts
@@ -2,3 +2,6 @@ export const LOCAL_HOST_PORT: string = "http://localhost:3000"
 
 export const WIDTH: number = 640;
 export const HEIGHT: number = 480;
+
+export const REQUIRED_DIFFERENCES_1P: number = 7;
+export const REQUIRED_DIFFERENCES_2P: number = 4;
