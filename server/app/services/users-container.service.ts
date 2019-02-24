97dd85d51cf3019a1b03825c5144f7ac987f4e82
Vincent <vg_4_@hotmail.fr>
2019-02-24 17:02:08 -0500

    VG: Les users sont un arrat aulieu d'un Map R

--- server/app/services/users-container.service.ts
+++ server/app/services/users-container.service.ts
@@ -1,9 +1,9 @@
 import { User } from "./user";
 
 export class UsersContainerService {
-    private static users: Map<string, string> = new Map();
+    private static users: User[] = [];
 
     public addUser(user: User): void {
-        UsersContainerService.users.set(user.socketId, user.name);
+        UsersContainerService.users.push(user);
     }
 }
