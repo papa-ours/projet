ca9a88db89b8a0186b14e896b78c01d75e47fe64
Vincent <vg_4_@hotmail.fr>
2019-02-24 13:46:25 -0500

    VG: Création du UsernameValidatorController D

---
+++ server/app/controllers/username-validator.controller.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { inject, injectable } from "inversify";
+import { Message, MessageType } from "../../../common/communication/message";
+import { UsernameValidatorService } from "../services/username-validator.service";
+import Types from "../types";
+
+@injectable()
+export class UsernameValidatorController {
+
+    public constructor(@inject(Types.UsernameValidatorService) private usernameValidatorService: UsernameValidatorService) { }
+
+    public get router(): Router {
+        const router: Router = Router();
+
+        router.post(
+            "/username",
+            (req: Request, res: Response, next: NextFunction) => {
+                res.send(this.usernameValidatorService.getUsernameValidation(req.body.name));
+            });
+
+        return router;
+    }
+}
