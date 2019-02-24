93b92828a1ecbc4cc37098ff5e716891200e6d79
David <david.piche@polymtl.ca>
2019-02-24 17:43:29 -0500

    DP: Initilisation de test avec mocks T

--- client/src/app/scene3d/get-scene-data.service.spec.ts
+++ client/src/app/scene3d/get-scene-data.service.spec.ts
@@ -1,9 +1,19 @@
 import { TestBed } from "@angular/core/testing";
-
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
 import { GetSceneDataService } from "./get-scene-data.service";
+import { SceneData } from "../../../../common/communication/geometry";
 
 describe("GetSceneDataService", () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    let getSceneDataService: GetSceneDataService;
+    let httpMock: HttpTestingController;
+    beforeEach(() => {
+        TestBed.configureTestingModule({   
+                imports: [HttpClientTestingModule],
+                providers: [GetSceneDataService],
+            });
+    getSceneDataService = TestBed.get(GetSceneDataService);
+    httpMock = TestBed.get(HttpTestingController);
+    });
 
     it("should be created", () => {
         const service: GetSceneDataService = TestBed.get(GetSceneDataService);
