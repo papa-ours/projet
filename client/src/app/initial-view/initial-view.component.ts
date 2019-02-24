940c0d7a7189de8e9964cd45a9da2ae060c1410d
Vincent <vg_4_@hotmail.fr>
2019-02-24 15:08:50 -0500

    VG: subscribe au delete du username D

--- client/src/app/initial-view/initial-view.component.ts
+++ client/src/app/initial-view/initial-view.component.ts
@@ -23,7 +23,7 @@ export class InitialViewComponent implements OnInit {
     }
 
     private deleteUsername(): void {
-        this.usernameValidationService.deleteUsername();
+        this.usernameValidationService.deleteUsername().subscribe();
         this.usernameValidationService.connected = false;
     }
 
