f52b0c6ce837ad0852739c75ae0453215ec8febd
Vincent <vg_4_@hotmail.fr>
2019-02-24 11:43:47 -0500

    VG: initialisation du socket D

--- client/src/app/socket.service.ts
+++ client/src/app/socket.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from "@angular/core";
+import * as io from "socket.io-client";
 
 @Injectable({
     providedIn: "root",
 })
 export class SocketService {
+    // @ts-ignore
+    private socket: SocketIOClient.Socket;
+    private readonly BASE_URL: string = "http://localhost:3000";
 
+    public constructor() {
+        this.socket = io(this.BASE_URL);
+    }
 }
