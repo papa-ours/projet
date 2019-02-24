799a54a844433c0264f0b1e882737741d824d564
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-02-23 21:10:34 -0500

    VM: usage des constantes WIDTH et HEIGHT du fichier common a la place R

--- client/src/app/difference-checker.service.ts
+++ client/src/app/difference-checker.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { LOCAL_HOST_PORT } from "../../../common/communication/constants";
+import { HEIGHT, LOCAL_HOST_PORT, WIDTH } from "../../../common/communication/constants";
 import { Message } from "../../../common/communication/message";
 
 @Injectable({
@@ -10,8 +10,6 @@ import { Message } from "../../../common/communication/message";
 })
 export class DifferenceCheckerService {
 
-    private readonly IMAGE_WIDTH: number = 640;
-    private readonly IMAGE_HEIGHT: number = 480;
     private readonly HTML_IMAGE_WIDTH: number = 500;
     private readonly HTML_IMAGE_HEIGHT: number = 375;
 
@@ -19,8 +17,8 @@ export class DifferenceCheckerService {
     public constructor(private http: HttpClient) { }
 
     public isPositionDifference(id: string, x: number, y: number): Observable<boolean> {
-        x = Math.floor(this.mapValue(x, 0, this.HTML_IMAGE_WIDTH, 0, this.IMAGE_WIDTH));
-        y = Math.floor(this.mapValue(y, 0, this.HTML_IMAGE_HEIGHT, this.IMAGE_HEIGHT, 0));
+        x = Math.floor(this.mapValue(x, 0, this.HTML_IMAGE_WIDTH, 0, WIDTH));
+        y = Math.floor(this.mapValue(y, 0, this.HTML_IMAGE_HEIGHT, HEIGHT, 0));
 
         return this.http.get<Message>(this.BASE_URL + id + "/" + x + "/" + y)
             .pipe(map((message: Message) => JSON.parse(message.body)),
