b8f855ec9fd1b54bdccee5e3cd407ba7ec90b0d2
Vincent <vg_4_@hotmail.fr>
2019-02-24 16:42:27 -0500

    VG: Initialisation des attributs dnas le constructeur R

--- client/src/app/connection.service.ts
+++ client/src/app/connection.service.ts
@@ -4,18 +4,22 @@ import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Message } from "../../../common/communication/message";
 import { SocketService } from "./socket.service";
+import { StringMap } from "@angular/core/src/render3/jit/compiler_facade_interface";
 
 @Injectable()
 export class ConnectionService {
 
-    public connected: boolean = false;
-    public username: string = "";
+    public connected: boolean;
+    public username: String;
     public readonly BASE_URL: string = "http://localhost:3000/api/user/";
 
     public constructor(
         public socketService: SocketService,
         private http: HttpClient,
-    ) {}
+    ) {
+        this.connected = false;
+        this.username = "";
+    }
 
     public deleteUsername(): Observable<void> {
         return this.http.delete<void>(this.BASE_URL + "delete/" + this.username);
