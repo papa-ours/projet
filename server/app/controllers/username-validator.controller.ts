dc6c07253465520c25830bd106060a2193f3d31e
Vincent <vg_4_@hotmail.fr>
2019-02-24 17:10:32 -0500

    VG: Utilisation du UsersContainerService D

--- server/app/controllers/username-validator.controller.ts
+++ server/app/controllers/username-validator.controller.ts
@@ -1,14 +1,16 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
 import { Message } from "../../../common/communication/message";
-import { DBConnectionService } from "../services/dbconnection.service";
 import { UsernameValidatorService } from "../services/username-validator.service";
+import { UsersContainerService } from "../services/users-container.service";
 import Types from "../types";
 
 @injectable()
 export class UsernameValidatorController {
 
-    public constructor(@inject(Types.UsernameValidatorService) private usernameValidatorService: UsernameValidatorService) { }
+    public constructor(
+        @inject(Types.UsernameValidatorService) private usernameValidatorService: UsernameValidatorService,
+        @inject(Types.UsersContainerService) private usersContainerService: UsersContainerService) { }
 
     public get router(): Router {
         const router: Router = Router();
@@ -20,7 +22,7 @@ export class UsernameValidatorController {
 
                 if (message.body === "") {
                     const PREFIX: string = "/#";
-                    DBConnectionService.getInstance().addUser({name: req.body.name, socketId: `${PREFIX}${req.body.socketId}`});
+                    this.usersContainerService.addUser({name: req.body.name, socketId: `${PREFIX}${req.body.socketId}`});
                 }
 
                 res.send(message);
@@ -29,7 +31,7 @@ export class UsernameValidatorController {
         router.delete(
             "/delete/:name",
             (req: Request, res: Response, next: NextFunction) => {
-                DBConnectionService.getInstance().deleteUserByName(req.params.name);
+                this.usersContainerService.deleteUserByName(req.params.name);
                 res.send();
             });
 
