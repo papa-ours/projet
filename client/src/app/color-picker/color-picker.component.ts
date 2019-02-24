51d491b435ab3a357cc8ce8982455301625f798d
Vincent <vg_4_@hotmail.fr>
2019-02-24 00:13:18 -0500

    VG: Ajout d'un attribut ColorPalette[] D

--- client/src/app/color-picker/color-picker.component.ts
+++ client/src/app/color-picker/color-picker.component.ts
@@ -1,10 +1,26 @@
 import { Component } from "@angular/core";
 
+interface ColorPalette {
+    primary: string;
+    secondary: string;
+}
+
 @Component({
     selector: "app-color-picker",
     templateUrl: "./color-picker.component.html",
     styleUrls: ["./color-picker.component.css"],
 })
 export class ColorPickerComponent {
+
+    public colorPalettes: ColorPalette[];
+
+    public constructor() {
+        this.colorPalettes = [
+            {primary: "#8AC4FF", secondary: "#379bff"},
+            {primary: "#FF0000", secondary: "#379bff"},
+            {primary: "#00FF00", secondary: "#379bff"},
+            {primary: "#FFFF00", secondary: "#379bff"},
+        ];
+    }
 
 }
