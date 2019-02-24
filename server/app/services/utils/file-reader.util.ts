197a08be5dfdf452426a7de8447202d82e5a6926
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-02-24 18:42:35 -0500

    PDB: correction d'erreur ts-lint R

--- server/app/services/utils/file-reader.util.ts
+++ server/app/services/utils/file-reader.util.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 
 export class FileReaderUtil {
-    public static readFile(path: string): Promise<Buffer> {
+    public static async readFile(path: string): Promise<Buffer> {
         return new Promise((resolve: Function, reject: Function) => {
             fs.readFile(path, (err: Error, data: Buffer) => {
                 if (err) {
