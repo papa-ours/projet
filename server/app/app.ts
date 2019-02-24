eb9ca6a83f194f0478554cd3c86ec295d695c60a
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-02-24 16:09:20 -0500

    PDB: formatage R

--- server/app/app.ts
+++ server/app/app.ts
@@ -19,12 +19,13 @@ export class Application {
     public app: express.Application;
 
     public constructor(
-            @inject(Types.GetGameListController) private getGameListController: GetGameListController,
-            @inject(Types.GetGameController) private getGameController: GetGameController,
-            @inject(Types.GameSheetGenerationController) private gameSheetGenerationController: GameSheetGenerationController,
-            @inject(Types.SceneDataController) private sceneDataController: SceneDataController,
-            @inject(Types.DifferenceImageController) private differenceImageController: DifferenceImageController,
-            @inject(Types.DifferenceCheckerController) private differenceCheckerController: DifferenceCheckerController) {
+        @inject(Types.GetGameListController) private getGameListController: GetGameListController,
+        @inject(Types.GetGameController) private getGameController: GetGameController,
+        @inject(Types.GameSheetGenerationController) private gameSheetGenerationController: GameSheetGenerationController,
+        @inject(Types.SceneDataController) private sceneDataController: SceneDataController,
+        @inject(Types.DifferenceImageController) private differenceImageController: DifferenceImageController,
+        @inject(Types.DifferenceCheckerController) private differenceCheckerController: DifferenceCheckerController,
+    ) {
         this.app = express();
 
         this.config();
