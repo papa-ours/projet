82943f387785bce667e9d47354693a81dd101278
Vincent <vg_4_@hotmail.fr>
2019-02-25 15:17:16 -0500

    VG: Rajout de DBConnectionService comme il était avant D

---
+++ server/app/services/db-connection.service.ts
@@ -0,0 +1,46 @@
+import { injectable } from "inversify";
+import * as mongoose from "mongoose";
+import "reflect-metadata";
+import { GameSheetDescription, GameType } from "../../../common/communication/game-description";
+
+@injectable()
+export class DBConnectionService {
+    private static instance: DBConnectionService;
+
+    private readonly uri: string = "mongodb+srv://ving34:pass123@cluster0-m1gwf.mongodb.net/test?retryWrites=true";
+    public readonly gameSheetSchema: mongoose.Schema = new mongoose.Schema({
+        name: String,
+        id: String,
+        topScores: Array,
+        type: Number,
+    });
+    public connected: boolean = false;
+
+    public constructor() {
+        this.connect();
+    }
+
+    public static getInstance(): DBConnectionService {
+        if (!DBConnectionService.instance) {
+            DBConnectionService.instance = new DBConnectionService();
+        }
+
+        return DBConnectionService.instance;
+    }
+
+    public async connect(): Promise<typeof mongoose> {
+        return mongoose.connect(this.uri, {
+            useNewUrlParser: true,
+        });
+    }
+
+    public async saveGameSheet2D(gameSheetDescription: GameSheetDescription): Promise<mongoose.Document> {
+        const gameSheet: mongoose.Document = new mongoose.models.GameSheet2D(gameSheetDescription);
+
+        return gameSheet.save();
+    }
+
+    public async getGameSheets(type: GameType): Promise<mongoose.Document[]> {
+        return mongoose.models.GameSheet2D.find({}).exec();
+    }
+}
