24164248b2da40a588e001b4293b9eaa406de2c0
David <david.piche@polymtl.ca>
2019-02-24 21:59:25 -0500

    DP: Ajout de async pour les subscribe des tests R

--- client/src/app/difference-checker.service.spec.ts
+++ client/src/app/difference-checker.service.spec.ts
@@ -21,7 +21,7 @@ describe("DifferenceCheckerService", () => {
 
     it("should be a GET REQUEST", () => {
         differenceCheckerService.isPositionDifference("0", 0, 0)
-            .subscribe((data: boolean) => expect(data).toBeDefined());
+            .subscribe(async (data: boolean) => expect(data).toBeDefined());
         const request: TestRequest = httpMock.expectOne(`${differenceCheckerService.BASE_URL}0/0/480`);
         expect(request.request.method).toBe("GET");
         httpMock.verify();
@@ -29,7 +29,7 @@ describe("DifferenceCheckerService", () => {
 
     it("should return a JSON", () => {
         differenceCheckerService.isPositionDifference("0", 0, 0)
-            .subscribe((data: boolean) => expect(data).toBeDefined());
+            .subscribe(async (data: boolean) => expect(data).toBeDefined());
         const request: TestRequest = httpMock.expectOne(`${differenceCheckerService.BASE_URL}0/0/480`);
         expect(request.request.responseType).toBe("json");
         httpMock.verify();
