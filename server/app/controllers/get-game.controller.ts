19b2364fad17ac63c64f42d9fb70dbe1b5935b1b
Vincent <vg_4_@hotmail.fr>
2019-02-24 19:33:45 -0500

    VG: Retrait des routes inutiles D

--- server/app/controllers/get-game.controller.ts
+++ server/app/controllers/get-game.controller.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
 import { Message, MessageType } from "../../../common/communication/message";
-import { ImageType } from "../../../common/images/image-type";
-import { Game } from "../services/game";
 import { GetGameService } from "../services/get-game.service";
 import Types from "../types";
 
@@ -13,63 +11,6 @@ export class GetGameController {
 
     public get router(): Router {
         const router: Router = Router();
-
-        router.get(
-            "/:id/differenceImage",
-            (req: Request, res: Response, next: NextFunction) => {
-                const message: Message = {
-                    type: MessageType.GAME_SHEET_GENERATION,
-                    body: "",
-                };
-
-                try {
-                    const game: Game = this.getGameService.getGame(req.params.id);
-                    const imageData: string = game.differenceImage.toArray().toString();
-                    message.body = imageData;
-                } catch (err) {
-                    message.body = err.message;
-                }
-
-                res.send(message);
-            });
-
-        router.get(
-            "/:id/originalImage",
-            (req: Request, res: Response, next: NextFunction) => {
-                const message: Message = {
-                    type: MessageType.GAME_SHEET_GENERATION,
-                    body: "",
-                };
-
-                try {
-                    const game: Game = this.getGameService.getGame(req.params.id);
-                    const imageData: string = game.images[ImageType.Original].toArray().toString();
-                    message.body = imageData;
-                } catch (err) {
-                    message.body = err.message;
-                }
-
-                res.send(message);
-            });
-
-        router.get(
-            "/:id/modifiedImage",
-            (req: Request, res: Response, next: NextFunction) => {
-                const message: Message = {
-                    type: MessageType.GAME_SHEET_GENERATION,
-                    body: "",
-                };
-
-                try {
-                    const game: Game = this.getGameService.getGame(req.params.id);
-                    const imageData: string = game.images[ImageType.Modified].toArray().toString();
-                    message.body = imageData;
-                } catch (err) {
-                    message.body = err.message;
-                }
-
-                res.send(message);
-            });
 
         router.get(
             "/:name",
