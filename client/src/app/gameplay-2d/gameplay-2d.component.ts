8d921e9f0b5635df1c17d38c9103d87f0c30807b
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-02-25 12:24:17 -0500

    MC : decouplage du component gameplay -view components D

--- client/src/app/gameplay-2d/gameplay-2d.component.ts
+++ client/src/app/gameplay-2d/gameplay-2d.component.ts
@@ -1,12 +1,58 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { SERVER_ADDRESS } from "../../../../common/communication/constants";
+import { GameType } from "../../../../common/communication/game-description";
+import { ImageType } from "../../../../common/images/image-type";
+import { DifferenceCheckerService } from "../difference-checker.service";
 
 @Component({
   selector: "app-gameplay-2d",
   templateUrl: "./gameplay-2d.component.html",
   styleUrls: ["./gameplay-2d.component.css"],
 })
-export class Gameplay2DComponent {
+export class Gameplay2DComponent implements OnInit {
 
-  public constructor() { }
+    @Input() private name: string;
+    @Input() private id: string;
+    public images: string[];
+    public type: GameType;
+    private foundDifferencesCounter: number;
+    @Output() public foundDifferenceEvent: EventEmitter<number>;
+
+    public constructor(
+        private differenceCheckerService: DifferenceCheckerService,
+    ) {
+        this.images = [];
+        this.foundDifferencesCounter = 0;
+        this.foundDifferenceEvent = new EventEmitter<number>();
+    }
+    public ngOnInit(): void {
+        this.setImagesPath();
+    }
+
+    private setImagesPath(): void {
+        this.images[ImageType.Original] = `${SERVER_ADDRESS}/${this.name}-originalImage.bmp`;
+        this.images[ImageType.Modified] = `${SERVER_ADDRESS}/${this.name}-modifiedImage.bmp`;
+    }
+
+    public checkDifference(position: [number, number]): void {
+        this.differenceCheckerService.isPositionDifference(this.id, position[0], position[1])
+            .subscribe((isDifference: boolean) => {
+                if (isDifference) {
+                    this.differenceFound();
+                }
+            },
+        );
+    }
+
+    private differenceFound(): void {
+        this.foundDifferencesCounter++;
+        this.foundDifferenceEvent.emit(this.foundDifferencesCounter);
+        this.updateDifferenceImage();
+
+    }
+
+    private updateDifferenceImage(): void {
+        this.images[ImageType.Modified] = `${SERVER_ADDRESS}/${this.id}.bmp?${this.foundDifferencesCounter}`;
+    }
 
 }
--- client/src/app/gameplay-view/gameplay-view.component.ts
+++ client/src/app/gameplay-view/gameplay-view.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { faHourglassHalf, IconDefinition } from "@fortawesome/free-solid-svg-icons";
-import { REQUIRED_DIFFERENCES_1P, REQUIRED_DIFFERENCES_2P, SERVER_ADDRESS } from "../../../../common/communication/constants";
+import { REQUIRED_DIFFERENCES_1P, REQUIRED_DIFFERENCES_2P } from "../../../../common/communication/constants";
 import { GameType } from "../../../../common/communication/game-description";
-import { ImageType } from "../../../../common/images/image-type";
-import { DifferenceCheckerService } from "../difference-checker.service";
 import { GameplayService } from "../gameplay.service";
 
 @Component({
@@ -19,23 +17,21 @@ export class GameplayViewComponent implements OnInit {
     public readonly nbPlayers: number;
 
     public foundDifferencesCounter: number;
-    private name: string;
-    private id: string;
     public images: string[];
     public requiredDifferences: number;
     public type: GameType;
 
     public constructor(
         private route: ActivatedRoute,
-        private differenceCheckerService: DifferenceCheckerService,
         private gameplayService: GameplayService,
+        public name: string,
+        public id: string,
     ) {
         this.nbPlayers = 1;
         this.requiredDifferences = this.nbPlayers === 1 ? REQUIRED_DIFFERENCES_1P : REQUIRED_DIFFERENCES_2P;
         this.foundDifferencesCounter = 0;
         this.images = [];
     }
-
     public ngOnInit(): void {
         this.route.params.subscribe((params: Params) => {
             this.name = params["name"];
@@ -43,33 +39,12 @@ export class GameplayViewComponent implements OnInit {
             this.gameplayService.getGameId(this.name, this.type).subscribe((id: string) => {
                 this.id = id;
             });
-            this.setImagesPath();
         });
     }
 
-    private setImagesPath(): void {
-        this.images[ImageType.Original] = `${SERVER_ADDRESS}/${this.name}-originalImage.bmp`;
-        this.images[ImageType.Modified] = `${SERVER_ADDRESS}/${this.name}-modifiedImage.bmp`;
-    }
-
-    public checkDifference(position: [number, number]): void {
-        this.differenceCheckerService.isPositionDifference(this.id, position[0], position[1])
-            .subscribe((isDifference: boolean) => {
-                if (isDifference) {
-                    this.differenceFound();
-                }
-            },
-        );
-    }
-
-    private differenceFound(): void {
-        this.foundDifferencesCounter++;
-        this.updateDifferenceImage();
+    public updateView(nDifference: number): void {
+        this.foundDifferencesCounter = nDifference;
         this.playSound();
-    }
-
-    private updateDifferenceImage(): void {
-        this.images[ImageType.Modified] = `${SERVER_ADDRESS}/${this.id}.bmp?${this.foundDifferencesCounter}`;
     }
 
     private playSound(): void {
