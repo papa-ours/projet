9aec3b25252980065d930e28e90d22e3aac9cab2
David <david.piche@polymtl.ca>
2019-02-25 11:33:03 -0500

    DP: Ajout de valeur de retour de methode D

--- client/src/app/scene3d/deplacement-camera.service.ts
+++ client/src/app/scene3d/deplacement-camera.service.ts
@@ -1,6 +1,6 @@
 export class DeplacementCameraService{
 
-    public static keyPress(data: string){
+    public static keyPress(data: string): void {
         const render = document.getElementById(data);
         if (!!render) {
             render.addEventListener("keypress", (e: KeyboardEvent) => {
