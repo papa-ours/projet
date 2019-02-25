ddfab0622348a5867a85356f29ae22ee7b24a799
David <david.piche@polymtl.ca>
2019-02-25 11:51:10 -0500

    DP: Changement de nom de variable R

--- client/src/app/scene3d/deplacement-camera.service.ts
+++ client/src/app/scene3d/deplacement-camera.service.ts
@@ -3,10 +3,10 @@ import { RenderService } from "./render.service";
 export class DeplacementCameraService{
 
     public static keyPress(data: string, render3d: RenderService): void {
-        const render = document.getElementById(data);
-        if (!!render) {
-            render.addEventListener("keypress", (e: KeyboardEvent) => {
-
+        const scene3d = document.getElementById(data);
+        if (!!scene3d) {
+            scene3d.addEventListener("keypress", (e: KeyboardEvent) => {
+            
             });
         }
     }
