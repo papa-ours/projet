bd3fd1409c8a0ae4ed7c6d82b0c84518ac61af8a
Vincent <vg_4_@hotmail.fr>
2019-02-25 12:24:08 -0500

    VG: Ajout d'une méthode readFile D

--- server/app/services/utils/aws-file-reader.ts
+++ server/app/services/utils/aws-file-reader.ts
@@ -1,9 +1,14 @@
 import * as aws from "aws-sdk";
+import { PromiseResult } from "aws-sdk/lib/request";
 
 export class S3FileReader {
     public s3: aws.S3;
 
     public constructor() {
         this.s3 = new aws.S3();
+    }
+
+    public async readFile(fileName: string): Promise<PromiseResult<aws.S3.GetObjectOutput, aws.AWSError>> {
+        return this.s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
     }
 }
