fc1d5bc9561696ec3c410a3ac59d82f420f8123f
Vincent <vg_4_@hotmail.fr>
2019-02-25 13:02:44 -0500

    VG: Tout est statique dans la classe R

--- server/app/services/utils/aws-file-reader.ts
+++ server/app/services/utils/aws-file-reader.ts
@@ -2,13 +2,9 @@ import * as aws from "aws-sdk";
 import { PromiseResult } from "aws-sdk/lib/request";
 
 export class S3FileReader {
-    public s3: aws.S3;
+    public static s3: aws.S3 = new aws.S3();
 
-    public constructor() {
-        this.s3 = new aws.S3();
-    }
-
-    public async readFile(fileName: string): Promise<PromiseResult<aws.S3.GetObjectOutput, aws.AWSError>> {
-        return this.s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
+    public static async readFile(fileName: string): Promise<PromiseResult<aws.S3.GetObjectOutput, aws.AWSError>> {
+        return S3FileReader.s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
     }
 }
