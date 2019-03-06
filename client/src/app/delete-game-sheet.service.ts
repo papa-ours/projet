3fb67d65608c7fd88709885b34970f2df3edec5c
Vincent <vg_4_@hotmail.fr>
2019-03-06 10:47:17 -0500

    VG: Implémentation de la communication avec le servuer pour la suppression d'une gamesheet D

--- client/src/app/delete-game-sheet.service.ts
+++ client/src/app/delete-game-sheet.service.ts
@@ -1,3 +1,11 @@
+import { of, Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+import { SERVER_ADDRESS } from "../../../common/communication/constants";
+import { GameType } from "../../../common/communication/game-description";
+import { Message } from "../../../common/communication/message";
+
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable({
@@ -5,4 +13,16 @@ import { Injectable } from "@angular/core";
 })
 export class DeleteGameSheetService {
 
+    public readonly URL: string = `${SERVER_ADDRESS}/api/game/sheet`;
+    public constructor(private http: HttpClient) { }
+
+    public deleteGameSheet(id: string, type: GameType): Observable<Message> {
+        return this.http.delete<Message>(`${this.URL}/${id}/${type}`)
+            .pipe(catchError(this.handleError<Message>("deleteGameSheet")),
+        );
+    }
+
+    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+        return (error: Error): Observable<T> => of(result as T);
+    }
 }
