f9a61b60bd28445b4b66086545f297ea2a8fad19
Vincent <vg_4_@hotmail.fr>
2019-03-06 10:19:14 -0500

    VG: Ajout de 4 couleurs D

--- client/src/app/color-picker/color-picker.component.ts
+++ client/src/app/color-picker/color-picker.component.ts
@@ -26,6 +26,10 @@ export class ColorPickerComponent {
             {primary: "#F4429B", secondary: "#D1297D"},
             {primary: "#F73D1D", secondary: "#FF6247"},
             {primary: "#FF8B1E", secondary: "#FFB049"},
+            {primary: "#BEC619", secondary: "#F2FF00"},
+            {primary: "#2CA02C", secondary: "#35D642"},
+            {primary: "#9608CE", secondary: "#B826F2"},
+            {primary: "#07C4BA", secondary: "#1AEFE4"},
         ];
 
         this.colorSelected(this.colorPalettes[0]);
