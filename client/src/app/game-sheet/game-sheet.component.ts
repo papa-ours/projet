c0690aa93040a7b50b3e766e8239693dc49b425c
Vincent <vg_4_@hotmail.fr>
2019-03-06 10:47:42 -0500

    VG: Utilisation du DeleteGameSheetService D

--- client/src/app/game-sheet/game-sheet.component.ts
+++ client/src/app/game-sheet/game-sheet.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { SERVER_ADDRESS } from "../../../../common/communication/constants";
 import { GameSheet, GameType } from "../../../../common/communication/game-description";
+import { DeleteGameSheetService } from "../delete-game-sheet.service";
 
 @Component({
     selector: "app-game-sheet",
@@ -21,7 +22,7 @@ export class GameSheetComponent implements OnInit {
     @ViewChild("btn1") private btn1: ElementRef;
     @ViewChild("btn2") private btn2: ElementRef;
 
-    public constructor(private router: Router) {
+    public constructor(private router: Router, private deleteGameSheetService: DeleteGameSheetService) {
         this.source = "";
         this.isAdmin = false;
     }
@@ -35,7 +36,12 @@ export class GameSheetComponent implements OnInit {
     }
 
     public play(): void {
-        if (!this.isAdmin) {
+        if (this.isAdmin) {
+            this.deleteGameSheetService.deleteGameSheet(this.description.id, this.type)
+                .subscribe(() => {
+                    location.reload();
+                });
+        } else {
             this.router.navigateByUrl(`/game/${this.description.name}/${this.type}`)
             .catch((err: Error) => {
                 console.error(err);
