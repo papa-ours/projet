3f90c66453920ba9d25caf38819c5ccf1d8b397e
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-07 17:24:53 -0500

    MC: transformation des coordonne de la souris en NDC D

--- client/src/app/scene3d/raycaster.service.ts
+++ client/src/app/scene3d/raycaster.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from "@angular/core";
+import * as THREE from "three";
+import { RenderService } from "./render.service";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: "root",
 })
 export class RaycasterService {
+    private mouse: THREE.Vector2;
+    private rayCaster: THREE.Raycaster;
 
-  public constructor() { }
+    public constructor(private renderService: RenderService) {
+       this.mouse = new THREE.Vector2();
+       this.rayCaster = new THREE.Raycaster();
+    }
+    private computeMousePosition(event: MouseEvent, container: HTMLDivElement) {
+        const boundingRect: ClientRect = container.getBoundingClientRect();
+        this.mouse.x = 2 * ( (event.clientX - boundingRect.left) / container.clientWidth ) - 1;
+        this.mouse.y = 1 - 2* ( (event.clientY - boundingRect.top)/ container.clientHeight );
+    }
+
 }
