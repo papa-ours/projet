04d8e7eb0ddfcea3dce60bd2d03cf7d96b8b0b9a
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-03-06 22:55:37 -0500

    VM: implementation d'un scroll to bottom D

--- client/src/app/chat-messages/chat-messages.component.ts
+++ client/src/app/chat-messages/chat-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { ChatMessage } from "../../../../common/communication/message";
 import { SocketService } from "../socket.service";
 
@@ -7,8 +7,10 @@ import { SocketService } from "../socket.service";
     templateUrl: "./chat-messages.component.html",
     styleUrls: ["./chat-messages.component.css"],
 })
-export class ChatMessagesComponent implements OnInit {
+export class ChatMessagesComponent implements OnInit, AfterViewChecked {
     public chatMessages: ChatMessage[];
+
+    @ViewChild("chatContainer") private chatContainer: ElementRef;
 
     public constructor(private socketService: SocketService) {
         this.chatMessages = [];
@@ -18,5 +20,12 @@ export class ChatMessagesComponent implements OnInit {
         this.socketService.getChatMessage().subscribe((message: ChatMessage) => {
             this.chatMessages.push(message);
         });
+    }
+    public ngAfterViewChecked(): void {
+        this.scrollToBottom();
+    }
+
+    private scrollToBottom(): void {
+        this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
     }
 }
