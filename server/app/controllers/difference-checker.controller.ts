aed0d9ca4932d1b5076354b04f6fa0adbe546edf
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-09 17:11:36 -0500

    MC: detection de difference dans le controleur D

--- server/app/controllers/difference-checker.controller.ts
+++ server/app/controllers/difference-checker.controller.ts
@@ -1,9 +1,14 @@
+import Axios from "axios";
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
+import { SERVER_ADDRESS } from "../../../common/communication/constants";
+import { SceneData } from "../../../common/communication/geometry";
 import { Message, MessageType } from "../../../common/communication/message";
+import { VectorInterface } from "../../../common/communication/vector-interface";
 import { DifferenceCheckerService } from "../services/difference-checker.service";
 import { Game } from "../services/game";
 import { GetGameService } from "../services/get-game.service";
+import { SceneDifferenceCheckerService } from "../services/scene/scene-difference-checker";
 import Types from "../types";
 
 @injectable()
@@ -47,6 +52,29 @@ export class DifferenceCheckerController {
                 res.send(message);
             });
 
+        router.post(
+                "/3D",
+                async(req: Request, res: Response, next: NextFunction) => {
+                        const message: Message = {
+                            type: MessageType.SCENE_DATA,
+                            body: "false",
+                        };
+                        const name: string = "Scene3D";
+                        this.getSceneData(name).then(
+                            //TODO: trouver un typedef
+                            (promise) => {
+                                const sceneData: SceneData = promise.data;
+                                const position: VectorInterface = req.body.position;
+                                const differenceChecker: SceneDifferenceCheckerService = new SceneDifferenceCheckerService();
+                                message.body = String(differenceChecker.checkDifference(sceneData, position));
+                                res.send(message);
+                            }).catch(console.error);
+                });
+
         return router;
+    }
+    //TODO: remplacer le any
+    private getSceneData(name: string): Promise<any> {
+       return Axios.get(`${SERVER_ADDRESS}/${name}-data.txt`);
     }
 }
