0a84eed5c9352119612b304372f3209d6eafc432
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-09 17:09:35 -0500

    MC: requete serveur pour la detection de diff D

---
+++ client/src/app/gameplay-3d/difference3d-checker.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from "@angular/core/testing";
+
+import { Difference3DCheckerService } from "./difference3d-checker.service";
+
+describe("Difference3DCheckerService", () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it("should be created", () => {
+    const service: Difference3DCheckerService = TestBed.get(Difference3DCheckerService);
+    expect(service).toBeTruthy();
+  });
+});
