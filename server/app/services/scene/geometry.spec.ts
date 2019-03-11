27d16e1f2eacebc534c7736e27b446e5b5b5cd24
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-11 13:14:48 -0400

    MC : test pour geometry

--- server/app/services/scene/geometry.spec.ts
+++ server/app/services/scene/geometry.spec.ts
@@ -6,9 +6,10 @@ import { Geometry } from "./geometry";
 describe("Geometry", () => {
     const geometryData: GeometryData = {
             size: 0 , position: {x: 1, y: 1 , z: 1},
-            rotation: {x: 1, y: 1 , z: 1}, color: 0,
+            rotation: {x: 10, y: 10 , z: 10}, color: 0,
             type: 0, isModified: false,
             };
+
     describe("position", () => {
 
         it("should return true if position is equal ", () => {
@@ -31,20 +32,42 @@ describe("Geometry", () => {
     describe("rotation", () => {
 
         it("should return true if rotation is equal ", () => {
-            const rotation: VectorInterface = {x: 1, y: 1 , z: 1};
+            const rotation: VectorInterface = {x: 10, y: 10 , z: 10};
             const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
-            const result: boolean = geometry.isPositionEqual(rotation);
+            const result: boolean = geometry.isRotationEqual(rotation);
 
             expect(result).to.equal(true);
         });
 
         it("should return false if rotation is not equal ", () => {
-            const rotation: VectorInterface = {x: 0, y: 1 , z: 1};
+            const rotation: VectorInterface = {x: 0, y: 10 , z: 10};
             const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
-            const result: boolean = geometry.isPositionEqual(rotation);
+            const result: boolean = geometry.isRotationEqual(rotation);
 
             expect(result).to.equal(false);
         });
     });
-    
+
+    describe("Is equal", () => {
+
+        it("should return true if GeometryData is equal ", () => {
+            const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
+            const result: boolean = geometry.isEqual(geometryData);
+
+            expect(result).to.equal(true);
+        });
+
+        it("should return false if GeometryData is not equal ", () => {
+            const geometryData2: GeometryData = {
+                size: 0 , position: {x: 100, y: 59 , z: 36},
+                rotation: {x: 20, y: 15 , z: 1}, color: 36,
+                type: 3, isModified: false,
+                };
+            const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
+            const result: boolean = geometry.isEqual(geometryData2);
+
+            expect(result).to.equal(false);
+        });
+    });
+
 });
