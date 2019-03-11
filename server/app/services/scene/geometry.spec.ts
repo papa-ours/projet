3409623d4bad3ff8d5145bfeb8c6086c4cecb379
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-11 09:19:42 -0400

    MC : class utilitaire pour geometry D

---
+++ server/app/services/scene/geometry.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+import { GeometryData } from "../../../../common/communication/geometry";
+import { VectorInterface } from "../../../../common/communication/vector-interface";
+import { Geometry } from "./geometry";
+
+describe("Geometry", () => {
+    const geometryData: GeometryData = {
+            size: 0 , position: {x: 1, y: 1 , z: 1},
+            rotation: {x: 1, y: 1 , z: 1}, color: 0,
+            type: 0, isModified: false,
+            };
+    describe("position", () => {
+
+        it("should return true if position is equal ", () => {
+            const position: VectorInterface = {x: 1, y: 1 , z: 1};
+            const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
+            const result: boolean = geometry.isPositionEqual(position);
+
+            expect(result).to.equal(true);
+        });
+
+        it("should return false if position is not equal ", () => {
+            const position: VectorInterface = {x: 0, y: 1 , z: 1};
+            const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
+            const result: boolean = geometry.isPositionEqual(position);
+
+            expect(result).to.equal(false);
+        });
+    });
+});
