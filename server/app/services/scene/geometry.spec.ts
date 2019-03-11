b41dbe6820c51a9eaeb4c6500361de1133f880a6
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-11 09:24:26 -0400

    MC : ajout de test pour la rotation isEqual T

--- server/app/services/scene/geometry.spec.ts
+++ server/app/services/scene/geometry.spec.ts
@@ -27,4 +27,24 @@ describe("Geometry", () => {
             expect(result).to.equal(false);
         });
     });
+
+    describe("rotation", () => {
+
+        it("should return true if rotation is equal ", () => {
+            const rotation: VectorInterface = {x: 1, y: 1 , z: 1};
+            const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
+            const result: boolean = geometry.isPositionEqual(rotation);
+
+            expect(result).to.equal(true);
+        });
+
+        it("should return false if rotation is not equal ", () => {
+            const rotation: VectorInterface = {x: 0, y: 1 , z: 1};
+            const geometry: Geometry =  Geometry.fromGeometryData(geometryData);
+            const result: boolean = geometry.isPositionEqual(rotation);
+
+            expect(result).to.equal(false);
+        });
+    });
+    
 });
