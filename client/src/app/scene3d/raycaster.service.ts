ab2055eee80af783e9c20fc0ff0357d317980cec
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-11 13:08:49 -0400

    MC : raycaster prend 2 render et cast dans les 2 scene D

--- client/src/app/scene3d/raycaster.service.ts
+++ client/src/app/scene3d/raycaster.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { VectorInterface } from "../../../../common/communication/vector-interface";
 import { RenderService } from "./render.service";
 
 @Injectable({
@@ -8,38 +9,50 @@ import { RenderService } from "./render.service";
 export class RaycasterService {
     private mouse: THREE.Vector2;
     private rayCaster: THREE.Raycaster;
+    private readonly nearestPosition: number = 0;
 
-    public constructor(private renderService: RenderService) {
+    public constructor(private originalRender: RenderService, private modifiedRender: RenderService) {
        this.mouse = new THREE.Vector2();
        this.rayCaster = new THREE.Raycaster();
     }
 
-    private computeMousePosition(event: MouseEvent, container: HTMLDivElement): void {
+    public static getMousePosition(event: MouseEvent, container: HTMLDivElement): VectorInterface {
         const boundingRect: ClientRect = container.getBoundingClientRect();
         const ratioX: number = (event.clientX - boundingRect.left) / container.clientWidth;
         const ratioY: number = (event.clientY - boundingRect.top) / container.clientHeight;
+        // formule pour convertire la position en x et en y tslint disable pour le * 2
         // tslint:disable-next-line:no-magic-numbers
-        this.mouse.x =  (ratioX * 2) - 1;
+        const x: number = (ratioX * 2) - 1;
         // tslint:disable-next-line:no-magic-numbers
-        this.mouse.y = 1 - (ratioY * 2);
+        const y: number = 1 - (ratioY * 2);
+
+        return {x: x, y: y, z: 0};
     }
 
-    public findObject(event: MouseEvent, container: HTMLDivElement): THREE.Vector3 | undefined {
+    private getIntersections( render: RenderService): THREE.Intersection[] {
+        render.camera.updateMatrixWorld(false);
+        this.rayCaster.setFromCamera(this.mouse.clone(),  render.camera);
 
-        this.computeMousePosition(event, container);
+        return this.rayCaster.intersectObjects(render.scene.children);
+    }
 
-        this.renderService.camera.updateMatrixWorld(false);
-        this.rayCaster.setFromCamera(this.mouse.clone(),  this.renderService.camera);
-
-        const intersections: THREE.Intersection[] = this.rayCaster.intersectObjects(this.renderService.scene.children);
-
+    public findObject(position: VectorInterface): VectorInterface | undefined {
+        this.mouse.x = position.x;
+        this.mouse.y = position.y;
+        let intersections: THREE.Intersection[] = this.getIntersections(this.originalRender);
+        let nearestObject: THREE.Object3D;
         if (intersections.length > 0) {
-            const nearestObject: THREE.Object3D = intersections[0].object;
+            nearestObject = intersections[this.nearestPosition].object;
+
+            return nearestObject.position;
+        }
+        intersections = this.getIntersections(this.modifiedRender);
+        if (intersections.length > 0) {
+            nearestObject = intersections[this.nearestPosition].object;
 
             return nearestObject.position;
         }
 
         return undefined;
     }
-
 }
