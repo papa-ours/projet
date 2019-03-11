65858ef4715fe94f7848759285408254a5e0e46f
Vincent <vg_4_@hotmail.fr>
2019-03-11 11:59:36 -0400

    VG: Ajout d'un EventEmitter pour la confirmation de l'Action D

--- client/src/app/confirm-action/confirm-action.component.ts
+++ client/src/app/confirm-action/confirm-action.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
   selector: "app-confirm-action",
@@ -7,4 +7,13 @@ import { Component, Input } from "@angular/core";
 })
 export class ConfirmActionComponent {
     @Input() public actionMessage: string;
+    @Output() public confirmAction: EventEmitter<boolean>;
+
+    public constructor() {
+        this.confirmAction = new EventEmitter<boolean>();
+    }
+
+    public confirm(isActionConfirmed: boolean): void {
+        this.confirmAction.emit(isActionConfirmed);
+    }
 }
