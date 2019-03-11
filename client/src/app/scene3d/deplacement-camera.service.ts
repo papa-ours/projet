dc7397a00d382b2314f34817dc1ee3f0f4fccbbe
David <david.piche@polymtl.ca>
2019-03-11 07:55:45 -0400

    DP: Ajout de constante pour le clic droit R

--- client/src/app/scene3d/deplacement-camera.service.ts
+++ client/src/app/scene3d/deplacement-camera.service.ts
@@ -2,6 +2,8 @@ import { RenderService } from "./render.service";
 
 export class DeplacementCameraService{
 
+    private readonly RIGHT_CLICK: number = 2; 
+
     public static keyPress( render3d: RenderService): void {
 
         document.body.addEventListener("keypress", (e: KeyboardEvent) => {
@@ -17,7 +19,7 @@ export class DeplacementCameraService{
         });
 
         document.body.addEventListener("mousedown", (e: MouseEvent) => {
-            if (e.buttons == 2){
+            if (e.buttons == RIGHT_CLICK){
                 document.body.addEventListener("mousemove", (e: MouseEvent) => {
                  });
                 }
