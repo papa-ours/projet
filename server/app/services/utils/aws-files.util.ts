aa552742cdb0b5fa6a9e282267bd042e63ec6ff5
Vincent <vg_4_@hotmail.fr>
2019-03-14 14:40:28 -0400

    VG: La variable S3 n'est plus une variable de classe R

--- server/app/services/utils/aws-files.util.ts
+++ server/app/services/utils/aws-files.util.ts
@@ -1,15 +1,26 @@
 import * as aws from "aws-sdk";
 import { PromiseResult } from "aws-sdk/lib/request";
+import { S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } from "../../../../common/communication/constants";
 
 export class AWSFilesUtil {
-    private static s3: aws.S3 = new aws.S3();
-
     public static async readFile(fileName: string): Promise<PromiseResult<aws.S3.GetObjectOutput, aws.AWSError>> {
-        return this.s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
+        aws.config.update({
+            accessKeyId: S3_ACCESS_KEY_ID,
+            secretAccessKey: S3_SECRET_ACCESS_KEY,
+        });
+        const s3: aws.S3 = new aws.S3();
+
+        return s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
     }
 
     public static async writeFile(fileName: string, data: Buffer): Promise<PromiseResult<aws.S3.PutObjectOutput, aws.AWSError>> {
-        return this.s3.putObject({
+        aws.config.update({
+            accessKeyId: S3_ACCESS_KEY_ID,
+            secretAccessKey: S3_SECRET_ACCESS_KEY,
+        });
+        const s3: aws.S3 = new aws.S3();
+
+        return s3.putObject({
             Bucket: "uploads-diffs",
             Key: fileName,
             Body: data,
