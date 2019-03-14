65b1f9ec33edae5737419708cc3d9f6126487c99
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-14 16:36:25 -0400

    MC : alignement tslint R

--- client/src/app/gameplay-3d/difference3d-checker.service.ts
+++ client/src/app/gameplay-3d/difference3d-checker.service.ts
@@ -8,18 +8,18 @@ import { Message } from "../../../../common/communication/message";
 import { VectorInterface } from "../../../../common/communication/vector-interface";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: "root",
 })
 export class Difference3DCheckerService {
 
-  public readonly URL: string = `${SERVER_ADDRESS}/api/difference/3D`;
-  public constructor(private http: HttpClient) { }
+    public readonly URL: string = `${SERVER_ADDRESS}/api/difference/3D`;
+    public constructor(private http: HttpClient) { }
 
-  public isPositionDifference(position: VectorInterface, name: string): Observable<boolean> {
-    const type: GameType = GameType.Free;
+    public isPositionDifference(position: VectorInterface, name: string): Observable<boolean> {
+        const type: GameType = GameType.Free;
 
-    return this.http.post<Message>(this.URL, {position, name, type} )
-        .pipe(map((message: Message) => JSON.parse(message.body)),
-    );
-}
+        return this.http.post<Message>(this.URL, { position, name, type })
+            .pipe(map((message: Message) => JSON.parse(message.body)),
+            );
+    }
 }
