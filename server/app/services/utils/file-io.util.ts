9014252e1a78edd78f809b71f223548260c9aa83
padal <pascal.dally-belanger@polymtl.ca>
2019-03-14 16:46:09 -0400

    PDB: importation des anciennes fonctions D

--- server/app/services/utils/file-io.util.ts
+++ server/app/services/utils/file-io.util.ts
@@ -2,4 +2,25 @@ import * as fs from "fs";
 
 export class FileIO {
 
+    public static async readFile(path: string): Promise<Buffer> {
+        return new Promise((resolve: (buffer: Buffer) => void, reject: Function) => {
+            fs.readFile(path, (err: Error, data: Buffer) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(data);
+            });
+        });
+    }
+
+    public static async writeFile(path: string, data: Buffer): Promise<{}> {
+        return new Promise((resolve: Function, reject: Function) => {
+            fs.writeFile(path, data, (err: Error) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve();
+            });
+        });
+    }
 }
