466264bcebd4b414e45b3b5abfb47f30e07b8149
padal <pascal.dally-belanger@polymtl.ca>
2019-03-14 17:09:27 -0400

    PDB: Ajout des tests file writer T

--- server/app/services/utils/file-io.util.spec.ts
+++ server/app/services/utils/file-io.util.spec.ts
@@ -22,5 +22,24 @@ describe("IO", () => {
                 .catch(() => done());
         });
     });
-    
+
+    describe("FileWriterUtil", () => {
+
+        it("should reject if the directory doesn't exist", (done: Mocha.Done) => {
+            const data: Uint8Array = new Uint8Array([]);
+            FileIO.writeFile("fakeDire/fakeFile.out", Buffer.from(data))
+                .then(() => done(new Error("Promise should not resolve")))
+                .catch(() => done());
+        });
+
+        it("should resolve if the directory exists", async () => {
+            const data: Uint8Array = new Uint8Array([0, 1, 0, 1]);
+            await FileIO.writeFile("test/test.bmp", Buffer.from(data));
+            const readData: Buffer = await FileIO.readFile("test/test.bmp");
+            const readArray: Uint8Array = new Uint8Array(readData);
+
+            expect(readArray).to.deep.equals(data);
+        });
+    });
+
 });
