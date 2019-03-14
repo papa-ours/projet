cfa7f58d0a2624027a6805d7593f731d60b5f769
Vincent <vg_4_@hotmail.fr>
2019-03-14 14:29:05 -0400

    VG: Transfert du s3 dans une variable de classe D

--- server/app/services/utils/aws-files.util.ts
+++ server/app/services/utils/aws-files.util.ts
@@ -2,25 +2,14 @@ import * as aws from "aws-sdk";
 import { PromiseResult } from "aws-sdk/lib/request";
 
 export class S3FileReader {
+    private static s3: aws.S3 = new aws.S3();
+
     public static async readFile(fileName: string): Promise<PromiseResult<aws.S3.GetObjectOutput, aws.AWSError>> {
-        aws.config.update({
-            accessKeyId: "AKIAI23N35EM3WRJXQRA",
-            secretAccessKey: "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",
-        });
-
-        const s3: aws.S3 = new aws.S3();
-
-        return s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
+        return this.s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
     }
 
     public static async writeFile(fileName: string, data: Buffer): Promise<PromiseResult<aws.S3.PutObjectOutput, aws.AWSError>> {
-        aws.config.update({
-            accessKeyId: "AKIAI23N35EM3WRJXQRA",
-            secretAccessKey: "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",
-        });
-        const s3: aws.S3 = new aws.S3();
-
-        return s3.putObject({
+        return this.s3.putObject({
             Bucket: "uploads-diffs",
             Key: fileName,
             Body: data,
