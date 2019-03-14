c1eb6381ebb851096f0e422041208bdd0f6fb253
Vincent <vg_4_@hotmail.fr>
2019-03-14 14:47:36 -0400

    VG: Utilisation de la classe S3Util D

--- server/app/services/utils/aws-files.util.ts
+++ server/app/services/utils/aws-files.util.ts
@@ -1,26 +1,16 @@
 import * as aws from "aws-sdk";
 import { PromiseResult } from "aws-sdk/lib/request";
-import { S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } from "../../../../common/communication/constants";
+import { S3Util } from "./s3.util";
 
 export class AWSFilesUtil {
-    public static async readFile(fileName: string): Promise<PromiseResult<aws.S3.GetObjectOutput, aws.AWSError>> {
-        aws.config.update({
-            accessKeyId: S3_ACCESS_KEY_ID,
-            secretAccessKey: S3_SECRET_ACCESS_KEY,
-        });
-        const s3: aws.S3 = new aws.S3();
+    private static s3Util: S3Util = new S3Util();
 
-        return s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
+    public static async readFile(fileName: string): Promise<PromiseResult<aws.S3.GetObjectOutput, aws.AWSError>> {
+        return this.s3Util.s3.getObject({Bucket: "uploads-diffs", Key: fileName}).promise();
     }
 
     public static async writeFile(fileName: string, data: Buffer): Promise<PromiseResult<aws.S3.PutObjectOutput, aws.AWSError>> {
-        aws.config.update({
-            accessKeyId: S3_ACCESS_KEY_ID,
-            secretAccessKey: S3_SECRET_ACCESS_KEY,
-        });
-        const s3: aws.S3 = new aws.S3();
-
-        return s3.putObject({
+        return this.s3Util.s3.putObject({
             Bucket: "uploads-diffs",
             Key: fileName,
             Body: data,
