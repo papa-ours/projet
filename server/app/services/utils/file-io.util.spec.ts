dc8b04748193bf6b1f949cff906cc0b82f6f6074
padal <pascal.dally-belanger@polymtl.ca>
2019-03-14 17:08:53 -0400

    PDB: Ajout des tests file reader T

--- server/app/services/utils/file-io.util.spec.ts
+++ server/app/services/utils/file-io.util.spec.ts
@@ -1,5 +1,26 @@
-
+import { expect } from "chai";
+import * as fs from "fs";
+import { FileIO } from "./file-io.util";
 
 describe("IO", () => {
 
+    describe("file-reader.util", () => {
+        let data: Buffer;
+        fs.readFile("../client/src/assets/img/car_original.bmp", (err: NodeJS.ErrnoException, fileData: Buffer) => {
+            data = fileData;
+        });
+
+        it("should resolve", async () => {
+            const result: Buffer = await FileIO.readFile("../client/src/assets/img/car_original.bmp");
+
+            expect(result).to.deep.equal(data);
+        });
+
+        it("should reject if the image doesn't exist", (done: Mocha.Done) => {
+            FileIO.readFile("")
+                .then(() => done(new Error("Promise should not resolve")))
+                .catch(() => done());
+        });
+    });
+    
 });
