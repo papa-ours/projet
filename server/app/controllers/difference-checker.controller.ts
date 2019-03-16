798107a61cc765114f56d49ef70ed485d0e55988
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-16 15:13:01 -0400

    MC : integration de la game au controleur D

--- server/app/controllers/difference-checker.controller.ts
+++ server/app/controllers/difference-checker.controller.ts
@@ -1,16 +1,11 @@
-import Axios from "axios";
 import { NextFunction, Request, Response, Router } from "express";
 import { inject, injectable } from "inversify";
-import { SERVER_ADDRESS } from "../../../common/communication/constants";
-import { SceneData } from "../../../common/communication/geometry";
 import { Message, MessageType } from "../../../common/communication/message";
 import { VectorInterface } from "../../../common/communication/vector-interface";
 import { DifferenceCheckerService } from "../services/difference-checker.service";
 import { Game } from "../services/game";
 import { GetGameService } from "../services/get-game.service";
 import { SceneDifferenceCheckerService } from "../services/scene/scene-difference-checker";
-import { SceneDifferenceRestorationService } from "../services/scene/scene-difference-restoration";
-import { FileWriterUtil } from "../services/utils/file-writer.util";
 import Types from "../types";
 
 @injectable()
@@ -62,30 +57,18 @@ export class DifferenceCheckerController {
                     body: "false",
                 };
                 const sceneName: string = String(req.body.name);
-                this.getSceneData(sceneName).then(
-                    //TODO: trouver un typedef
-                    (promise) => {
-                        let sceneData: SceneData = promise.data;
-                        const position: VectorInterface = req.body.position;
-                        const differenceChecker: SceneDifferenceCheckerService = new SceneDifferenceCheckerService(sceneData);
-                        const isModification: boolean = differenceChecker.checkDifference(position);
-                        if (isModification) {
-                            const differenceRestoration: SceneDifferenceRestorationService =
-                                                         new SceneDifferenceRestorationService(sceneData);
-
-                            sceneData = differenceRestoration.getSceneAfterDifferenceUpdate(position);
-                            FileWriterUtil.writeFile(`uploads/${sceneName}-data.txt`, Buffer.from(JSON.stringify(sceneData)))
-                                .catch((err: Error) => console.error(err));
-                        }
-                        message.body = String(isModification);
-                        res.send(message);
-                    }).catch(console.error);
+                const getGameService: GetGameService = new GetGameService();
+                const game: Game = getGameService.getGame(sceneName);
+                const position: VectorInterface = req.body.position;
+                const differenceChecker: SceneDifferenceCheckerService = new SceneDifferenceCheckerService(game.scene);
+                const isModification: boolean = differenceChecker.checkDifference(position);
+                if (isModification) {
+                    game.restoreModifiedScene(position);
+                }
+                message.body = String(isModification);
+                res.send(message);
             });
 
         return router;
-    }
-    //TODO: remplacer le any
-    private getSceneData(name: string): Promise<any> {
-        return Axios.get(`${SERVER_ADDRESS}/${name}-data.txt`);
     }
 }
