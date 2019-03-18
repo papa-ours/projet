841cd8a048f74944486abc4c5f8679ef7cc5690d
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-18 18:20:16 -0400

    MC : fonction pour aller chercher object de difference dans la scene D

--- client/src/app/gameplay-3d/cheat-mode.service.ts
+++ client/src/app/gameplay-3d/cheat-mode.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from "@angular/core";
+import { GeometryData } from "../../../../common/communication/geometry";
+import { RenderService } from "../scene3d/render.service";
 
 @Injectable({
     providedIn: "root",
 })
 export class CheatModeService {
 
-    public constructor() { }
+    public constructor(private originalRender: RenderService, private modifiedRender: RenderService) { }
+
+    private findGeometry(renderer: RenderService, geometry: GeometryData): THREE.Object3D | undefined {
+        return renderer.scene.children.find(
+            (object: THREE.Object3D) => {
+                return object.position.x === geometry.position.x &&
+                       object.position.y === geometry.position.y &&
+                       object.position.z === geometry.position.z;
+
+        });
+    }
+
 }
