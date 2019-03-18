c517d1be78b659667eab44ad49d586daaa4e1fb7
Vincent <vg_4_@hotmail.fr>
2019-03-18 10:52:38 -0400

    VG: Déconnection de la DB après le test T

--- server/app/services/get-game-list.service.spec.ts
+++ server/app/services/get-game-list.service.spec.ts
@@ -1,8 +1,13 @@
 import { expect } from "chai";
 import { GameLists } from "../../../common/communication/game-description";
+import { DBConnectionService } from "./db-connection.service";
 import { GetGameListService } from "./get-game-list.service";
 
 describe("get game list", () => {
+
+    afterEach(() => {
+        DBConnectionService.getInstance().closeConnection();
+    });
 
     it("should return the game list properly", async () => {
         const getGameListService: GetGameListService = new GetGameListService();
