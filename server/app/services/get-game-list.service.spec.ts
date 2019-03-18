6123136386a835f245ad98109b42c8a3de026a7e
Vincent <vg_4_@hotmail.fr>
2019-03-18 10:22:53 -0400

    VG: Retrait d'un test bidon T

--- server/app/services/get-game-list.service.spec.ts
+++ server/app/services/get-game-list.service.spec.ts
@@ -4,10 +4,6 @@ import { GetGameListService } from "./get-game-list.service";
 
 describe("get game list", () => {
 
-    it("should actually test something", () => {
-        expect(true).to.equals(true);
-    });
-
     it("should return the game list properly", async () => {
         const getGameListService: GetGameListService = new GetGameListService();
         const result: GameLists = await getGameListService.getGameList();
