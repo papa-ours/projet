5f43a13bfbc30c49bda466b2af4a4a58f56ef56a
David <david.piche@polymtl.ca>
2019-03-19 18:42:44 -0400

    DP: Alphabetisation des imports R

--- client/src/app/scene3d/scene3d.component.ts
+++ client/src/app/scene3d/scene3d.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input ,
          OnChanges, Output, SimpleChange, SimpleChanges, ViewChild} from "@angular/core";
 import { GeometryData, SceneData } from "../../../../common/communication/geometry";
-import { DeplacementCameraService } from "../scene3d/deplacement-camera.service";
 import { VectorInterface } from "../../../../common/communication/vector-interface";
+import { DeplacementCameraService } from "../scene3d/deplacement-camera.service";
 import { GetSceneDataService } from "./get-scene-data.service";
 import { RaycasterService } from "./raycaster.service";
 import { RenderService } from "./render.service";
