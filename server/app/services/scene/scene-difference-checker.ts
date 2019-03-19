084a060e09b80ed88a6fe3ed27cf5865e49d29b8
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-19 11:42:04 -0400

    MC : refactor du calcul de differences R

--- server/app/services/scene/scene-difference-checker.ts
+++ server/app/services/scene/scene-difference-checker.ts
@@ -3,35 +3,39 @@ import "reflect-metadata";
 import { GeometryData, SceneData } from "../../../../common/communication/geometry";
 import { VectorInterface } from "../../../../common/communication/vector-interface";
 import { Geometry } from "./geometry";
+import { Scene } from "./scene";
 
 @injectable()
 export class SceneDifferenceCheckerService {
 
-    public differenceSet: GeometryData[];
+    public differences: GeometryData[];
+    public scene: Scene;
 
-    public constructor(scene: SceneData) {
-       this.differenceSet = this.getSetDifference(scene.originalScene, scene.modifiedScene);
+    public constructor(sceneData: SceneData) {
+        this.scene = Scene.fromSceneData(sceneData);
+        this.differences = this.getDifferences(sceneData.originalScene, sceneData.modifiedScene);
     }
 
-    private isDifferenteFromCollection(newGeometry: GeometryData, collection: GeometryData[]): boolean {
+    private isPositionInCollection(newGeometry: GeometryData, collection: GeometryData[]): boolean {
         for (const geometry of collection) {
-            if (Geometry.fromGeometryData(geometry).isEqual(newGeometry)) {
-                return false;
+            if (Geometry.fromGeometryData(geometry).isPositionEqual(newGeometry.position)) {
+                return true;
             }
         }
 
-        return true;
+        return false;
     }
 
-    private getSetDifference(scene1: GeometryData[], scene2: GeometryData[]): GeometryData[] {
-        const diffScene1: GeometryData[] = scene1.filter((geometry: GeometryData) => this.isDifferenteFromCollection(geometry, scene2));
-        const diffScene2:  GeometryData[] = scene2.filter((geometry: GeometryData) => this.isDifferenteFromCollection(geometry, scene1));
+    private getDifferences(original: GeometryData[], modified: GeometryData[]): GeometryData[] {
+        const suppression: GeometryData[] = original.filter((geometry: GeometryData) => !this.isPositionInCollection(geometry, modified));
+        const adding: GeometryData[] = modified.filter((geometry: GeometryData) => !this.isPositionInCollection(geometry, original));
+        const color: GeometryData[] = original.filter((geometry: GeometryData) => this.scene.isColorChangeAtPosition(geometry.position));
 
-        return [...diffScene1, ...diffScene2];
+        return [...suppression, ...adding, ...color];
     }
 
     public checkDifference(position: VectorInterface): boolean {
 
-        return this.differenceSet.some((geometry: GeometryData) => Geometry.fromGeometryData(geometry).isPositionEqual(position));
+        return this.differences.some((geometry: GeometryData) => Geometry.fromGeometryData(geometry).isPositionEqual(position));
     }
 }
