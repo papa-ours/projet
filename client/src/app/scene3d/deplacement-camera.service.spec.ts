4f3112638dd45f37181b6a0a2d1bc7589118f6ce
David <david.piche@polymtl.ca>
2019-03-19 21:21:01 -0400

    DP: Suppression classe mock inutile T

--- client/src/app/scene3d/deplacement-camera.service.spec.ts
+++ client/src/app/scene3d/deplacement-camera.service.spec.ts
@@ -3,11 +3,6 @@ import { TestBed } from "@angular/core/testing";
 import { DeplacementCameraService } from "./deplacement-camera.service";
 import { RenderService } from "./render.service";
 
-@Injectable()
-export class MockElementRef {
-  public nativeElement: {};
-}
-
 describe("DeplacementCameraService", () => {
 
     let element: ElementRef;
@@ -34,7 +29,7 @@ describe("DeplacementCameraService", () => {
         DeplacementCameraService.activateMovement();
         const event = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, key : "w", shiftKey : false});
         spyOn(render1, "setDeltaZ");
-        DeplacementCameraService["setCameraSpeed"](e);
+        DeplacementCameraService["setCameraSpeed"](event);
         expect(render1.setDeltaZ).toHaveBeenCalled();
-     });
+    });
 });
