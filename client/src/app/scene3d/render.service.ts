f8ffb6427fb1f409efd5b1af74aab4af1d35df01
David <david.piche@polymtl.ca>
2019-03-19 20:20:29 -0400

    DP: Alignement d'arguments tslint R

--- client/src/app/scene3d/render.service.ts
+++ client/src/app/scene3d/render.service.ts
@@ -80,7 +80,7 @@ export class RenderService {
         setInterval(() => {
             this.camera.translateX(this.deltaX);
             this.camera.translateZ(this.deltaZ);
-        }, this.MOUVEMENT_INTERVAL);
+        },          this.MOUVEMENT_INTERVAL);
     }
 
     public reInitialize(container: HTMLDivElement, scene: THREE.Scene): void {
