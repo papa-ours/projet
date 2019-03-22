34e7770dc399db2657b4f782bedce30776080268
Vincent <vg_4_@hotmail.fr>
2019-03-21 23:56:38 -0400

    VG: Le service est pris a partir du container R

--- server/app/services/get-game-list.service.spec.ts
+++ server/app/services/get-game-list.service.spec.ts
@@ -1,10 +1,12 @@
 import { expect } from "chai";
 import { GameLists } from "../../../common/communication/game-description";
+import { container } from "../inversify.config";
+import Types from "../types";
 import { GetGameListService } from "./get-game-list.service";
 
 describe("get game list", () => {
+    const getGameListService: GetGameListService = container.get<GetGameListService>(Types.GetGameListService);
     it("should return the game list properly", async () => {
-        const getGameListService: GetGameListService = new GetGameListService();
         const result: GameLists = await getGameListService.getGameList();
         expect(result).to.not.equals(undefined);
     });
