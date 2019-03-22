1ff10993fb9fcbdacbfc53616e37486e34e0d756
Vincent <vg_4_@hotmail.fr>
2019-03-21 23:43:25 -0400

    VG: Utilisation de la DB sans singleton R

--- server/app/services/get-game-list.service.ts
+++ server/app/services/get-game-list.service.ts
@@ -1,15 +1,17 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import "reflect-metadata";
 import { GameLists, GameSheet, GameType } from "../../../common/communication/game-description";
+import Types from "../types";
 import { DBConnectionService } from "./db-connection.service";
-
 @injectable()
 export class GetGameListService {
 
+    private constructor(@inject(Types.DBConnectionService) private db: DBConnectionService) {}
+
     public async getGameList(): Promise<GameLists> {
-        const db: DBConnectionService = DBConnectionService.getInstance();
-        const simpleGameSheets: GameSheet[] = await db.getGameSheets(GameType.Simple);
-        const freeGameSheets: GameSheet[] = await db.getGameSheets(GameType.Free);
+        const simpleGameSheets: GameSheet[] = await this.db.getGameSheets(GameType.Simple);
+        const freeGameSheets: GameSheet[] = await this.db.getGameSheets(GameType.Free);
+        this.db.closeConnection();
 
         return new Promise((resolve: (gameLists: GameLists) => void) => {
             resolve({
