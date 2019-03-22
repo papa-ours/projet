75f928324cc8bff10383aff90bc6c0be2f8b3bcb
Vincent <vg_4_@hotmail.fr>
2019-03-21 23:43:15 -0400

    VG: Retrait du after R

--- server/app/services/get-game-list.service.spec.ts
+++ server/app/services/get-game-list.service.spec.ts
@@ -1,14 +1,8 @@
 import { expect } from "chai";
 import { GameLists } from "../../../common/communication/game-description";
-import { DBConnectionService } from "./db-connection.service";
 import { GetGameListService } from "./get-game-list.service";
 
 describe("get game list", () => {
-
-    afterEach(() => {
-        DBConnectionService.getInstance().closeConnection();
-    });
-
     it("should return the game list properly", async () => {
         const getGameListService: GetGameListService = new GetGameListService();
         const result: GameLists = await getGameListService.getGameList();
