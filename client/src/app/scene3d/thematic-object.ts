45039a8bd7b2d8775909fc42dadec0c606581f57
Vincent <vg_4_@hotmail.fr>
2019-03-22 13:42:00 -0400

    VG: Création des objets 3D D

--- client/src/app/scene3d/thematic-object.ts
+++ client/src/app/scene3d/thematic-object.ts
@@ -1,4 +1,14 @@
 export interface ThematicObject {
-    maxCount: number;
     name: string;
+    maxCount: number;
 }
+
+export const THEMATIC_OBJECTS: ThematicObject[] = [
+    {name: "laptop", maxCount: 2},
+    {name: "apple", maxCount: 4},
+    {name: "pencil", maxCount: 5},
+    {name: "eraser", maxCount: 6},
+    {name: "calculator", maxCount: 3},
+    {name: "coffeeMug", maxCount: 10},
+    {name: "usbKey", maxCount: 10},
+];
