414e84ef3e599ded18dfd96f815ee2c5be78778f
Vincent <vg_4_@hotmail.fr>
2019-03-22 10:43:16 -0400

    VG: Les requêtes se font séquentiellement D

--- server/app/services/get-game-list.service.ts
+++ server/app/services/get-game-list.service.ts
@@ -10,15 +10,19 @@ export class GetGameListService {
 
     public async getGameList(): Promise<GameLists> {
         await this.db.connect();
-        const simpleGameSheets: GameSheet[] = await this.db.getGameSheets(GameType.Simple);
-        const freeGameSheets: GameSheet[] = await this.db.getGameSheets(GameType.Free);
+        const types: GameType[] = [GameType.Simple, GameType.Free];
+        const gameSheets: GameSheet[][] = await Promise.all(types.map((type: GameType) => this.getGameSheets(type)));
         await this.db.closeConnection();
 
         return new Promise((resolve: (gameLists: GameLists) => void) => {
             resolve({
-                list2d: simpleGameSheets,
-                list3d: freeGameSheets,
+                list2d: gameSheets[GameType.Simple],
+                list3d: gameSheets[GameType.Free],
             });
         });
+    }
+
+    public async getGameSheets(type: GameType): Promise<GameSheet[]> {
+        return this.db.getGameSheets(type);
     }
 }
