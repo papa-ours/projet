55198796bfc76b292ab2612ed873e95d3390744c
Vincent <vg_4_@hotmail.fr>
2019-03-22 13:49:23 -0400

    VG: Ajout d'une méthode pour loader des objets 3D D

--- client/src/app/scene3d/thematic-object-generator.service.ts
+++ client/src/app/scene3d/thematic-object-generator.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from "@angular/core";
+import * as THREE from "three";
+import * as OBJLoader from "three-obj-loader";
+import { ThematicObject } from "./thematic-object";
 
 @Injectable({
     providedIn: "root",
 })
 export class ThematicObjectGeneratorService {
+    private objLoader: THREE.OBJLoader;
 
+    public constructor() {
+        OBJLoader(THREE);
+        this.objLoader = new THREE.OBJLoader();
+        this.objLoader.setPath("../../assets/3d-objects/");
+    }
+
+    public async createObject(thematicObject: ThematicObject): Promise<THREE.Group> {
+        return new Promise((resolve: (group: THREE.Group) => void) =>
+            this.objLoader.load(`${thematicObject.name}/${thematicObject.name}.obj`, resolve));
+    }
 }
