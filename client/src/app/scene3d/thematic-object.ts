b6f91c694f48ae4fb28d9d14c56d7c055d795e85
Vincent <vg_4_@hotmail.fr>
2019-03-22 18:00:27 -0400

    VG: Modification des objets D

--- client/src/app/scene3d/thematic-object.ts
+++ client/src/app/scene3d/thematic-object.ts
@@ -1,14 +1,13 @@
 export interface ThematicObject {
     name: string;
     maxCount: number;
+    baseScale: number;
 }
 
 export const THEMATIC_OBJECTS: ThematicObject[] = [
-    {name: "laptop", maxCount: 2},
-    {name: "apple", maxCount: 4},
-    {name: "pencil", maxCount: 5},
-    {name: "eraser", maxCount: 6},
-    {name: "calculator", maxCount: 3},
-    {name: "coffeeMug", maxCount: 10},
-    {name: "usbKey", maxCount: 10},
+    {name: "laptop", maxCount: 2, baseScale: 150},
+    {name: "apple", maxCount: 4, baseScale: 0.8},
+    {name: "calculator", maxCount: 3, baseScale: 10},
+    {name: "coffeeMug", maxCount: 10, baseScale: 3},
+    {name: "usbKey", maxCount: 10, baseScale: 0.01},
 ];
