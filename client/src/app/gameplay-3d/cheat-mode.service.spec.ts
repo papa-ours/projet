9525f207de105efe17e1f6d8174e8467c537fa29
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-23 14:37:50 -0400

    MC : initialisation des tests pour cheat mode T

--- client/src/app/gameplay-3d/cheat-mode.service.spec.ts
+++ client/src/app/gameplay-3d/cheat-mode.service.spec.ts
@@ -1,12 +1,35 @@
-import { TestBed } from "@angular/core/testing";
-
+import { GeometryData } from "../../../../common/communication/geometry";
+import { GeometryFactoryService } from "../scene3d/geometry-factory.service";
+import { RenderService } from "../scene3d/render.service";
+import { SceneGeneratorService } from "../scene3d/scene-generator.service";
 import { CheatModeService } from "./cheat-mode.service";
 
 describe("CheatModeService", () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    const originalRenderer: RenderService = new RenderService();
+    const modifiedRenderer: RenderService = new RenderService();
+    const sceneGenerator: SceneGeneratorService = new SceneGeneratorService(new GeometryFactoryService());
+    const geometry: GeometryData[] = [{
+        color: 0xFF00FF,
+        size: 65,
+        type: 2,
+        position: {x: 0, y: 0, z: 0},
+        rotation: {x: 0, y: 0, z: 0},
+        isModified: false,
+    }];
+    const modifiedGeometry: GeometryData[] = [{
+        color: 0x00FF00,
+        size: 65,
+        type: 2,
+        position: {x: 0, y: 0, z: 0},
+        rotation: {x: 0, y: 0, z: 0},
+        isModified: false,
+    }];
+    const div: HTMLDivElement = document.createElement("div");
 
     it("should be created", () => {
-        const service: CheatModeService = TestBed.get(CheatModeService);
-        expect(service).toBeTruthy();
+        originalRenderer.initialize(div, sceneGenerator.createScene(geometry));
+        modifiedRenderer.initialize(div, sceneGenerator.createScene(modifiedGeometry));
+        const cheatModeService: CheatModeService = new CheatModeService(originalRenderer, modifiedRenderer);
+        expect(cheatModeService).toBeTruthy();
     });
 });
