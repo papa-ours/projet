fc0be3babaed282304c5974a94cde7cc714bcd97
Vincent <vg_4_@hotmail.fr>
2019-03-23 16:27:01 -0400

    VG: Ajout d'un enum pour le type d'objet 3d D

--- common/communication/thematic-object.ts
+++ common/communication/thematic-object.ts
@@ -9,3 +9,9 @@ export const THEMATIC_OBJECTS: ThematicObject[] = [
     {name: "calculator", maxCount: 3, baseScale: 10},
     {name: "coffeeMug", maxCount: 10, baseScale: 3},
 ];
+
+export enum ThematicObjectType {
+    APPLE,
+    CALCULATOR,
+    COFFEE_MUG,
+}
