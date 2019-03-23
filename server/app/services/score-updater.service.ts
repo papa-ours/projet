df8f302dca0c5b640a38e9147cb10217b09c7722
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-03-23 00:09:58 -0400

    PDB: Ajout de la signature de la fonction putScore D

--- server/app/services/score-updater.service.ts
+++ server/app/services/score-updater.service.ts
@@ -3,4 +3,7 @@ import { injectable } from "inversify";
 @injectable()
 export class ScoreUpdaterService {
 
+    public async putScore(gameSheetId: string, name: string, time: number): Promise<{}> {
+        return {};
+    }
 }
