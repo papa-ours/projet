61933bc48097d59fee0f5671787310ce9f09539e
Vincent <vg_4_@hotmail.fr>
2019-03-23 17:15:20 -0400

    VG: Setup des objets 3d D

--- client/src/app/scene3d/thematic-scene-generator.service.ts
+++ client/src/app/scene3d/thematic-scene-generator.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { GeometryData } from "../../../../common/communication/geometry";
 import { ThematicObject, THEMATIC_OBJECTS } from "../../../../common/communication/thematic-object";
 import { ThematicObjectGeneratorService } from "./thematic-object-generator.service";
 
@@ -13,21 +14,37 @@ export class ThematicSceneGeneratorService {
     public constructor(public thematicObjectGeneratorService: ThematicObjectGeneratorService) {
     }
 
-    public async createScene(): Promise<THREE.Scene> {
+    public async createScene(geometryData: GeometryData[]): Promise<THREE.Scene> {
         this.scene = new THREE.Scene();
 
         this.thematicObjectGeneratorService.waitForObjects().then(() =>
-            THEMATIC_OBJECTS.map((object: ThematicObject) => this.addObject(object)),
+            geometryData.map((data: GeometryData) => this.addObject(data)),
         );
 
         return this.scene;
     }
 
-    private addObject(object: ThematicObject): void {
+    private addObject(data: GeometryData): void {
         try {
-            this.scene.add(this.thematicObjectGeneratorService.getObject(object));
+            if (data.thematicObjectType) {
+                const group: THREE.Group = this.thematicObjectGeneratorService.getObject(THEMATIC_OBJECTS[data.thematicObjectType]);
+                group.position.set(data.position.x, data.position.y, data.position.z);
+                group.rotation.set(data.rotation.x, data.rotation.y, data.rotation.z);
+                const scale: number = this.calculateScale(THEMATIC_OBJECTS[data.thematicObjectType], data.size);
+                group.scale.set(scale, scale, scale);
+                this.scene.add(group);
+            }
         } catch (error) {
             console.error(error.message);
         }
+    }
+
+    private calculateScale(object: ThematicObject, size: number): number {
+        const objectSize: number | undefined = ThematicObjectGeneratorService.sizes.get(object.name);
+        if (!objectSize) {
+            throw RangeError("Object size could not be found");
+        }
+
+        return object.baseScale * size / objectSize;
     }
 }
