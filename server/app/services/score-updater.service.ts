70e831ce58d3f5e28ec0d971aa5840c965dd023e
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-03-23 15:39:59 -0400

    PDB: implémentation de base de la mise a jour de score D

--- server/app/services/score-updater.service.ts
+++ server/app/services/score-updater.service.ts
@@ -1,9 +1,17 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import Types from "../types";
+import { DBConnectionService } from "./db-connection.service";
 
 @injectable()
 export class ScoreUpdaterService {
 
+    public constructor(@inject(Types.DBConnectionService) private dbConnectionService: DBConnectionService) {
+
+    }
+
     public async putScore(gameSheetId: string, name: string, time: number): Promise<{}> {
-        return {};
+        await this.dbConnectionService.putScore(gameSheetId, name, time);
+
+        return this.dbConnectionService.deleteWorstScore(gameSheetId);
     }
 }
