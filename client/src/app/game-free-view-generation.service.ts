d59dbc07135e178946ce34e54b6d6a6777d26e61
Vincent <vg_4_@hotmail.fr>
2019-03-23 15:48:51 -0400

    VG: envoie des sizes si la scene est thematique D

--- client/src/app/game-free-view-generation.service.ts
+++ client/src/app/game-free-view-generation.service.ts
@@ -2,16 +2,21 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { SERVER_ADDRESS } from "../../../common/communication/constants";
 import { Message } from "../../../common/communication/message";
+import { ThematicObjectGeneratorService } from "./scene3d/thematic-object-generator.service";
 @Injectable({
   providedIn: "root",
 })
 export class GameFreeViewGenerationService {
 
-    public readonly URL: string = `${SERVER_ADDRESS}/api/scene`;
+    public readonly URL: string = `${SERVER_ADDRESS}/api/scene/`;
     public constructor(private http: HttpClient) { }
 
     public async postGenerate(formData: FormData): Promise<Message> {
-        return this.http.post(this.URL, formData).toPromise()
+        if (formData.get("objectType") === "thematic") {
+            formData.append("sizes", JSON.stringify(Array.from(ThematicObjectGeneratorService.sizes.values())));
+        }
+
+        return this.http.post(this.URL + formData.get("objectType"), formData).toPromise()
             .catch((err: Error) => console.error(err.message)) as Promise<Message>;
     }
 }
