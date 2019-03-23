1790404c2f12276724695fca0e55aae0b36d36cc
Vincent <vg_4_@hotmail.fr>
2019-03-23 15:13:11 -0400

    VG: Retrait de 2 objets D

--- client/src/app/scene3d/thematic-object.ts
+++ client/src/app/scene3d/thematic-object.ts
@@ -5,9 +5,7 @@ export interface ThematicObject {
 }
 
 export const THEMATIC_OBJECTS: ThematicObject[] = [
-    {name: "laptop", maxCount: 2, baseScale: 150},
     {name: "apple", maxCount: 4, baseScale: 0.8},
     {name: "calculator", maxCount: 3, baseScale: 10},
     {name: "coffeeMug", maxCount: 10, baseScale: 3},
-    {name: "usbKey", maxCount: 10, baseScale: 0.01},
 ];
