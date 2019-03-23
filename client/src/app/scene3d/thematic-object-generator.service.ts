7ff4dca2ebb5e9671abcc905974cf1515de794ab
Vincent <vg_4_@hotmail.fr>
2019-03-23 15:51:42 -0400

    VG: Ajout des sizes dans ThematicObjectGenerator D

--- client/src/app/scene3d/thematic-object-generator.service.ts
+++ client/src/app/scene3d/thematic-object-generator.service.ts
@@ -9,6 +9,7 @@ import { ThematicObject, THEMATIC_OBJECTS } from "./thematic-object";
 export class ThematicObjectGeneratorService {
     public static areObjectsLoaded: boolean = false;
     private static objects: Map<string, THREE.Group>;
+    public static sizes: Map<string, number>;
     private objLoader: THREE.OBJLoader;
 
     public constructor() {
@@ -24,6 +25,7 @@ export class ThematicObjectGeneratorService {
                     .then((objectPairs: [string, THREE.Group][]) => {
                         ThematicObjectGeneratorService.areObjectsLoaded = true;
                         ThematicObjectGeneratorService.objects = new Map(objectPairs);
+                        this.createSizes();
                         resolve();
                     })
                     .catch((error: Error) => console.error(error.message));
@@ -31,6 +33,20 @@ export class ThematicObjectGeneratorService {
                 resolve();
             }
         });
+    }
+
+    private createSizes(): void {
+        ThematicObjectGeneratorService.sizes = new Map();
+        ThematicObjectGeneratorService.objects.forEach((group: THREE.Group, name: string) => {
+            ThematicObjectGeneratorService.sizes.set(name, this.calculateDimension(group));
+        });
+    }
+
+    private calculateDimension(group: THREE.Group): number {
+        const box: THREE.Box3 = new THREE.Box3().setFromObject(group);
+        const dimensions: THREE.Vector3 = box.max.sub(box.min);
+
+        return dimensions.x > dimensions.z ? dimensions.x : dimensions.z;
     }
 
     private createAllObjects(): Promise<[string, THREE.Group][]> {
