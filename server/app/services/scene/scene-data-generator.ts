d74c8821e277f0ccc1d93acf5204cc35b49cc92e
Vincent <vg_4_@hotmail.fr>
2019-03-23 15:47:48 -0400

    VG: Ajout de plusieurs parametres optionnelles pour fixer des attributs des geometryData D

--- server/app/services/scene/scene-data-generator.ts
+++ server/app/services/scene/scene-data-generator.ts
@@ -23,20 +23,20 @@ export class SceneDataGeneratorService {
         }
     }
 
-    public getRandomPosition(): VectorInterface {
+    public getRandomPosition(isYFixed: boolean = false): VectorInterface {
         return {
             x: RandomNumber.randomInteger(SKYBOX_MIN.x, SKYBOX_MAX.x),
-            y: RandomNumber.randomInteger(SKYBOX_MIN.y, SKYBOX_MAX.y),
+            y: isYFixed ? 0 : RandomNumber.randomInteger(SKYBOX_MIN.y, SKYBOX_MAX.y),
             z: RandomNumber.randomInteger(SKYBOX_MIN.z, SKYBOX_MAX.z),
         };
     }
 
-    public getRandomRotation(): VectorInterface {
+    public getRandomRotation(isYFixed: boolean = false): VectorInterface {
         const maxAngle: number = Math.PI;
 
         return {
             x: RandomNumber.randomFloat(0, maxAngle),
-            y: RandomNumber.randomFloat(0, maxAngle),
+            y: isYFixed ? 0 : RandomNumber.randomFloat(0, maxAngle),
             z: RandomNumber.randomFloat(0, maxAngle),
         };
     }
@@ -45,11 +45,11 @@ export class SceneDataGeneratorService {
         return RandomNumber.randomInteger(0, this.BASECOLOR);
     }
 
-    public getRandomSize(): number {
+    public getRandomSize(baseSize: number = this.GEOMETRY_BASE_SIZE): number {
         const MIN_FACTOR: number = 0.5;
         const MAX_FACTOR: number = 1.5;
 
-        return RandomNumber.randomInteger(this.GEOMETRY_BASE_SIZE * MIN_FACTOR, this.GEOMETRY_BASE_SIZE * MAX_FACTOR);
+        return RandomNumber.randomInteger(baseSize * MIN_FACTOR, baseSize * MAX_FACTOR);
     }
 
     public getRandomGeometryType(): GeometryType {
@@ -62,11 +62,11 @@ export class SceneDataGeneratorService {
         return geometryTypes[Math.floor(Math.random() * geometryTypes.length)];
     }
 
-    public getRandomGeometryData(): GeometryData {
-        const randomPosition: VectorInterface = this.getRandomPosition();
-        const randomRotation: VectorInterface = this.getRandomRotation();
+    public getRandomGeometryData(size?: number, type?: GeometryType): GeometryData {
+        const randomPosition: VectorInterface = this.getRandomPosition(size !== undefined);
+        const randomRotation: VectorInterface = this.getRandomRotation(size !== undefined);
         const randomColor: number = this.getRandomColor();
-        const randomSize: number = this.getRandomSize();
+        const randomSize: number = size ? this.getRandomSize(size) : this.getRandomSize();
 
         return {
             position: randomPosition,
@@ -74,24 +74,25 @@ export class SceneDataGeneratorService {
             color: randomColor,
             size: randomSize,
             isModified: false,
-            type: this.getRandomGeometryType(),
+            type: type ? type : this.getRandomGeometryType(),
         };
     }
 
-    public getRandomNonIntersectingGeometryData(collection: GeometryData[]): GeometryData {
+    public getRandomNonIntersectingGeometryData(collection: GeometryData[], size?: number, type?: GeometryType): GeometryData {
         let geometry: GeometryData;
         do {
-            geometry = this.getRandomGeometryData();
+            geometry = this.getRandomGeometryData(size, type);
         } while (GeometryIntersection.intersectsWithCollection(geometry, collection));
 
         return geometry;
     }
 
-    public getSceneData(numberOfObjects: number): GeometryData[] {
+    public getSceneData(numberOfObjects: number, sizes?: number[], type?: GeometryType): GeometryData[] {
         this.validateNumberOfObjects(numberOfObjects);
         const geometryData: GeometryData[] = [];
         for (let i: number = 0; i < numberOfObjects; i++) {
-            geometryData.push(this.getRandomNonIntersectingGeometryData(geometryData));
+            const size: number | undefined = sizes ? sizes[Math.floor(Math.random() * sizes.length)] : undefined;
+            geometryData.push(this.getRandomNonIntersectingGeometryData(geometryData, size, type));
         }
 
         return geometryData;
