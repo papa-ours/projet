ba931a968fc9afa5ef22a67aaae326cab336e825
Vincent <vg_4_@hotmail.fr>
2019-03-24 13:09:10 -0400

    VG: retrait d'objets D

--- common/communication/thematic-object.ts
+++ common/communication/thematic-object.ts
@@ -1,23 +1,16 @@
 export interface ThematicObject {
     name: string;
-    maxCount: number;
     baseScale: number;
 }
 
 export const THEMATIC_OBJECTS: ThematicObject[] = [
-    {name: "apple", maxCount: 4, baseScale: 0.8},
-    {name: "calculator", maxCount: 3, baseScale: 10},
-    {name: "coffeeMug", maxCount: 10, baseScale: 3},
-    {name: "banana", maxCount: 10, baseScale: 1},
-    {name: "orange", maxCount: 10, baseScale: 20},
-    {name: "sodaCan", maxCount: 10, baseScale: 20},
+    {name: "calculator", baseScale: 10},
+    {name: "coffeeMug", baseScale: 3},
+    {name: "banana", baseScale: 1},
 ];
 
 export enum ThematicObjectType {
-    APPLE,
     CALCULATOR,
     COFFEE_MUG,
     BANANA,
-    ORANGE,
-    SODA_CAN,
 }
