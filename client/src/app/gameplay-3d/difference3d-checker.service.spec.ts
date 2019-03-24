d1eadde9e316e57855397340a5dc6c279605bbf2
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-24 16:58:17 -0400

    MC :  initialisation des tests pour difference checker T

--- client/src/app/gameplay-3d/difference3d-checker.service.spec.ts
+++ client/src/app/gameplay-3d/difference3d-checker.service.spec.ts
@@ -1,12 +1,21 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
-
 import { Difference3DCheckerService } from "./difference3d-checker.service";
 
 describe("Difference3DCheckerService", () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    let differenceCheckerService: Difference3DCheckerService;
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [Difference3DCheckerService],
+        });
+        differenceCheckerService = TestBed.get(Difference3DCheckerService);
+        httpMock = TestBed.get(HttpTestingController);
+    });
 
     it("should be created", () => {
-        const service: Difference3DCheckerService = TestBed.get(Difference3DCheckerService);
-        expect(service).toBeTruthy();
+        expect(differenceCheckerService).toBeTruthy();
     });
 });
