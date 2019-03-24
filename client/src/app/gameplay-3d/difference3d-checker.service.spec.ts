e97fe54b10a345e39d7d35c8dc3eb36c14091a2f
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-24 17:09:06 -0400

    MC : test pour la requete de differences T

--- client/src/app/gameplay-3d/difference3d-checker.service.spec.ts
+++ client/src/app/gameplay-3d/difference3d-checker.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
+import { GeometryData } from "../../../../common/communication/geometry";
 import { Message, MessageType } from "../../../../common/communication/message";
 import { VectorInterface } from "../../../../common/communication/vector-interface";
 import { Difference3DCheckerService } from "./difference3d-checker.service";
@@ -31,6 +32,27 @@ describe("Difference3DCheckerService", () => {
             (value: boolean) => { expect(value).toBeTruthy(); },
         );
         const request: TestRequest = httpMock.expectOne(`${differenceCheckerService.URL}`);
+        expect(request.request.method).toBe("POST");
+        request.flush(response);
+    });
+
+    it("should be a POST REQUEST and should return a data set of geometry on getAllDifferences", () => {
+        const result: GeometryData[] = [{
+            color: 0xFFFFFF,
+            size: 10,
+            rotation: { x: 0, y: 0, z: 0 },
+            position: { x: 0, y: 0, z: 0 },
+            isModified: false,
+            type: 0,
+        }];
+        const response: Message = {
+            type: MessageType.SCENE_DATA,
+            body: JSON.stringify(result),
+        };
+        differenceCheckerService.getAllDifference("test").subscribe(
+            (value: GeometryData[]) => { expect(value).toBeDefined(); },
+        );
+        const request: TestRequest = httpMock.expectOne(`${differenceCheckerService.URL}/differences`);
         expect(request.request.method).toBe("POST");
         request.flush(response);
     });
