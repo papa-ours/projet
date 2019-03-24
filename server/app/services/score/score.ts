9c370212d58ba1e9f546d6a0bf6b85d185dcbd16
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-03-24 00:06:08 -0400

    PDB: Suppression de méthode de génération inutile D

--- server/app/services/score/score.ts
+++ server/app/services/score/score.ts
@@ -29,31 +29,4 @@ export class Score implements ScoreInterface {
         return ((this.seconds < FIRST_TWO_DIGITS_NUMBER) ? "0" : "") + this.seconds.toString();
     }
 
-    private makeMinutes(): void {
-        const MIN: number = 8;
-        const MAX: number = 15;
-
-        this.minutes = RandomNumber.randomInteger(MIN, MAX);
-    }
-
-    private makeSeconds(): void {
-        const MIN: number = 0;
-        const MAX: number = 59;
-
-        this.seconds = RandomNumber.randomInteger(MIN, MAX);
-    }
-
-    public makeUsername(): void {
-        const POSSIBLE_VALUES: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        const MIN_LENGTH: number = 3;
-        const MAX_LENGTH: number = 12;
-        const usernameLength: number = RandomNumber.randomInteger(MIN_LENGTH, MAX_LENGTH);
-        const username: string[] = [...Array(usernameLength)].map(() => {
-            const index: number = RandomNumber.randomInteger(0, POSSIBLE_VALUES.length - 1);
-
-            return POSSIBLE_VALUES.charAt(index);
-        });
-
-        this.username = username.join("");
-    }
 }
