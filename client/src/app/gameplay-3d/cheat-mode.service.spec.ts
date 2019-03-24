946a5190267e07e3a5ea67ca978c74bc0a14939b
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-24 09:34:03 -0400

    MC : fix cheat des tests pour cheat mode B

--- client/src/app/gameplay-3d/cheat-mode.service.spec.ts
+++ client/src/app/gameplay-3d/cheat-mode.service.spec.ts
@@ -45,10 +45,10 @@ describe("CheatModeService", () => {
     const originalRenderer: RenderService = new RenderService();
     const modifiedRenderer: RenderService = new RenderService();
     const sceneGenerator: SceneGeneratorService = new SceneGeneratorService(new GeometryFactoryService());
-    const div: HTMLDivElement = new HTMLDivElement();
+    const div: HTMLDivElement = document.createElement("div");
     let cheatModeService: CheatModeService;
 
-    beforeEach(() => {
+    beforeAll(() => {
         originalRenderer.initialize(div, sceneGenerator.createScene(ORIGINAL_GEOMETRY));
         modifiedRenderer.initialize(div, sceneGenerator.createScene(MODIFIED_GEOMETRY));
         cheatModeService = new CheatModeService(originalRenderer, modifiedRenderer);
@@ -65,74 +65,74 @@ describe("CheatModeService", () => {
         expect(cheatModeService["isActivated"]).toBeFalsy();
     });
 
-    it("should change the emissive color 4 times per seconds for the modified scene a color change", () => {
+    it("should change the emissive visibility 4 times per seconds for the modified scene a visibility change", () => {
         // tslint:disable:no-magic-numbers
         cheatModeService.toggleCheatMode(ORIGINAL_GEOMETRY);
         let mesh: THREE.Mesh = cheatModeService["modifiedRender"].scene.children[0] as THREE.Mesh;
-        let material: THREE.MeshStandardMaterial = mesh.material as THREE.MeshStandardMaterial;
-        let prevColor: number =  material.emissive.getHex();
+        let prevVisibility: boolean = mesh.visible;
 
-        const interval: number = window.setInterval(() => {
+        const interval: number = window.setInterval(
+        () => {
             mesh = cheatModeService["modifiedRender"].scene.children[0] as THREE.Mesh;
-            material = mesh.material as THREE.MeshStandardMaterial;
-            const color: number =  material.emissive.getHex();
-            expect(prevColor !== color).toBeTruthy();
-            prevColor = color;
-        },                                          253);
+            const visibility: boolean =  mesh.visible;
+            expect(prevVisibility !== visibility).toBeTruthy();
+            prevVisibility = visibility;
+        },
+        253);
         setTimeout(() => (clearInterval(interval)), 1100);
         // tslint:enable:no-magic-numbers
     });
 
-    it("should change the emissive color 4 times per seconds for the original scene on a color change", () => {
+    it("should change the emissive visibility 4 times per seconds for the original scene on a visibility change", () => {
         // tslint:disable:no-magic-numbers
         cheatModeService.toggleCheatMode(ORIGINAL_GEOMETRY);
         let mesh: THREE.Mesh = cheatModeService["originalRender"].scene.children[0] as THREE.Mesh;
-        let material: THREE.MeshStandardMaterial = mesh.material as THREE.MeshStandardMaterial;
-        let prevColor: number =  material.emissive.getHex();
+        let prevVisibility: boolean =  mesh.visible;
 
-        const interval: number = window.setInterval(() => {
+        const interval: number = window.setInterval(
+        () => {
             mesh = cheatModeService["originalRender"].scene.children[0] as THREE.Mesh;
-            material = mesh.material as THREE.MeshStandardMaterial;
-            const color: number =  material.emissive.getHex();
-            expect(prevColor !== color).toBeTruthy();
-            prevColor = color;
-        },                                          253);
+            const visibility: boolean =  mesh.visible;
+            expect(prevVisibility !== visibility).toBeTruthy();
+            prevVisibility = visibility;
+        },
+        253);
         setTimeout(() => (clearInterval(interval)), 1100);
         // tslint:enable:no-magic-numbers
     });
 
-    it("should change the emissive color 4 times per seconds for the orginal scene when an object is deleted", () => {
+    it("should change the emissive visibility 4 times per seconds for the orginal scene when an object is deleted", () => {
         // tslint:disable:no-magic-numbers
         cheatModeService.toggleCheatMode(ORIGINAL_GEOMETRY);
         let mesh: THREE.Mesh = cheatModeService["originalRender"].scene.children[1] as THREE.Mesh;
-        let material: THREE.MeshStandardMaterial = mesh.material as THREE.MeshStandardMaterial;
-        let prevColor: number =  material.emissive.getHex();
+        let prevVisibility: boolean =  mesh.visible;
 
-        const interval: number = window.setInterval(() => {
+        const interval: number = window.setInterval(
+        () => {
             mesh = cheatModeService["originalRender"].scene.children[1] as THREE.Mesh;
-            material = mesh.material as THREE.MeshStandardMaterial;
-            const color: number =  material.emissive.getHex();
-            expect(prevColor !== color).toBeTruthy();
-            prevColor = color;
-        },                                          253);
+            const visibility: boolean =  mesh.visible;
+            expect(prevVisibility !== visibility).toBeTruthy();
+            prevVisibility = visibility;
+        },
+        253);
         setTimeout(() => (clearInterval(interval)), 1100);
         // tslint:enable:no-magic-numbers
     });
 
-    it("should change the emissive color 4 times per seconds for the modified scene when an object is added", () => {
+    it("should change the emissive visibility 4 times per seconds for the modified scene when an object is added", () => {
         // tslint:disable:no-magic-numbers
         cheatModeService.toggleCheatMode(ORIGINAL_GEOMETRY);
         let mesh: THREE.Mesh = cheatModeService["modifiedRender"].scene.children[1] as THREE.Mesh;
-        let material: THREE.MeshStandardMaterial = mesh.material as THREE.MeshStandardMaterial;
-        let prevColor: number =  material.emissive.getHex();
+        let prevVisibility: boolean =  mesh.visible;
 
-        const interval: number = window.setInterval(() => {
+        const interval: number = window.setInterval(
+        () => {
             mesh = cheatModeService["modifiedRender"].scene.children[1] as THREE.Mesh;
-            material = mesh.material as THREE.MeshStandardMaterial;
-            const color: number =  material.emissive.getHex();
-            expect(prevColor !== color).toBeTruthy();
-            prevColor = color;
-        },                                          253);
+            const visibility: boolean =  mesh.visible;
+            expect(prevVisibility !== visibility).toBeTruthy();
+            prevVisibility = visibility;
+        },
+        253);
         setTimeout(() => (clearInterval(interval)), 1100);
         // tslint:enable:no-magic-numbers
     });
