9f44a0c7dea5b021a79e1bdb14c6120dbaf3a829
Vincent <vg_4_@hotmail.fr>
2019-03-24 13:08:55 -0400

    VG: Configuration de l'ajout d'objet theamtique D

--- server/app/services/scene/scene-difference-generator.ts
+++ server/app/services/scene/scene-difference-generator.ts
@@ -1,7 +1,8 @@
 import { injectable } from "inversify";
 import "reflect-metadata";
 import { REQUIRED_DIFFERENCES_1P } from "../../../../common/communication/constants";
-import { GeometryData, Modification, ModificationType } from "../../../../common/communication/geometry";
+import { GeometryData, GeometryType, Modification, ModificationType } from "../../../../common/communication/geometry";
+import { ThematicObjectType } from "../../../../common/communication/thematic-object";
 import { DeepCloner } from "../utils/deep-cloner";
 import { SceneDataGeneratorService } from "./scene-data-generator";
 
@@ -20,8 +21,13 @@ export class SceneDataDifferenceService {
         this.modificationMap.set(ModificationType.CHANGE_COLOR, this.changeColorGeometryData);
     }
 
-    private addGeometryData = (geometryDataDifference: GeometryData[], index: number): void => {
-        geometryDataDifference.push(this.sceneDataGeneratorService.getRandomNonIntersectingGeometryData(geometryDataDifference));
+    private addGeometryData = (geometryDataDifference: GeometryData[], index: number,
+                               size?: number, type?: GeometryType, thematicObjectType?: ThematicObjectType): void => {
+        geometryDataDifference.push(
+            this.sceneDataGeneratorService.getRandomNonIntersectingGeometryData(geometryDataDifference, size, type, thematicObjectType),
+        );
+        console.log(thematicObjectType);
+
         geometryDataDifference[geometryDataDifference.length - 1].isModified = true;
     }
 
@@ -36,22 +42,30 @@ export class SceneDataDifferenceService {
         geometryDataDifference[index].isModified = true;
     }
 
-    private applyRandomChange(geometryDataDifference: GeometryData[], randomIndex: number): void {
+    private applyRandomChange(geometryDataDifference: GeometryData[], randomIndex: number,
+                              size?: number, type?: GeometryType, thematicObjectType?: ThematicObjectType): void {
         const randomModificationIndex: number = Math.floor(Math.random() * this.modifications.length);
         const randomModificationType: ModificationType = this.modifications[randomModificationIndex].type;
         const randomFunction: Function = this.modificationMap.get(randomModificationType) as Function;
 
-        randomFunction(geometryDataDifference, randomIndex);
+        randomModificationType === ModificationType.ADD ?
+            randomFunction(geometryDataDifference, randomIndex, size, type, thematicObjectType) :
+            randomFunction(geometryDataDifference, randomIndex);
     }
 
-    public getDifference(geometryData: GeometryData[], modifications: Modification[]): GeometryData[] {
+    public getDifference(geometryData: GeometryData[], modifications: Modification[],
+                         sizes?: number[], type?: GeometryType): GeometryData[] {
         const geometryDataDifference: GeometryData[] = DeepCloner.clone(geometryData);
         this.modifications = modifications;
         let differenceCounter: number = 0;
         while (differenceCounter < REQUIRED_DIFFERENCES_1P) {
             const randomIndex: number = Math.floor(Math.random() * geometryDataDifference.length);
+            const thematicObjectType: ThematicObjectType | undefined =
+                sizes ? this.sceneDataGeneratorService.getRandomThematicObjectType() : undefined;
+            const size: number | undefined = thematicObjectType && sizes ? sizes[thematicObjectType] : undefined;
+
             if (!geometryDataDifference[randomIndex].isModified) {
-                this.applyRandomChange(geometryDataDifference, randomIndex);
+                this.applyRandomChange(geometryDataDifference, randomIndex, size, type, thematicObjectType);
                 differenceCounter++;
             }
         }
