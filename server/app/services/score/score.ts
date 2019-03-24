12dcda24eecee56e31722213f493d39ea23410d0
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-03-24 14:09:23 -0400

    PDB: réimplémentation de la génération aléatoire de scores D

--- server/app/services/score/score.ts
+++ server/app/services/score/score.ts
@@ -1,4 +1,5 @@
 import { ScoreInterface } from "../../../../common/communication/game-description";
+import { RandomNumber } from "../utils/random-number";
 
 export class Score implements ScoreInterface {
 
@@ -6,6 +7,27 @@ export class Score implements ScoreInterface {
     public static readonly SECONDS_PER_MINUTES: number = 60;
 
     public constructor(public time: number, public username: string) {
+    }
+
+    public static createRandom(): Score {
+        const MIN_TIME: number = 800;
+        const MAX_TIME: number = 1200;
+
+        return new Score(RandomNumber.randomInteger(MIN_TIME, MAX_TIME), Score.generateRandomUsername());
+    }
+
+    private static generateRandomUsername(): string {
+        const POSSIBLE_VALUES: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const MIN_LENGTH: number = 3;
+        const MAX_LENGTH: number = 12;
+        const usernameLength: number = RandomNumber.randomInteger(MIN_LENGTH, MAX_LENGTH);
+        const username: string[] = [...Array(usernameLength)].map(() => {
+            const index: number = RandomNumber.randomInteger(0, POSSIBLE_VALUES.length - 1);
+
+            return POSSIBLE_VALUES.charAt(index);
+        });
+
+        return username.join("");
     }
 
     public get seconds(): number {
@@ -34,31 +56,4 @@ export class Score implements ScoreInterface {
         return ((this.seconds < FIRST_TWO_DIGITS_NUMBER) ? "0" : "") + this.seconds.toString();
     }
 
-    private makeMinutes(): void {
-        const MIN: number = 8;
-        const MAX: number = 15;
-
-        this.minutes = RandomNumber.randomInteger(MIN, MAX);
-    }
-
-    private makeSeconds(): void {
-        const MIN: number = 0;
-        const MAX: number = 59;
-
-        this.seconds = RandomNumber.randomInteger(MIN, MAX);
-    }
-
-    public makeUsername(): void {
-        const POSSIBLE_VALUES: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        const MIN_LENGTH: number = 3;
-        const MAX_LENGTH: number = 12;
-        const usernameLength: number = RandomNumber.randomInteger(MIN_LENGTH, MAX_LENGTH);
-        const username: string[] = [...Array(usernameLength)].map(() => {
-            const index: number = RandomNumber.randomInteger(0, POSSIBLE_VALUES.length - 1);
-
-            return POSSIBLE_VALUES.charAt(index);
-        });
-
-        this.username = username.join("");
-    }
 }
