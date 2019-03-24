1d937c14149a1bc2c4b9a7adbc6350ef80dd2ff0
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-03-24 00:15:43 -0400

    PDB: ajout d'un "get method" pour scoresString D

--- server/app/services/score/top-scores.ts
+++ server/app/services/score/top-scores.ts
@@ -5,16 +5,10 @@ export class TopScores implements TopScoresInterface {
 
     public static readonly SCORE_LENGTH: number = 3;
 
-    public scores: Score[];
-    public scoresStrings: string[];
-
-    public constructor() {
-        this.createScores();
+    public constructor(public scores: Score[]) {
     }
 
-    private createScoreStrings(): void {
-        this.scoresStrings = this.scores.map((score: Score) => {
-            return score.toString();
-        });
+    public get scoresStrings(): string[] {
+        return this.scores.map((score: Score) => score.toString());
     }
 }
