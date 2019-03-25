2e62a3146d353afd1c382a56693c7784648d5021
David <david.piche@polymtl.ca>
2019-03-24 23:42:41 -0400

    DP: beforeEach pour les tests T

--- client/src/app/scene3d/render.service.spec.ts
+++ client/src/app/scene3d/render.service.spec.ts
@@ -2,3 +2,23 @@ import { ElementRef } from "@angular/core";
 import { TestBed } from "@angular/core/testing";
 import * as THREE from "three";
 import { RenderService } from "./render.service";
+
+describe("RenderService", () => {
+
+    let scene: THREE.Scene;
+    let container: HTMLDivElement;
+    let render: RenderService;
+    let elementRef: ElementRef;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                THREE.Scene,
+                RenderService,
+            ],
+        });
+        render = TestBed.get(RenderService);
+        scene = TestBed.get(THREE.Scene);
+        elementRef = new ElementRef(document.body);
+        container = elementRef.nativeElement;
+    });
+
