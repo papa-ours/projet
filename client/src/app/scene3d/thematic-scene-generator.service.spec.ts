c8b33f1fb107688f7a3b24c816e2164259372689
Vincent <vg_4_@hotmail.fr>
2019-03-24 23:25:16 -0400

    VG: Ajout de tests pour la generation de scene theme T

--- client/src/app/scene3d/thematic-scene-generator.service.spec.ts
+++ client/src/app/scene3d/thematic-scene-generator.service.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from "@angular/core/testing";
-
+import * as THREE from "three";
+import { GeometryData } from "../../../../common/communication/geometry";
+import { ThematicObjectGeneratorService } from "./thematic-object-generator.service";
 import { ThematicSceneGeneratorService } from "./thematic-scene-generator.service";
 
-describe("ThematicSceneGeneratorService", () => {
+fdescribe("ThematicSceneGeneratorService", () => {
     beforeEach(() => TestBed.configureTestingModule({}));
 
     it("should be created", () => {
         const service: ThematicSceneGeneratorService = TestBed.get(ThematicSceneGeneratorService);
         expect(service).toBeTruthy();
+    });
+
+    it("should return a scene object", async () => {
+        const sceneService: ThematicSceneGeneratorService = TestBed.get(ThematicSceneGeneratorService);
+        const objectService: ThematicObjectGeneratorService = TestBed.get(ThematicObjectGeneratorService);
+        objectService.waitForObjects().then(async () => {
+            const geometryData: GeometryData[] = [{
+                position: { x: 0 , y: 0 , z: 0 },
+                rotation: { x: 0 , y: 0 , z: 0 },
+                size: 30,
+                color: 0,
+                type: 0,
+                isModified: false,
+                thematicObjectType: 0,
+            }];
+            expect((await sceneService.createScene(geometryData)).type).toBe(new THREE.Scene().type);
+        });
     });
 });
