8bd5bdf56dc18c00a5d2038d4505d7d378757d09
Vincent <vg_4_@hotmail.fr>
2019-03-24 21:59:21 -0400

    VG: Ajout de tests pour les paramètres optionnels T

--- server/app/services/scene/scene-data-generator.spec.ts
+++ server/app/services/scene/scene-data-generator.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { VectorInterface } from "../../../../common/communication/vector-interface";
 import { SceneDataGeneratorService } from "./scene-data-generator";
 
 describe("scenceDataGenerator", () => {
@@ -14,9 +15,24 @@ describe("scenceDataGenerator", () => {
         expect(sceneDataGeneratorService.getSceneData(nGeometry).length).to.equal(nGeometry);
     });
 
+    it("should create sceneData if the parameter sizes is defined", () => {
+        const nGeometry: number = 10;
+        const sizes: number[] = [1, 1, 1];
+        expect(sceneDataGeneratorService.getSceneData(nGeometry, sizes).length).to.equal(nGeometry);
+    });
+
     it("Should throw an error if the number of geometry is lower than 10 on getSceneData", () => {
         const nGeometry: number = 3;
         expect(() => sceneDataGeneratorService.getSceneData(nGeometry)).to.throw("Number should be beetwen 10 and 200");
+    });
+
+    it("should return 0 rotation for x and z axes if y is fixed", () => {
+        const result: VectorInterface = sceneDataGeneratorService.getRandomRotation(true);
+        expect(result.x === 0 && result.z === 0).to.equal(true);
+    });
+
+    it("should return 0 for y position if y is fixed", () => {
+        expect(sceneDataGeneratorService.getRandomPosition(true).y).to.equal(0);
     });
 
     it("Should throw an error if the number of geometry is greater than 200 on getSceneData", () => {
