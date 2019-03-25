e9a56f9be857e5f87d3c4a336837182445e547f5
Vincent <vg_4_@hotmail.fr>
2019-03-24 22:41:43 -0400

    VG: Ajout des tests pour ThematicObjectGeneratorService T

--- client/src/app/scene3d/thematic-object-generator.service.spec.ts
+++ client/src/app/scene3d/thematic-object-generator.service.spec.ts
@@ -1,12 +1,37 @@
 import { TestBed } from "@angular/core/testing";
 
+import { ThematicObjectType } from "../../../../common/communication/thematic-object";
 import { ThematicObjectGeneratorService } from "./thematic-object-generator.service";
 
-describe("ThematicObjectGeneratorService", () => {
+fdescribe("ThematicObjectGeneratorService", () => {
     beforeEach(() => TestBed.configureTestingModule({}));
-
     it("should be created", () => {
         const service: ThematicObjectGeneratorService = TestBed.get(ThematicObjectGeneratorService);
         expect(service).toBeTruthy();
+    });
+
+    it("should create the objects", async () => {
+        const service: ThematicObjectGeneratorService = TestBed.get(ThematicObjectGeneratorService);
+
+        service.waitForObjects()
+        .then(() => {
+            expect(ThematicObjectGeneratorService.areObjectsLoaded).toBeTruthy();
+        });
+    });
+
+    it("should return the right object", async () => {
+        const service: ThematicObjectGeneratorService = TestBed.get(ThematicObjectGeneratorService);
+
+        service.waitForObjects().then(() => {
+            const object: THREE.Group = service.getObject(ThematicObjectType.APPLE);
+            expect(object).toBeDefined();
+        });
+    });
+
+    it("should throw if the object does not exist", async () => {
+        const service: ThematicObjectGeneratorService = TestBed.get(ThematicObjectGeneratorService);
+        service.waitForObjects().then(() => {
+            expect(() => service.getObject(-1)).toThrowError("Object requested does not exist");
+        });
     });
 });
