3579f8f1c089fea2848c8305df046089fb79cbb5
Vincent <vg_4_@hotmail.fr>
2019-03-24 21:25:00 -0400

    VG: Retrait d'un variable inutilisée R

--- client/src/app/scene3d/thematic-scene-generator.service.ts
+++ client/src/app/scene3d/thematic-scene-generator.service.ts
@@ -35,7 +35,6 @@ export class ThematicSceneGeneratorService {
         try {
             if (data.thematicObjectType !== undefined) {
                 const group: THREE.Group = this.thematicObjectGeneratorService.getObject(data.thematicObjectType);
-                const MINUS_HALF: number = 0.5;
                 group.position.set(data.position.x, data.position.y, data.position.z);
                 group.rotation.set(data.rotation.x, data.rotation.y, data.rotation.z);
                 group.traverse((object: THREE.Object3D) => {
