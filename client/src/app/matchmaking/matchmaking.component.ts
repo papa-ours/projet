508c8e163fe67a9236fd3dd05c1c40f61e5ac6fb
Vincent <vg_4_@hotmail.fr>
2019-03-25 20:57:55 -0400

    VG: Ajout du username dans le matchmaking D

--- client/src/app/matchmaking/matchmaking.component.ts
+++ client/src/app/matchmaking/matchmaking.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ConnectionService } from "../connection.service";
 
 @Component({
     selector: "app-matchmaking",
     templateUrl: "./matchmaking.component.html",
     styleUrls: ["./matchmaking.component.css"],
 })
-export class MatchmakingComponent {
+export class MatchmakingComponent implements OnInit {
+    public username: string;
 
+    public constructor(
+        private connectionService: ConnectionService,
+        private router: Router,
+    ) {}
+
+    public ngOnInit(): void {
+        if (!this.connectionService.connected) {
+            this.router.navigateByUrl("");
+        }
+        this.username = this.connectionService.username;
+    }
 }
