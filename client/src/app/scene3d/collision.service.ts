b6b41ef2f213713956f0dd9e81bb51126c9cfaac
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-28 16:28:18 -0400

    MC : detection collision D

--- client/src/app/scene3d/collision.service.ts
+++ client/src/app/scene3d/collision.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from "@angular/core";
+import * as THREE from "three";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: "root",
 })
 export class CollisionService {
 
-  public constructor() { }
+    public static canMove(scene: THREE.Scene, camera: THREE.Camera, cameradirection: THREE.Vector3): boolean {
+        const raycaster: THREE.Raycaster = new THREE.Raycaster();
+        const normalisedDirection: THREE.Vector3 = cameradirection.clone().normalize();
+        raycaster.set(camera.position, normalisedDirection);
+        const intersections: THREE.Intersection[] = raycaster.intersectObjects(scene.children, true);
+
+        return intersections.length <= 0;
+    }
+
 }
