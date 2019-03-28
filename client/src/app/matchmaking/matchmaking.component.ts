a2cd0596b2ce88adbf003298c744f76fa35cf71b
Vincent <vg_4_@hotmail.fr>
2019-03-28 17:13:47 -0400

    VG VM: Traitement de l'evenemtn lorsque qqn rejoint la partie D

--- client/src/app/matchmaking/matchmaking.component.ts
+++ client/src/app/matchmaking/matchmaking.component.ts
@@ -1,8 +1,12 @@
 import { Location } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Params, Router} from "@angular/router";
 import { faUser, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { Subscription } from "rxjs";
 import { ConnectionService } from "../connection.service";
+import { GameplayService } from "../gameplay.service";
+import { SocketService } from "../socket.service";
+import { GameMode } from "../../../../common/communication/game-description";
 
 @Component({
     selector: "app-matchmaking",
@@ -12,19 +16,31 @@ import { ConnectionService } from "../connection.service";
 export class MatchmakingComponent implements OnInit {
     public username: string;
     public faUser: IconDefinition = faUser;
-    public other: string;
+    public joinSubscription: Subscription;
+
     public constructor(
+        private route: ActivatedRoute,
         private connectionService: ConnectionService,
         private location: Location,
         private router: Router,
+        private socketService: SocketService,
+        private gameplayService: GameplayService,
     ) {
-       this.other = "";
     }
 
     public ngOnInit(): void {
         this.connectionService.connected ?
             this.username = this.connectionService.username :
             this.router.navigateByUrl("").catch((error: Error) => console.error(error.message));
+
+        this.route.params.subscribe((params: Params) => {
+            this.gameplayService.createWaitingRoom(params["name"], params["type"], this.username).subscribe(() => {
+                this.joinSubscription = this.socketService.getPlayerHasJoined().subscribe((id: string) => {
+                    this.router.navigateByUrl(`game/${params["name"]}/${params["type"]}/${GameMode.Pvp}/${id}`)
+                    .catch((error: Error) => console.error(error.message));
+                });
+            });
+        });
     }
 
     public goToGameList(): void {
