72baa56b787522cfb1cd17fbea95741cfd4337f2
Vincent <vg_4_@hotmail.fr>
2019-03-28 10:43:23 -0400

    VG: Centralisation des préférences dans un interface D

--- client/src/app/color-picker/color-picker.component.ts
+++ client/src/app/color-picker/color-picker.component.ts
@@ -6,6 +6,12 @@ interface ColorPalette {
     secondary: string;
 }
 
+interface ColorPreference {
+    isDark: boolean;
+    isTextWhite: boolean;
+    palette: ColorPalette;
+}
+
 @Component({
     selector: "app-color-picker",
     templateUrl: "./color-picker.component.html",
@@ -18,11 +24,10 @@ export class ColorPickerComponent {
     public isPanelShown: boolean;
     public isTextWhite: boolean;
     public isDarkTheme: boolean;
+    private activeColorPreference: ColorPreference;
 
     public constructor(private cookieService: CookieService) {
         this.isPanelShown = false;
-        this.isDarkTheme = false;
-
         this.colorPalettes = [
             {primary: "#379BFF", secondary: "#8AC4FF"},
             {primary: "#F4429B", secondary: "#D1297D"},
@@ -34,19 +39,34 @@ export class ColorPickerComponent {
             {primary: "#07C4BA", secondary: "#1AEFE4"},
         ];
 
-        this.cookieService.check(this.COOKIE_KEY) ?
-            this.colorSelected(JSON.parse(this.cookieService.get(this.COOKIE_KEY))) :
-            this.colorSelected(this.colorPalettes[0]);
+        this.activeColorPreference = {
+            palette: this.cookieService.check(this.COOKIE_KEY) ?
+                JSON.parse(this.cookieService.get(this.COOKIE_KEY)).palette : this.colorPalettes[0],
+            isDark: false,
+            isTextWhite: true,
+        };
+
+        this.changeColor();
         this.changeTheme();
         this.changeTextColor();
     }
 
     public colorSelected(colorPalette: ColorPalette): void {
-        const root: HTMLElement = this.getRoot();
-        root.style.setProperty("--primary-color", colorPalette.primary);
-        root.style.setProperty("--secondary-color", colorPalette.secondary);
+        this.activeColorPreference.palette = colorPalette;
+        this.changeColor();
 
-        this.cookieService.set(this.COOKIE_KEY, JSON.stringify(colorPalette));
+    }
+
+    private changeColor(): void {
+        const root: HTMLElement = this.getRoot();
+        root.style.setProperty("--primary-color", this.activeColorPreference.palette.primary);
+        root.style.setProperty("--secondary-color", this.activeColorPreference.palette.secondary);
+
+        this.savePreference();
+    }
+
+    private savePreference(): void {
+        this.cookieService.set(this.COOKIE_KEY, JSON.stringify(this.activeColorPreference));
     }
 
     public changePanelState(): void {
@@ -54,31 +74,33 @@ export class ColorPickerComponent {
     }
 
     public changeTheme(): void {
-        this.isDarkTheme = !this.isDarkTheme;
+        this.activeColorPreference.isDark = !this.activeColorPreference.isDark;
         const root: HTMLElement = this.getRoot();
         this.changeHeaderColor(root);
         this.changeHeaderSecondaryColor(root);
         this.changeHeaderTextColor(root);
+
+        this.savePreference();
     }
 
     private changeHeaderColor(root: HTMLElement): void {
-        const headerColor: string = this.isDarkTheme ? "#332323" : "#DADADA";
+        const headerColor: string = this.activeColorPreference.isDark ? "#332323" : "#DADADA";
         root.style.setProperty("--header-color", headerColor);
     }
 
     private changeHeaderTextColor(root: HTMLElement): void {
-        const headerTextColor: string = this.isDarkTheme ? "#FFFFFF" : "#000000";
+        const headerTextColor: string = this.activeColorPreference.isDark ? "#FFFFFF" : "#000000";
         root.style.setProperty("--header-text-color", headerTextColor);
     }
 
     private changeHeaderSecondaryColor(root: HTMLElement): void {
-        const headerSecondaryColor: string = this.isDarkTheme ? "#404040" : "#CCCCCC";
+        const headerSecondaryColor: string = this.activeColorPreference.isDark ? "#404040" : "#CCCCCC";
         root.style.setProperty("--header-secondary-color", headerSecondaryColor);
     }
 
     public changeTextColor(): void {
-        this.isTextWhite = !this.isTextWhite;
-        const textColor: string = this.isTextWhite ? "#FFFFFF" : "#000000";
+        this.activeColorPreference.isTextWhite = !this.activeColorPreference.isTextWhite;
+        const textColor: string = this.activeColorPreference.isTextWhite ? "#FFFFFF" : "#000000";
         this.getRoot().style.setProperty("--text-color", textColor);
     }
 
