ec313d9908e506701f6bd78a20a56a17f7158e49
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-28 20:24:04 -0400

    MC : tslint import en ordre R

--- client/src/app/scene3d/collision.service.ts
+++ client/src/app/scene3d/collision.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { SKYBOX_MAX, SKYBOX_MIN} from "../../../../common/communication/skybox";
 import * as THREE from "three";
+import { SKYBOX_MAX, SKYBOX_MIN} from "../../../../common/communication/skybox";
 
 @Injectable({
     providedIn: "root",
