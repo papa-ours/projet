bfedbb030aff2d6117819ae56b6adcf96ba286e3
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-03-28 20:23:33 -0400

    MC : fix skybox B

--- common/communication/skybox.ts
+++ common/communication/skybox.ts
@@ -1,5 +1,5 @@
 import { VectorInterface } from "./vector-interface";
 // tslint:disable-next-line:no-magic-numbers
-export const SKYBOX_MIN: VectorInterface = { x:-1000, y:-1000, z:-500 };
+export const SKYBOX_MIN: VectorInterface = { x:-1000, y:-1000, z:-5000};
 // tslint:disable-next-line:no-magic-numbers
-export const SKYBOX_MAX: VectorInterface = { x:1000, y:1000, z:-5000 };
+export const SKYBOX_MAX: VectorInterface = { x:1000, y:1000, z: 450};
