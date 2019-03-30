4e03856f0301ae2fa634b7060811ec8c34ec68aa
Vincent <vg_4_@hotmail.fr>
2019-03-30 12:06:07 -0400

    VG: Utilisation des Rooms de SocketIO D

--- server/app/services/game/waiting-room.ts
+++ server/app/services/game/waiting-room.ts
@@ -1,10 +1,33 @@
+import { inject } from "inversify";
 import { GameType } from "../../../../common/communication/game-description";
+import { Socket } from "../../socket";
+import Types from "../../types";
+import { UsersContainerService } from "../users-container.service";
 
 export class WaitingRoom {
+    public usernames: string[];
+
     public constructor(
         public gameSheetId: string,
-        public username: string,
+        username: string,
         public type: GameType,
+        @inject(Types.UsersContainerService) private usersContainer: UsersContainerService,
     ) {
+        this.usernames = [];
+        this.addUser(username);
+    }
+
+    public addUser(username: string): void {
+        this.usernames.push(username);
+        this.joinRoom(username);
+    }
+
+    private joinRoom(username: string): void {
+        const socketId: string = this.usersContainer.getSocketIdByUsername(username);
+        const socket: SocketIO.Socket | undefined = Socket.getSocket(socketId);
+
+        if (socket) {
+            socket.join(this.gameSheetId);
+        }
     }
 }
