36969b87f983d8a78b286624c183b7dbdc10d24d
Vincent <vg_4_@hotmail.fr>
2019-03-30 14:08:29 -0400

    VG: Les types de retour sont void D

--- client/src/app/gameplay.service.ts
+++ client/src/app/gameplay.service.ts
@@ -21,19 +21,15 @@ export class GameplayService {
         );
     }
 
-    public createWaitingRoom(name: string, type: GameType, username: string): Observable<string> {
-        return this.http.post<Message>(`${this.URL}/waitingRoom/create/`, {name: name, type: type, username: username})
-            .pipe(map((message: Message) => JSON.parse(message.body)),
-        );
+    public createWaitingRoom(name: string, type: GameType, username: string): Observable<void> {
+        return this.http.post<void>(`${this.URL}/waitingRoom/create/`, {name: name, type: type, username: username});
     }
 
     public deleteWaitingRoom(name: string, type: GameType, username: string): Observable<void> {
         return this.http.delete<void>(`${this.URL}/waitingRoom/${name}/${username}/${type}`);
     }
 
-    public joinWaitingRoom(name: string, type: GameType, username: string): Observable<string> {
-        return this.http.post<Message>(`${this.URL}/waitingRoom/join/`, {name: name, type: type, username: username})
-            .pipe(map((message: Message) => JSON.parse(message.body)),
-        );
+    public joinWaitingRoom(name: string, type: GameType, username: string): Observable<void> {
+        return this.http.post<void>(`${this.URL}/waitingRoom/join/`, {name: name, type: type, username: username});
     }
 }
