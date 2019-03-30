78327ae06e0b4b9551a3af4b7ba340f5998558d1
Vincent <vg_4_@hotmail.fr>
2019-03-29 22:28:38 -0400

    vG: Ajout de 3 attributs a WaitingRoom D

--- server/app/services/game/waiting-room.ts
+++ server/app/services/game/waiting-room.ts
@@ -1,3 +1,11 @@
+import { GameType } from "../../../../common/communication/game-description";
+
 export class WaitingRoom {
     public gameSheetId: string;
+
+    public constructor(
+        public name: string,
+        public username: string,
+        public type: GameType,
+    ) {}
 }
