9cf4bae9eb29ac6b0eb671feb093bc098c453191
Vincent <vg_4_@hotmail.fr>
2019-03-30 16:41:24 -0400

    VG: Création d'un service pour gérer les WaitingRooms D

---
+++ server/app/services/waiting-room.service.ts
@@ -0,0 +1,55 @@
+import { injectable } from "inversify";
+import "reflect-metadata";
+import { GameType } from "../../../common/communication/game-description";
+import { Socket } from "../socket";
+import { WaitingRoom } from "./game/waiting-room";
+import Axios from "axios";
+import { SERVER_ADDRESS } from "../../../common/communication/constants";
+
+@injectable()
+export class WaitingRoomService {
+    private static readonly waitingRooms: [WaitingRoom[], WaitingRoom[]] = [[], []];
+
+    public createWaitingRoom(name: string, username: string, type: GameType): void {
+        this.getSheetId(name, type)
+        .then((id: string) => {
+            Socket.io.emit(`GameCreated-${id}`, true);
+            WaitingRoomService.waitingRooms[type]
+                .push(new WaitingRoom(id, name, username, type));
+        })
+        .catch((error: Error) => console.error(error.message));
+    }
+
+    public joinWaitingRoom(name: string, username: string, type: GameType): void {
+        this.getSheetId(name, type)
+        .then((id: string) => {
+            const waitingRoom: WaitingRoom | undefined = WaitingRoomService.waitingRooms[type].find((currentWaitingRoom: WaitingRoom) => {
+                return currentWaitingRoom.gameSheetId === id;
+            });
+
+            if (waitingRoom) {
+                waitingRoom.addUser(username);
+            }
+        })
+        .catch((error: Error) => console.error(error.message));
+    }
+
+    public deleteWaitingRoom(name: string, username: string, type: GameType): void {
+        this.getSheetId(name, type)
+        .then((id: string) => {
+            const index: number = WaitingRoomService.waitingRooms[type].findIndex((waitingRoom: WaitingRoom) => {
+                return waitingRoom.gameSheetId === id && waitingRoom.usernames[0] === username;
+            });
+
+            if (index !== -1) {
+                Socket.io.emit(`GameCreated-${id}`, false);
+                WaitingRoomService.waitingRooms[type].splice(index, 1);
+            }
+        })
+        .catch((error: Error) => console.error(error.message));
+    }
+
+    private async getSheetId(name: string, type: GameType): Promise<string> {
+        return (await Axios.get(`${SERVER_ADDRESS}/api/game/sheet/id/${name}/${type}`)).data.body;
+    }
+}
