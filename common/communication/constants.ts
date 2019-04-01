09162e652cdbf4b0e28d8b4d94f1ad851b5aa884
Vincent <vg_4_@hotmail.fr>
2019-04-01 14:46:56 -0400

    VG: Correction de l'adresse IP D

--- common/communication/constants.ts
+++ common/communication/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_ADDRESS: string = "http://10.200.30.135:5001";
+export const SERVER_ADDRESS: string = "http://192.168.1.10:5001";
 
 export const BMP_IMAGE_WIDTH: number = 640;
 export const BMP_IMAGE_HEIGHT: number = 480;
