a4bcb32f366e26b1ae05f52ec0e38af77048bafa
Vincent <vg_4_@hotmail.fr>
2019-04-01 10:13:29 -0400

    VG: Le serveur est l'adresse IP de mon ordi A

--- common/communication/constants.ts
+++ common/communication/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_ADDRESS: string = "http://localhost:3000"
+export const SERVER_ADDRESS: string = "http://10.200.30.135:5001";
 
 export const BMP_IMAGE_WIDTH: number = 640;
 export const BMP_IMAGE_HEIGHT: number = 480;
