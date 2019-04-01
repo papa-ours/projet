42844d128e039dfbef8910627c364b33a35f0fe5
Vincent <vg_4_@hotmail.fr>
2019-04-01 10:13:09 -0400

    VG: Vérification de la présence de waitingRoom pour chauqe GameSheet D

--- server/app/services/get-game-list.service.ts
+++ server/app/services/get-game-list.service.ts
@@ -1,8 +1,12 @@
+import Axios from "axios";
 import { inject, injectable } from "inversify";
 import "reflect-metadata";
+import { SERVER_ADDRESS } from "../../../common/communication/constants";
 import { GameLists, GameSheet, GameType } from "../../../common/communication/game-description";
 import Types from "../types";
 import { DBConnectionService } from "./db-connection.service";
+import { WaitingRoom } from "./game/waiting-room";
+
 @injectable()
 export class GetGameListService {
 
@@ -11,6 +15,22 @@ export class GetGameListService {
     public async getGameList(): Promise<GameLists> {
         const types: GameType[] = [GameType.Simple, GameType.Free];
         const gameSheets: Map<GameType, GameSheet[]> = await this.db.getGameSheets(types);
+
+        const waitingRooms: WaitingRoom[][] = (await Axios.get(`${SERVER_ADDRESS}/api/game/waitingRoom/listAll`)).data;
+
+        waitingRooms.forEach((list: WaitingRoom[]) => {
+            list.forEach((waitingRoom: WaitingRoom) => {
+                const gameSheetList: GameSheet[] | undefined = gameSheets.get(waitingRoom.type);
+
+                if (gameSheetList) {
+                    const index: number = gameSheetList.findIndex((gs: GameSheet) => gs.id === waitingRoom.gameSheetId);
+                    if (index !== -1) {
+                        (gameSheets.get(waitingRoom.type) as GameSheet[])[index].hasWaitingRoom = true;
+                    }
+                }
+
+            });
+        });
 
         return {
             list2d: gameSheets.get(GameType.Simple) as GameSheet[],
