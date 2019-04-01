81fec94cbdd4a537333700ddd0c158d15574935a
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-04-01 11:16:23 -0400

    MC : fix calcule de direction B

--- client/src/app/scene3d/collision.service.ts
+++ client/src/app/scene3d/collision.service.ts
@@ -8,6 +8,15 @@ import { SKYBOX_MAX, SKYBOX_MIN} from "../../../../common/communication/skybox";
 export class CollisionDetectionService {
     public static MAX_CAST_DISTANCE: number = 20;
 
+    private static computeDirection(camera: THREE.Camera, cameraDirection: THREE.Vector3): THREE.Vector3 {
+        const position: THREE.Vector3 = camera.position.clone();
+        const direction: THREE.Vector3 = cameraDirection.clone();
+        direction.x = cameraDirection.x * Math.cos(camera.rotation.x);
+        direction.z = cameraDirection.z * Math.cos(camera.rotation.z);
+
+        return  position.add(direction);
+    }
+
     public static canMove(scene: THREE.Scene, camera: THREE.Camera, cameraDirection: THREE.Vector3): boolean {
         const normalisedDirection: THREE.Vector3 = cameraDirection.clone().normalize();
         const raycaster: THREE.Raycaster = new THREE.Raycaster(
@@ -22,8 +31,7 @@ export class CollisionDetectionService {
     }
 
     public static isInsideBorder(camera: THREE.Camera, cameraDirection: THREE.Vector3): boolean {
-        const position: THREE.Vector3 = camera.position.clone();
-        position.add(cameraDirection);
+        const position: THREE.Vector3 = CollisionDetectionService.computeDirection(camera, cameraDirection);
 
         return (SKYBOX_MAX.x > position.x && position.x > SKYBOX_MIN.x &&
                 SKYBOX_MAX.y > position.y && position.y > SKYBOX_MIN.y &&
