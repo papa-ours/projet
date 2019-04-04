26c21b6cf3838e89dd7851887dc1ac2ce8eac127
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-04-04 15:44:28 -0400

    MC : formatage du fichier R

--- server/app/services/utils/chrono.spec.ts
+++ server/app/services/utils/chrono.spec.ts
@@ -1,9 +1,12 @@
 import { expect } from "chai";
 import { Chrono } from "./chrono";
+
 describe("chrono", () => {
     const chrono: Chrono = new Chrono();
+
     it("should return a positive time ", () => {
         chrono.start();
         expect(chrono.time).to.be.gte(0);
     });
+
 });
