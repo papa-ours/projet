90c11be051152149c36089bfeb04f0d8622f70fa
David <david.piche@polymtl.ca>
2019-04-04 15:27:01 -0400

    DP: Ajout methode returnToLobby D

--- client/src/app/game-over-message/game-over-message.component.ts
+++ client/src/app/game-over-message/game-over-message.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-game-over-message',
@@ -7,8 +8,15 @@ import { Component } from '@angular/core';
 })
 export class GameOverMessageComponent {
 
-  constructor() { }
+    public constructor(private router: Router) {
+        
+    }
 
-  }
-
+    public returnToLobby(): void {
+        this.router.navigateByUrl("/gamelist/" + "aaa")
+                    .catch((err: Error) => {
+                        console.error(err);
+                    },
+                );
+    }
 }
