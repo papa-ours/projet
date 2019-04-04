66bac546ccf4d238eac1352b4c90dd4eb8232e5e
David <david.piche@polymtl.ca>
2019-04-04 14:08:23 -0400

    DP: Ajout de component pour message de fin de partie D

---
+++ client/src/app/game-over-message/game-over-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-over-message',
+  templateUrl: './game-over-message.component.html',
+  styleUrls: ['./game-over-message.component.css']
+})
+export class GameOverMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
