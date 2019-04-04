d2c10920530772b5a0753ae3c36735869bd0a0f0
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-04-04 15:11:20 -0400

    MC : fix test qui bloque B

--- server/app/services/get-game-list.service.spec.ts
+++ server/app/services/get-game-list.service.spec.ts
@@ -1,5 +1,4 @@
 import { expect } from "chai";
-import { GameLists } from "../../../common/communication/game-description";
 import { container } from "../inversify.config";
 import Types from "../types";
 import { GetGameListService } from "./get-game-list.service";
@@ -7,7 +6,6 @@ import { GetGameListService } from "./get-game-list.service";
 describe("get game list", () => {
     const getGameListService: GetGameListService = container.get<GetGameListService>(Types.GetGameListService);
     it("should return the game list properly", async () => {
-        const result: GameLists = await getGameListService.getGameList();
-        expect(result).to.not.equals(undefined);
+        expect(await getGameListService.getGameList()).to.not.equals(undefined);
     });
 });
