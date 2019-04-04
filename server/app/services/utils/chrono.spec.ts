af6d9714e8a4786f34a15196dc61a763ba084345
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-04-04 15:40:12 -0400

    MC : test positive time pour chrono T

--- server/app/services/utils/chrono.spec.ts
+++ server/app/services/utils/chrono.spec.ts
@@ -1,5 +1,9 @@
 import { expect } from "chai";
 import { Chrono } from "./chrono";
 describe("chrono", () => {
-    const chrono : Chrono = new Chrono();
+    const chrono: Chrono = new Chrono();
+    it("should return a positive time ", () => {
+        chrono.start();
+        expect(chrono.time).to.be.gte(0);
+    });
 });
