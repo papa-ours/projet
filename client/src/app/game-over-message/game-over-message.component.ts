acfcdf33f1221264369f53f1db876d603f98fa84
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-04-04 20:16:41 -0400

    VM: tslint "" R

--- client/src/app/game-over-message/game-over-message.component.ts
+++ client/src/app/game-over-message/game-over-message.component.ts
@@ -1,16 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-game-over-message',
-  templateUrl: './game-over-message.component.html',
-  styleUrls: ['./game-over-message.component.css']
+  selector: "app-game-over-message",
+  templateUrl: "./game-over-message.component.html",
+  styleUrls: ["./game-over-message.component.css"],
 })
 export class GameOverMessageComponent {
 
-    public constructor(private router: Router) {
-        
-    }
+    public constructor(private router: Router) {}
 
     public returnToLobby(): void {
         this.router.navigateByUrl("/gamelist/" + "aaa")
