4315482eb87554f3140ccc02f24f31e9e75461b9
David <david.piche@polymtl.ca>
2019-04-08 11:19:50 -0400

    DP: AJout de foction checkName D

--- client/src/app/game-name-checker.service.ts
+++ client/src/app/game-name-checker.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { GameType } from "../../../common/communication/game-description";
 import { GetGameService } from "../../../server/app/services/get-game.service";
 @Injectable({
     providedIn: "root",
@@ -7,7 +8,9 @@ export class GameNameChecker {
 
     public constructor(private getGameService: GetGameService) { }
 
+    public async checkName(name: string, type: GameType): Promise<boolean>{
 
-        return false;
+        const id: string = await this.getGameService.getSheetId(name, type);
+        return id !== undefined;
     }
 }
