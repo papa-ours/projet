e26c739bf2c8690ba7203527404f2a8392bc1452
Vincent <vg_4_@hotmail.fr>
2019-04-08 11:51:55 -0400

    VG: Utilisation des infos du ficihier colors.ts R

--- client/src/app/color-picker/color-picker.component.ts
+++ client/src/app/color-picker/color-picker.component.ts
@@ -1,17 +1,7 @@
 import { Component } from "@angular/core";
 import { faFont, faMoon, faPalette, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { CookieService } from "ngx-cookie-service";
-
-interface ColorPalette {
-    primary: string;
-    secondary: string;
-}
-
-interface ColorPreference {
-    isDark: boolean;
-    isTextWhite: boolean;
-    palette: ColorPalette;
-}
+import { ColorPalette, ColorPreference, COLORS_PALETTES } from "./colors";
 
 @Component({
     selector: "app-color-picker",
@@ -31,16 +21,7 @@ export class ColorPickerComponent {
 
     public constructor(private cookieService: CookieService) {
         this.isPanelShown = false;
-        this.colorPalettes = [
-            {primary: "#379BFF", secondary: "#8AC4FF"},
-            {primary: "#F4429B", secondary: "#D1297D"},
-            {primary: "#F73D1D", secondary: "#FF6247"},
-            {primary: "#FF8B1E", secondary: "#FFB049"},
-            {primary: "#BEC619", secondary: "#F2FF00"},
-            {primary: "#2CA02C", secondary: "#35D642"},
-            {primary: "#9608CE", secondary: "#B826F2"},
-            {primary: "#07C4BA", secondary: "#1AEFE4"},
-        ];
+        this.colorPalettes = COLORS_PALETTES;
 
         this.activeColorPreference = this.cookieService.check(this.COOKIE_KEY) ?
             JSON.parse(this.cookieService.get(this.COOKIE_KEY)) :
