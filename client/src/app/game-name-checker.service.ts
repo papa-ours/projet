c0286e5594773a8da33637bec66031399bce365b
David <david.piche@polymtl.ca>
2019-04-08 11:19:26 -0400

    DP: Ajout de constructeur D

--- client/src/app/game-name-checker.service.ts
+++ client/src/app/game-name-checker.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-
+import { GetGameService } from "../../../server/app/services/get-game.service";
 @Injectable({
     providedIn: "root",
 })
 export class GameNameChecker {
 
-    public checkName(): boolean{
+    public constructor(private getGameService: GetGameService) { }
 
 
         return false;
