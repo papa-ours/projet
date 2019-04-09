26dabe46caf3f005b6f0606415914aeb318edb16
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-04-09 10:20:46 -0400

    MC : separation du chorono dans gameplay view R

--- client/src/app/gameplay-view/gameplay-view.component.ts
+++ client/src/app/gameplay-view/gameplay-view.component.ts
@@ -1,6 +1,5 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild  } from "@angular/core";
+import { Component, ElementRef, EventEmitter, HostListener, OnInit, Output, ViewChild  } from "@angular/core";
 import { ActivatedRoute, Params, Router } from "@angular/router";
-import { faHourglassHalf, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { REQUIRED_DIFFERENCES_1P, REQUIRED_DIFFERENCES_2P } from "../../../../common/communication/constants";
 import { GameMode, GameType } from "../../../../common/communication/game-description";
 import { ChatMessage } from "../../../../common/communication/message";
@@ -16,10 +15,11 @@ import { SocketService } from "../socket.service";
 export class GameplayViewComponent implements OnInit {
 
     public gameMode: GameMode;
-    public readonly hourglassIcon: IconDefinition = faHourglassHalf;
     private readonly CORRECT_SOUND: HTMLAudioElement = new Audio("../../../assets/sound/Correct-answer.ogg");
     private readonly WRONG_SOUND: HTMLAudioElement = new Audio("../../../assets/sound/Wrong-answer.mp3");
     private readonly ERROR_TIMEOUT: number = 1000;
+    private isChronoRunning: boolean;
+    @Output() public startChrono: EventEmitter <boolean>;
 
     public totalDifferenceCounter: number;
     public foundDifferencesCounters: number[];
@@ -29,8 +29,6 @@ export class GameplayViewComponent implements OnInit {
     public canClick: boolean;
     public isErrorMessageVisible: boolean;
     public clickPosition: Position;
-    public chrono: number;
-    private isChronoRunning: boolean;
 
     @ViewChild("container") private containerRef: ElementRef;
 
@@ -46,10 +44,8 @@ export class GameplayViewComponent implements OnInit {
         this.images = [];
         this.canClick = true;
         this.isErrorMessageVisible = false;
-        this.chrono = 0;
-        this.isChronoRunning = false;
         this.totalDifferenceCounter = 0;
-
+        this.isChronoRunning = false;
         if (!this.connectionService.connected) {
             this.router.navigateByUrl("/");
         }
@@ -76,7 +72,9 @@ export class GameplayViewComponent implements OnInit {
             this.gameMode = params["mode"];
             this.requiredDifferences = this.gameMode == GameMode.Solo ? REQUIRED_DIFFERENCES_1P : REQUIRED_DIFFERENCES_2P;
             this.foundDifferencesCounters = this.gameMode == GameMode.Solo ? [0] : [0, 0];
-            this.startChrono();
+            this.startChrono = new EventEmitter<boolean>();
+            this.isChronoRunning = true;
+            this.startChrono.emit( this.isChronoRunning);
         });
         const SOUND_VOLUME: number = 0.2;
         this.CORRECT_SOUND.volume = SOUND_VOLUME;
@@ -100,6 +98,7 @@ export class GameplayViewComponent implements OnInit {
         this.totalDifferenceCounter++;
         if (this.foundDifferencesCounters.indexOf(this.requiredDifferences) !== -1) {
             this.isChronoRunning = false;
+            this.startChrono.emit(this.isChronoRunning );
             this.canClick = false;
         }
     }
@@ -132,37 +131,5 @@ export class GameplayViewComponent implements OnInit {
                 this.canClick = true;
             },
             this.ERROR_TIMEOUT);
-    }
-
-    private startChrono(): void {
-        this.isChronoRunning = true;
-        this.incrementChrono();
-    }
-
-    private incrementChrono(): void {
-        const ONE_SECOND: number = 1000;
-        setTimeout(
-            () => {
-                if (this.isChronoRunning) {
-                    this.chrono++;
-                    this.incrementChrono();
-                }
-            },
-            ONE_SECOND,
-        );
-    }
-
-    public get formattedChrono(): string {
-        const SECONDS: number = 60;
-        const seconds: number = this.chrono % SECONDS;
-        const minutes: number = Math.floor(this.chrono / SECONDS);
-
-        return `${this.formatTimeUnit(minutes)}:${this.formatTimeUnit(seconds)}`;
-    }
-
-    private formatTimeUnit(n: number): string {
-        const BASE: number = 10;
-
-        return `${n < BASE ? "0" : ""}${n}`;
     }
 }
