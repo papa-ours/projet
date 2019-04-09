a4fef10aacc94cf01b326880406eea574e303abc
Meziane Chebili <meziane.chebili@polymtl.ca>
2019-04-09 10:22:09 -0400

    MC : separation chrono.ts R

--- client/src/app/chrono/chrono.component.ts
+++ client/src/app/chrono/chrono.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { faHourglassHalf, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: "app-chrono",
-  templateUrl: "./chrono.component.html",
-  styleUrls: ["./chrono.component.css"],
+    selector: "app-chrono",
+    templateUrl: "./chrono.component.html",
+    styleUrls: ["./chrono.component.css"],
 })
-export class ChronoComponent implements OnInit {
+export class ChronoComponent {
 
-  public constructor() { }
+    private isChronoRunning: boolean;
+    public chrono: number;
+    public readonly hourglassIcon: IconDefinition = faHourglassHalf;
 
-  public ngOnInit() {
-  }
+    public constructor() {
+        this.chrono = 0;
+        this.isChronoRunning = false;
+    }
 
+    public tuggleChrono(isRunningMode: boolean): void {
+        isRunningMode ? this.startChrono() : this.stopChrono();
+    }
+
+    private startChrono(): void {
+        this.isChronoRunning = true;
+        this.incrementChrono();
+    }
+
+    private stopChrono(): void {
+        this.isChronoRunning = false;
+    }
+
+    private incrementChrono(): void {
+        const ONE_SECOND: number = 1000;
+        setTimeout(
+            () => {
+                if (this.isChronoRunning) {
+                    this.chrono++;
+                    this.incrementChrono();
+                }
+            },
+            ONE_SECOND,
+        );
+    }
+
+    public get formattedChrono(): string {
+        const SECONDS: number = 60;
+        const seconds: number = this.chrono % SECONDS;
+        const minutes: number = Math.floor(this.chrono / SECONDS);
+
+        return `${this.formatTimeUnit(minutes)}:${this.formatTimeUnit(seconds)}`;
+    }
+
+    private formatTimeUnit(n: number): string {
+        const BASE: number = 10;
+
+        return `${n < BASE ? "0" : ""}${n}`;
+    }
 }
