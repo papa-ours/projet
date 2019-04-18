6d709acf7f6b680a8f5ea96f2bd1fafe8a985874
Vincent <vg_4_@hotmail.fr>
2019-04-18 14:57:04 -0400

    VG: Correction de l'indentation R

--- server/app/services/game/waiting-room.ts
+++ server/app/services/game/waiting-room.ts
@@ -20,24 +20,25 @@ export class WaitingRoom {
     public addUser(username: string): void {
         if (this.usernames.length < this.REQUIRED_PLAYERS) {
             this.usernames.push(username);
-            this.joinRoom(username).then(() => {
-                Socket.io.to(`${this.gameSheetId}-${this.usernames[0]}`).emit("UserJoined", JSON.stringify(this.usernames));
-                if (this.usernames.length === this.REQUIRED_PLAYERS) {
-                    this.startGame();
-                }
-            })
-            .catch((error: Error) => console.error(error.message));
+            this.joinRoom(username)
+                .then(() => {
+                    Socket.io.to(`${this.gameSheetId}-${this.usernames[0]}`).emit("UserJoined", JSON.stringify(this.usernames));
+                    if (this.usernames.length === this.REQUIRED_PLAYERS) {
+                        this.startGame();
+                    }
+                })
+                .catch((error: Error) => console.error(error.message));
         }
     }
 
     private startGame(): void {
         Axios.get(`${SERVER_ADDRESS}/api/game/id/${this.name}/${this.type}/${GameMode.Pvp}/${JSON.stringify(this.usernames)}`)
-        .then((response: AxiosResponse<Message>) => {
-            Axios.delete(`${SERVER_ADDRESS}/api/game/waitingRoom/${this.name}/${this.usernames[0]}/${this.type}`)
-                .catch((error: Error) => console.error(error.message));
-            Socket.io.to(`${this.gameSheetId}-${this.usernames[0]}`).emit("GameReady", response.data.body);
-        })
-        .catch((error: Error) => console.error(error.message));
+            .then((response: AxiosResponse<Message>) => {
+                Axios.delete(`${SERVER_ADDRESS}/api/game/waitingRoom/${this.name}/${this.usernames[0]}/${this.type}`)
+                    .catch((error: Error) => console.error(error.message));
+                Socket.io.to(`${this.gameSheetId}-${this.usernames[0]}`).emit("GameReady", response.data.body);
+            })
+            .catch((error: Error) => console.error(error.message));
     }
 
     private async joinRoom(username: string): Promise<{}> {
