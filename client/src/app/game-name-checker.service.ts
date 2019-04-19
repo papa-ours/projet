b92aa26a3b5397ad301c68fb284cf75ac69be5d3
David <david.piche@polymtl.ca>
2019-04-18 22:18:55 -0400

    DP: Changement d'implémentation de methodes R

--- client/src/app/game-name-checker.service.ts
+++ client/src/app/game-name-checker.service.ts
@@ -1,30 +1,26 @@
 import { Injectable } from "@angular/core";
-import { GameType } from "../../../common/communication/game-description";
+import { GameType, GameSheet } from "../../../common/communication/game-description";
 import { GameListService } from "./game-list-getter.service";
 @Injectable({
     providedIn: "root",
 })
 export class GameNameCheckerService {
 
-    private names2d: string[];
-    private names3d: string[];
+    private names: string[];
     public constructor(private gameListService: GameListService) {  }
 
     public initialize(type: GameType): void {
-        this.names2d = [];
-        this.names3d = [];
+        this.names = [];
+        
         this.gameListService.getGameList().subscribe((lists) => {
-            for (const game3d of lists.list3d) {
-                this.names2d.push(game3d.name);
-            }
-            for (const game3d of lists.list2d) {
-                this.names3d.push(game3d.name);
+            const listNames: GameSheet[] = type === GameType.Simple ? lists.list2d : lists.list3d;
+            for (const game3d of listNames) {
+                this.names.push(game3d.name);
             }
         });
     }
     public checkName(name: string, gameType: GameType): boolean {
-        const names: string[] = gameType === GameType.Simple ? this.names2d : this.names3d;
-        for (const nameGame of names) {
+        for (const nameGame of this.names) {
             if (name === nameGame) {
                 return true;
             }
