80daff6dc3bdc5dac8999b72bd5958614a03fd23
Virasone Manibod <virasone.manibod@polymtl.ca>
2019-04-19 17:02:43 -0400

    VM: ajout de catch T

--- server/app/services/waiting-room.service.spec.ts
+++ server/app/services/waiting-room.service.spec.ts
@@ -1,23 +1,24 @@
-// import { expect } from "chai";
 import { GameType } from "../../../common/communication/game-description";
 import { container } from "../inversify.config";
 import Types from "../types";
 import { WaitingRoomService } from "./waiting-room.service";
 
-describe.only("WaitingRoomService", () => {
+describe("WaitingRoomService", () => {
     const waitingRoomService: WaitingRoomService = container.get<WaitingRoomService>(Types.WaitingRoomService);
 
     it("should resolve when creating a waiting room", (done: Mocha.Done) => {
         waitingRoomService.createWaitingRoom("voiture", "username", GameType.Simple)
             .then(() => {
                 done();
-            });
+            })
+            .catch((error: Error) => console.error(error.message));
     });
 
     it("should resolve when joining a waitingRoom", (done: Mocha.Done) => {
         waitingRoomService.joinWaitingRoom("voiture", "username2", GameType.Simple)
             .then(() => {
                 done();
-            });
+            })
+            .catch((error: Error) => console.error(error.message));
     });
 });
