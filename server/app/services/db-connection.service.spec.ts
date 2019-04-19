9c650dbcb68864055336a71c4bda616a65579cff
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-04-18 21:34:02 -0400

    PDB: déplacement des before et after hooks pour tous les tests T

--- server/app/services/db-connection.service.spec.ts
+++ server/app/services/db-connection.service.spec.ts
@@ -4,22 +4,20 @@ import { container } from "../inversify.config";
 import Types from "../types";
 import { DBConnectionService } from "./db-connection.service";
 
+let mongodb: MongoMemoryServer;
+let dbConnection: DBConnectionService;
+
+before(async () => {
+    mongodb = new MongoMemoryServer();
+    DBConnectionService[`${"URI"}`] = await mongodb.getUri();
+    dbConnection = container.get<DBConnectionService>(Types.DBConnectionService);
+});
+
+after(async () => {
+    return mongodb.stop();
+});
+
 describe("DB connection service", async () => {
-
-    let dbConnection: DBConnectionService;
-    let mongodb: MongoMemoryServer;
-
-    before(async () => {
-        mongodb = new MongoMemoryServer();
-        container.unbind(Types.DBConnectionServiceURI);
-        container.bind(Types.DBConnectionServiceURI).toConstantValue(await mongodb.getUri());
-
-        dbConnection = container.get(Types.DBConnectionService);
-    });
-
-    after(() => {
-        mongodb.stop();
-    });
 
     it("Should inject URL properly", async () => {
         expect(dbConnection["uri"]).to.equal(await mongodb.getUri());
