06aabfa79fb2972fa0a9285fa098391e878fc982
Vincent <vg_4_@hotmail.fr>
2019-04-19 17:18:21 -0400

    VG: Tests pour WaitingRoom T

---
+++ server/app/services/game/waiting-room.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { GameType } from "../../../../common/communication/game-description";
+import { WaitingRoom } from "./waiting-room";
+
+describe.only("Waiting Room", async () => {
+    it("should add the user", () => {
+        const waitingRoom: WaitingRoom = new WaitingRoom("1", "test", "user", GameType.Simple);
+        waitingRoom.addUser("user2");
+
+        expect(waitingRoom.usernames[1]).to.equals("user2");
+    });
+});
