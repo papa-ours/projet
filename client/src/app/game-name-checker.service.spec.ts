9f81439dc88ff3e30ae0f5d61449ef0f66ecb3e7
David <david.piche@polymtl.ca>
2019-04-18 22:54:47 -0400

    DP: Ajout de stub pour methode get T

--- client/src/app/game-name-checker.service.spec.ts
+++ client/src/app/game-name-checker.service.spec.ts
@@ -1,6 +1,28 @@
 import { GameNameChecker } from "./game-name-checker.service";
 import { TestBed } from "@angular/core/testing";
+const getGameListServiceStub = {
+    getGameList(): Observable<GameLists> {
+        const gameList2d: GameSheet[] = [
+            {
+                id: "1",
+                name: "test2d",
+                topScores: [
+                    {
+                        scoresStrings: ["1"]
+                    },
+                    {
+                        scoresStrings: ["2"]
+                    }
+                ]
+            }
+        ];
+        const gameList3d: GameSheet[] = gameList2d;
+        gameList2d[0].name = "test3d";
+        const gameList: GameLists = { list2d: gameList2d, list3d: gameList3d }
 
+        return of( gameList );
+    }
+  };
 describe("GameNameChecker", () => {
     let gameNameChecker: GameNameChecker;
     beforeEach(() => {
