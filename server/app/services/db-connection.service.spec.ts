67a232f589bd5673b8c4e7029f7f31166e417086
Pascal Dally-Belanger <pascal.dally-belanger@polymtl.ca>
2019-04-18 21:37:35 -0400

    PDB: insertion et cleanup des données dans la base de données T

--- server/app/services/db-connection.service.spec.ts
+++ server/app/services/db-connection.service.spec.ts
@@ -1,8 +1,10 @@
 import { expect } from "chai";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import * as mongoose from "mongoose";
 import { container } from "../inversify.config";
 import Types from "../types";
 import { DBConnectionService } from "./db-connection.service";
+import { DUMMY_DATABASE } from "./db-connection.service.spec.dummy";
 
 let mongodb: MongoMemoryServer;
 let dbConnection: DBConnectionService;
@@ -15,6 +17,13 @@ before(async () => {
 
 after(async () => {
     return mongodb.stop();
+});
+
+beforeEach(async () => {
+    return dbConnection["performRequest"](async (instance: typeof mongoose) => {
+        await instance.models.GameSheet.deleteMany({});
+        await instance.models.GameSheet.create(DUMMY_DATABASE);
+    });
 });
 
 describe("DB connection service", async () => {
