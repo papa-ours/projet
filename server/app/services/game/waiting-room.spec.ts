0c26ab770b0a7cb04edc4d665ac5f36ab4d9daf1
Vincent <vg_4_@hotmail.fr>
2019-04-19 17:18:41 -0400

    VG: Retrait du only T

--- server/app/services/game/waiting-room.spec.ts
+++ server/app/services/game/waiting-room.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { GameType } from "../../../../common/communication/game-description";
 import { WaitingRoom } from "./waiting-room";
 
-describe.only("Waiting Room", async () => {
+describe("Waiting Room", async () => {
     it("should add the user", () => {
         const waitingRoom: WaitingRoom = new WaitingRoom("1", "test", "user", GameType.Simple);
         waitingRoom.addUser("user2");
