329268152a55343789e4e3c967509eca11c7c912
David <david.piche@polymtl.ca>
2019-04-19 20:21:01 -0400

    DP: Correction de test T

--- client/src/app/gameplay.service.spec.ts
+++ client/src/app/gameplay.service.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController, TestRequest } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
-import { GameType } from "../../../common/communication/game-description";
+import { GameType, GameMode } from "../../../common/communication/game-description";
 import { GameplayService } from "./gameplay.service";
 
 describe("GameplayService", () => {
@@ -21,10 +21,10 @@ describe("GameplayService", () => {
     });
 
     it("should be a GET REQUEST", () => {
-        gameplayService.getGameId("test", GameType.Simple, "name").subscribe(
+        gameplayService.getGameId("test", GameType.Simple, GameMode.Solo, "name").subscribe(
             (data: string) => { expect(data).toBeDefined();
         });
-        const request: TestRequest = httpMock.expectOne(`${gameplayService.URL}/test/${GameType.Simple}/name`);
+        const request: TestRequest = httpMock.expectOne(`${gameplayService.URL}/test/${GameType.Simple}/${GameMode.Solo}/${JSON.stringify("name")}`);
         expect(request.request.method).toBe("GET");
         httpMock.verify();
     });
